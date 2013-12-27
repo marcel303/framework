df0cd70b059a2c4bb8e24f08044cc5cb2bf4ef72
marcel303 <marcel303@gmail.com>
2013-12-27 19:24:24 +0100

    sRGB: - Added gamma correct vs non-correct side by side. - Added plane test + added a plane in the background.

--- users/marcel/sRGB/main.cpp
+++ users/marcel/sRGB/main.cpp
@@ -7,6 +7,8 @@
 typedef SimdVec Vec;
 typedef SimdVecArg VecArg;
 
+#define SHOW_NONLINEAR 1
+
 inline Vec operator+(VecArg v1, VecArg v2) { return v1.Add(v2); }
 inline Vec operator-(VecArg v1, VecArg v2) { return v1.Sub(v2); }
 inline Vec operator*(VecArg v1, VecArg v2) { return v1.Mul(v2); }
@@ -22,6 +24,7 @@ inline Vec operator-(VecArg v) { return v.Neg(); }
 
 const static float kGamma = 2.2f;
 
+#if 1
 inline float GammaToLinear(float v)
 {
 	return std::pow(v, kGamma);
@@ -49,6 +52,27 @@ inline Vec LinearToGamma(VecArg v)
 		LinearToGamma(v.Z()),
 		LinearToGamma(v.W()));
 }
+#else
+inline float GammaToLinear(float v)
+{
+	return v * v;
+}
+
+inline float LinearToGamma(float v)
+{
+	return std::sqrt(v);
+}
+
+inline Vec GammaToLinear(VecArg v)
+{
+	return v * v;
+}
+
+inline Vec LinearToGamma(VecArg v)
+{
+	return v.Sqrt();
+}
+#endif
 
 int LinearMix(int v1, int v2, float a)
 {
@@ -66,7 +90,10 @@ int RaySphere(
 	VecArg rayDirectionY,
 	VecArg rayDirectionZ,
 	VecArg sphereEq,
-	Vec & t1, Vec & t2)
+	Vec & out_t,
+	Vec & out_normalX,
+	Vec & out_normalY,
+	Vec & out_normalZ)
 { 
 	const Vec a =
 		rayDirectionX * rayDirectionX +
@@ -94,45 +121,88 @@ int RaySphere(
 
 	const Vec bb4ac = b * b - 4.f * a * c;
 
-	const Vec hit = a.CMP_GT(VEC_ZERO).AND(bb4ac.CMP_GE(VEC_ZERO));
+	const Vec hasSolution = a.CMP_GT(VEC_ZERO).AND(bb4ac.CMP_GE(VEC_ZERO));
 
-	const int result = hit.BITTEST();
-
-	if (result)
+	if (hasSolution.BITTEST())
 	{
-		t1 = (-b + bb4ac.Sqrt()) / (2.f * a);
-		t2 = (-b - bb4ac.Sqrt()) / (2.f * a);
-	}
+		const Vec bb4acSqrt = bb4ac.Sqrt();
+		const Vec t1 = (-b + bb4acSqrt) / (2.f * a);
+		const Vec t2 = (-b - bb4acSqrt) / (2.f * a);
+		const Vec t = t1.Min(t2).Max(VEC_ZERO);
+		
+		const Vec positionX = rayOriginX + rayDirectionX * t;
+		const Vec positionY = rayOriginY + rayDirectionY * t;
+		const Vec positionZ = rayOriginZ + rayDirectionZ * t;
 
-	return result;
+		const Vec normalX = positionX - sphereEq.ReplicateX();
+		const Vec normalY = positionY - sphereEq.ReplicateY();
+		const Vec normalZ = positionZ - sphereEq.ReplicateZ();
+		
+		const Vec mask = hasSolution.AND(t.CMP_LT(out_t));
+		
+		out_t = out_t.Select(mask, out_t, t);
+		out_normalX = out_normalX.Select(mask, out_normalX, normalX);
+		out_normalY = out_normalY.Select(mask, out_normalY, normalY);
+		out_normalZ = out_normalZ.Select(mask, out_normalZ, normalZ);
+		
+		return mask.BITTEST();
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-void RayPlane(
+int RayPlane(
 	VecArg rayOriginX,
 	VecArg rayOriginY,
 	VecArg rayOriginZ,
 	VecArg rayDirectionX,
 	VecArg rayDirectionY,
 	VecArg rayDirectionZ,
-	VecArg planeEquationX,
-	VecArg planeEquationY,
-	VecArg planeEquationZ,
-	VecArg planeEquationW,
-	Vec & t)
+	VecArg planeEq,
+	Vec & out_t,
+	Vec & out_normalX,
+	Vec & out_normalY,
+	Vec & out_normalZ)
 {
 	Vec d =
-		rayOriginX.Mul(planeEquationX).Add(
-		rayOriginY.Mul(planeEquationY).Add(
-		rayOriginZ.Mul(planeEquationZ)));
+		rayOriginX.Mul(planeEq.ReplicateX()).Add(
+		rayOriginY.Mul(planeEq.ReplicateY()).Add(
+		rayOriginZ.Mul(planeEq.ReplicateZ()).Add(
+		planeEq.ReplicateW())));
 		
 	Vec dd =
-		rayDirectionX.Mul(planeEquationX).Add(
-		rayDirectionY.Mul(planeEquationY).Add(
-		rayDirectionZ.Mul(planeEquationZ)));
+		rayDirectionX.Mul(planeEq.ReplicateX()).Add(
+		rayDirectionY.Mul(planeEq.ReplicateY()).Add(
+		rayDirectionZ.Mul(planeEq.ReplicateZ()).Add(
+		planeEq.ReplicateW())));
 
 	// d + dd * t = 0 => t = (0 - d) / dd
 	
-	t = d.Neg().Div(dd);
+	const Vec t = d.Neg().Div(dd);
+	
+	const Vec hasSolution = t.CMP_GE(VEC_ZERO);
+		
+	if (hasSolution.BITTEST())
+	{	
+		const Vec normalX = planeEq.ReplicateX();
+		const Vec normalY = planeEq.ReplicateY();
+		const Vec normalZ = planeEq.ReplicateZ();
+		
+		const Vec mask = hasSolution.AND(t.CMP_LT(out_t));
+		
+		out_t = out_t.Select(mask, out_t, t);
+		out_normalX = out_normalX.Select(mask, out_normalX, normalX);
+		out_normalY = out_normalY.Select(mask, out_normalY, normalY);
+		out_normalZ = out_normalZ.Select(mask, out_normalZ, normalZ);
+		
+		return mask.BITTEST();
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 int main(int argc, char * argv[])
@@ -193,7 +263,7 @@ int main(int argc, char * argv[])
 		
 		Vec materialColorBase(.5f, .7f, .9f);
 		
-		const int numLights = 2;
+		const int numLights = 3;
 		Vec lightDirection[numLights];
 		static Vec lightColorBase[numLights];
 		
@@ -218,16 +288,19 @@ int main(int argc, char * argv[])
 				std::sin(animationTime * (i + 1) * 1.234f),
 				std::sin(animationTime * (i + 1) * 2.345f), 0.f).UnitVec3();
 		}
-						
-		for (int r = 0; r < 2; ++r)
+		
+		const Vec sphereEq[2] =
+			{
+				Vec(-.6f, 0.f, 1.f, .5f),
+				Vec(+.6f, 0.f, 1.f, .5f)
+			};
+					
 		{
-			const Vec materialColor = (r == 1) ? GammaToLinear(materialColorBase) : materialColorBase;
+			const Vec materialColor = GammaToLinear(materialColorBase);
 			Vec lightColor[numLights];
 			for (int i = 0; i < numLights; ++i)
-				lightColor[i] = (r == 1) ? GammaToLinear(lightColorBase[i]) : lightColorBase[i];
+				lightColor[i] = GammaToLinear(lightColorBase[i]);
 			
-			const Vec sphereEq(r == 0 ? -.6f : +.6f, 0.f, 1.f, .5f);
-
 			const float viewScaleRcp = 1.f / (surface->h / 2.f);
 			const float viewCenterX = surface->w / 2.f;
 			const float viewCenterY = surface->h / 2.f;
@@ -244,39 +317,73 @@ int main(int argc, char * argv[])
 				Vec rayOriginZ(0.f);
 				for (int i = 0; i < 4; ++i)
 					rayOriginY(i) = (y - viewCenterY) * viewScaleRcp;
-										
+
 				for (int x = 0; x < surface->w / 4; ++x)
-				{				
+				{
 					for (int i = 0; i < 4; ++i)
 						rayOriginX(i) = ((x * 4 + i) - viewCenterX) * viewScaleRcp;
 					
 					// intersection
-					Vec t1, t2;
-					const int hit = RaySphere(
-						rayOriginX,
-						rayOriginY,
-						rayOriginZ,
-						rayDirectionX,
-						rayDirectionY,
-						rayDirectionZ,
-						sphereEq,
-						t1, t2);
+					Vec t(1000000.f);
+					Vec normalX;
+					Vec normalY;
+					Vec normalZ;
+					int hit = 0;
+				#if SHOW_NONLINEAR
+					int nonLinear = 0;
+				#endif
 					
+					for (int r = 0; r < 2; ++r)
+					{
+						const int temp = RaySphere(
+							rayOriginX,
+							rayOriginY,
+							rayOriginZ,
+							rayDirectionX,
+							rayDirectionY,
+							rayDirectionZ,
+							sphereEq[r],
+							t,
+							normalX,
+							normalY,
+							normalZ);
+						
+					#if SHOW_NONLINEAR
+						if (r == 0)
+							nonLinear = temp;
+					#endif
+						
+						hit |= temp;
+					}
+
+				#if 1
+					if (y >= s)
+					{
+						const Vec planeEq(0.f, 0.f, -1.f, .5f);
+						
+						hit |= RayPlane(
+							rayOriginX,
+							rayOriginY,
+							rayOriginZ,
+							rayDirectionX,
+							rayDirectionY,
+							rayDirectionZ,
+							planeEq,
+							t,
+							normalX,
+							normalY,
+							normalZ);
+					}
+				#endif
+				
 					if (hit)
 					{
-						// intersection point, normal
-						const Vec t = t1.Min(t2);
-						const Vec positionX = rayOriginX + rayDirectionX * t;
-						const Vec positionY = rayOriginY + rayDirectionY * t;
-						const Vec positionZ = rayOriginZ + rayDirectionZ * t;
-						Vec normalX = positionX - sphereEq.ReplicateX();
-						Vec normalY = positionY - sphereEq.ReplicateY();
-						Vec normalZ = positionZ - sphereEq.ReplicateZ();
-						Normalize3(normalX, normalY, normalZ);
 						// lighting
+						Normalize3(normalX, normalY, normalZ);
 						Vec diffuseX = VEC_ZERO;
 						Vec diffuseY = VEC_ZERO;
 						Vec diffuseZ = VEC_ZERO;
+					#if 1
 						for (int i = 0; i < numLights; ++i)
 						{
 							Vec lightIntensity =
@@ -284,18 +391,34 @@ int main(int argc, char * argv[])
 								lightDirection[i].ReplicateY() * normalY +
 								lightDirection[i].ReplicateZ() * normalZ;
 							if (i == 0 && numLights != 1)
-								lightIntensity = Vec(.5f);
-							//lightIntensity = lightIntensity.Add(Vec(.5f)).Mul(Vec(1.f/1.5f));
+								lightIntensity += Vec(.2f);
+							//lightIntensity = lightIntensity.Add(Vec(.2f)).Mul(Vec(1.f/1.2f)); // rim lighting
 							lightIntensity = lightIntensity.Mul(Vec(.5));
 							lightIntensity = lightIntensity.Max(VEC_ZERO);
-							diffuseX += lightIntensity * lightColor[i].ReplicateX();
-							diffuseY += lightIntensity * lightColor[i].ReplicateY();
-							diffuseZ += lightIntensity * lightColor[i].ReplicateZ();
+							
+						#if SHOW_NONLINEAR
+							const Vec lightColorTemp = nonLinear ? LinearToGamma(lightColor[i]) : lightColor[i];
+						#else
+							const Vec & lightColorTemp = lightColor[i];
+						#endif
+							diffuseX += lightIntensity * lightColorTemp.ReplicateX();
+							diffuseY += lightIntensity * lightColorTemp.ReplicateY();
+							diffuseZ += lightIntensity * lightColorTemp.ReplicateZ();
 						}
 						// mix material color
-						diffuseX *= materialColor.ReplicateX();
-						diffuseY *= materialColor.ReplicateY();
-						diffuseZ *= materialColor.ReplicateZ();
+					#if SHOW_NONLINEAR
+						const Vec materialColorTemp = nonLinear ? LinearToGamma(materialColor) : materialColor;
+					#else
+						const Vec & materialColorTemp = materialColor;
+					#endif
+						diffuseX *= materialColorTemp.ReplicateX();
+						diffuseY *= materialColorTemp.ReplicateY();
+						diffuseZ *= materialColorTemp.ReplicateZ();
+					#else
+						diffuseX = normalX;
+						diffuseY = normalY;
+						diffuseZ = normalZ;
+					#endif
 						// output
 						diffuseX = diffuseX.Saturate();
 						diffuseY = diffuseY.Saturate();
@@ -305,7 +428,9 @@ int main(int argc, char * argv[])
 							if (hit & (1 << i))
 							{
 								Vec diffuse(diffuseX(i), diffuseY(i), diffuseZ(i));
-								if (r == 1)
+							#if SHOW_NONLINEAR
+								if (!nonLinear)
+							#endif
 									diffuse = LinearToGamma(diffuse);
 								diffuse = diffuse.Mul(Vec(255.f));
 								const int cr = (int)diffuse(0);
