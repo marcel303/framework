1d14f7dff9260c9ba416fdb72e705040258b338e
marcel303 <marcel303@gmail.com>
2014-02-06 21:58:27 +0100

    - Added Creative Commons license - Added vertex welding - Fixed UVs (UV indices) - Added colors (with color indices)

--- users/marcel/fbx1/main.cpp
+++ users/marcel/fbx1/main.cpp
@@ -1,5 +1,8 @@
+#include <assert.h>
 #include <list>
+#include <map>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <vector>
 #include "fbx.h"
 
@@ -22,10 +25,11 @@ static void log(int logIndent, const char * fmt, ...)
 
 class FbxFileLogger
 {
+	int m_logIndent;
+	
 	const FbxReader * m_reader;
 	
 public:
-	int m_logIndent;
 	
 	FbxFileLogger(const FbxReader & reader)
 	{
@@ -97,10 +101,188 @@ public:
 class Mesh
 {
 public:
-	std::vector<float> vertices;
-	std::vector<float> normals;
-	std::vector<float> uvs;
-	std::vector<int> indices;
+	class Vertex
+	{
+	public:
+		Vertex()
+		{
+			memset(this, 0, sizeof(Vertex));
+		}
+		
+		inline bool operator<(const Vertex & w) const
+		{
+			const float * floats1 = reinterpret_cast<const float*>(this);
+			const float * floats2 = reinterpret_cast<const float*>(&w);
+			
+			const int kNumFloats = sizeof(Vertex) / sizeof(float);
+			
+			for (int i = 0; i < kNumFloats; ++i)
+			{
+				if (floats1[i] != floats2[i])
+					return floats1[i] < floats2[i];
+			}
+			
+			return false;
+		}
+		
+		inline bool operator==(const Vertex & w) const
+		{
+			const float * floats1 = reinterpret_cast<const float*>(this);
+			const float * floats2 = reinterpret_cast<const float*>(&w);
+			
+			const int kNumFloats = sizeof(Vertex) / sizeof(float);
+			
+			for (int i = 0; i < kNumFloats; ++i)
+			{
+				if (floats1[i] != floats2[i])
+					return false;
+			}
+			
+			return true;
+		}
+		
+		float px, py, pz;     // position
+		float nx, ny, nz;     // normal
+		float tx, ty;         // texture
+		float cx, cy, cz, cw; // color
+	};
+	
+	std::vector<Vertex> m_vertices;
+	std::vector<int> m_indices;
+	
+	void construct(
+		int logIndent,
+		const std::vector<float> & vertices,
+		const std::vector<int> & vertexIndices,
+		const std::vector<float> & normals,
+		const std::vector<float> & uvs,
+		const std::vector<int> & uvIndices,
+		const std::vector<float> & colors,
+		const std::vector<int> & colorIndices)
+	{
+		log(logIndent, "-- running da welding machine! --\n");
+		
+		logIndent++;
+		
+		log(logIndent, "input: %d vertices, %d indices, %d normals, %d UVs (%d indices), %d colors (%d indices)\n",
+			(int)vertices.size()/3,
+			(int)vertexIndices.size(),
+			(int)normals.size()/3,
+			(int)uvs.size()/2,
+			(int)uvIndices.size(),
+			(int)colors.size()/3,
+			(int)colorIndices.size());
+		
+		std::map<Vertex, int> weldVertices;
+		
+		for (size_t i = 0; i < vertexIndices.size(); ++i)
+		{
+			const bool isEnd = vertexIndices[i] < 0;
+			const size_t vertexIndex = isEnd ? ~vertexIndices[i] : vertexIndices[i];
+			
+			// fill in vertex members with the available data from the various input arrays
+			
+			Vertex vertex;
+			
+			// position
+			
+			if (vertexIndex * 3 < vertices.size())
+			{
+				vertex.px = vertices[vertexIndex * 3 + 0];
+				vertex.py = vertices[vertexIndex * 3 + 1];
+				vertex.pz = vertices[vertexIndex * 3 + 2];
+			}
+			
+			// normal
+			
+			if (i * 3 < normals.size())
+			{
+				vertex.nx = normals[i * 3 + 0];
+				vertex.ny = normals[i * 3 + 1];
+				vertex.nz = normals[i * 3 + 2];
+			}
+			
+			// uv
+			
+			if (uvIndices.size() >= vertexIndices.size())
+			{
+				// indexed UV
+				
+				const size_t uvIndex = uvIndices[i];
+				
+				if (uvIndex * 2 < uvs.size())
+				{
+					vertex.tx = uvs[uvIndex * 2 + 0];
+					vertex.ty = uvs[uvIndex * 2 + 1];
+				}
+			}
+			else if (uvIndices.size() == 0 && i * 2 < uvs.size())
+			{
+				// non-indexed UV
+				
+				vertex.tx = uvs[i * 2 + 0];
+				vertex.ty = uvs[i * 2 + 1];
+			}
+			
+			// color
+			
+			if (colorIndices.size() >= vertexIndices.size())
+			{
+				// indexed color
+				
+				const size_t colorIndex = colorIndices[i];
+				
+				if (colorIndex * 4 < colors.size())
+				{
+					vertex.cx = colors[colorIndex * 4 + 0];
+					vertex.cy = colors[colorIndex * 4 + 1];
+					vertex.cz = colors[colorIndex * 4 + 2];
+					vertex.cw = colors[colorIndex * 4 + 3];
+				}
+			}
+			else if (colorIndices.size() == 0 && i * 4 < colors.size())
+			{
+				// non-indexed color
+				
+				vertex.cx = colors[i * 4 + 0];
+				vertex.cy = colors[i * 4 + 1];
+				vertex.cz = colors[i * 4 + 2];
+				vertex.cw = colors[i * 4 + 3];
+			}
+			
+			// add unique vertex if it doesn't exist yet, or add an index for the existing vertex
+			
+			std::map<Vertex, int>::iterator w = weldVertices.find(vertex);
+			
+			int weldIndex;
+			
+			if (w == weldVertices.end())
+			{
+				// unique vertex. allocate index and add it to the map
+				
+				weldIndex = int(weldVertices.size());
+				
+				weldVertices[vertex] = weldIndex;
+				
+				m_vertices.push_back(vertex);
+			}
+			else
+			{
+				// duplicate vertex. just take the index
+				
+				weldIndex = w->second;
+			}
+			
+			m_indices.push_back(isEnd ? ~weldIndex : weldIndex);
+		}
+		
+		log(logIndent, "output: %d vertices, %d indices\n",
+			(int)m_vertices.size(),
+			(int)m_indices.size());
+		
+		logIndent--;
+	}
+	
 };
 
 bool readFile(const char * filename, std::vector<uint8_t> & bytes)
@@ -129,6 +311,7 @@ int main(int argc, const char * argv[])
 	
 	// parse command line
 	
+	bool dumpMeshes = false;
 	bool verbose = false;
 	const char * filename = "test.fbx";
 	
@@ -136,6 +319,8 @@ int main(int argc, const char * argv[])
 	{
 		if (!strcmp(argv[i], "-v"))
 			verbose = true;
+		else if (!strcmp(argv[i], "-m"))
+			dumpMeshes = true;
 		else
 			filename = argv[i];
 	}
@@ -185,100 +370,113 @@ int main(int argc, const char * argv[])
 				Mesh & mesh = meshes.back();
 				
 				const FbxRecord vertices = model.firstChild("Vertices");
-				const FbxRecord indices = model.firstChild("PolygonVertexIndex");
+				const FbxRecord vertexIndices = model.firstChild("PolygonVertexIndex");
 				
 				const FbxRecord normalsLayer = model.firstChild("LayerElementNormal");
 				const FbxRecord normals = normalsLayer.firstChild("Normals");
 				
 				const FbxRecord uvsLayer = model.firstChild("LayerElementUV");
 				const FbxRecord uvs = uvsLayer.firstChild("UV");
+				const FbxRecord uvIndices = uvsLayer.firstChild("UVIndex");
 				
-				log(logIndent, "found a mesh! hasVertices: %d, hasNormals: %d, hasUVs: %d\n",
+				const FbxRecord colorsLayer = model.firstChild("LayerElementColor");
+				const FbxRecord colors = colorsLayer.firstChild("Colors");
+				const FbxRecord colorIndices = colorsLayer.firstChild("ColorIndex");
+				
+				log(logIndent, "found a mesh! name: %s, hasVertices: %d, hasNormals: %d, hasUVs: %d\n",
+					modelProps[0].c_str(),
 					vertices.isValid(),
 					normals.isValid(),
 					uvs.isValid());
 				
-				mesh.vertices = vertices.captureProperties<float>();			
-				mesh.indices = indices.captureProperties<int>();
-				mesh.normals = normals.captureProperties<float>();
-				mesh.uvs = uvs.captureProperties<float>();
+				logIndent++;
+				
+				mesh.construct(
+					logIndent,
+					vertices.captureProperties<float>(),		
+					vertexIndices.captureProperties<int>(),
+					normals.captureProperties<float>(),
+					uvs.captureProperties<float>(),
+					uvIndices.captureProperties<int>(),
+					colors.captureProperties<float>(),
+					colorIndices.captureProperties<int>());
 				
 				// LayerElementNormal.Normals
 				// LayerElementUV.UV
 				// LayerElementMaterial.Materials
 				// LayerElementTexture.TextureId
 				
-				// make sure our normals and uvs arrays are at least as large as our vertex array.
-				// we don't want out of bounds accessed if some of the layer data is missing or incorrect
-				
-				const size_t numVertices = mesh.vertices.size() / 3;
-				if (mesh.normals.size() < numVertices * 3)
-					mesh.normals.resize(numVertices * 3);
-				if (mesh.uvs.size() < numVertices * 2)
-					mesh.uvs.resize(numVertices * 2);
+				logIndent--;
 			}
 		}
 	}
 	
 	// dump mesh data
 	
-	log(logIndent, "-- dumping mesh data --\n");
-	
-	for (std::list<Mesh>::iterator i = meshes.begin(); i != meshes.end(); ++i)
+	if (dumpMeshes)
 	{
-		const Mesh & mesh = *i;
+		log(logIndent, "-- dumping mesh data --\n");
 		
-		log(logIndent, "mesh: numVertices=%d, numIndices=%d\n", int(mesh.vertices.size()), int(mesh.indices.size()));
-		
-		logIndent++;
-		
-		int polygonIndex = ~0;
-		
-		for (size_t i = 0; i < mesh.indices.size(); ++i)
+		for (std::list<Mesh>::iterator i = meshes.begin(); i != meshes.end(); ++i)
 		{
-			int index = mesh.indices[i];
+			const Mesh & mesh = *i;
 			
-			bool end = false;
-			
-			if (index < 0)
-			{
-				// negative indices mark the end of a polygon
-				
-				index = ~index;
-				
-				end = true;
-			}
-			
-			if (polygonIndex < 0)
-			{
-				polygonIndex = ~polygonIndex;
-				log(logIndent, "polygon [%d]:\n", polygonIndex);
-			}
+			log(logIndent, "mesh: numVertices=%d, numIndices=%d\n", int(mesh.m_vertices.size()), int(mesh.m_indices.size()));
 			
 			logIndent++;
 			
-			log(logIndent, "position = (%+7.2f %+7.2f %+7.2f), normal = (%+5.2f %+5.2f %+5.2f), uv = (%+5.2f %+5.2f)\n",
-				index,
-				mesh.vertices[index*3+0],
-				mesh.vertices[index*3+1],
-				mesh.vertices[index*3+2],
-				mesh.normals[index*3+0],
-				mesh.normals[index*3+1],
-				mesh.normals[index*3+2],
-				mesh.uvs[index*2+0],
-				mesh.uvs[index*2+1]);
+			int polygonIndex = ~0;
+			
+			for (size_t i = 0; i < mesh.m_indices.size(); ++i)
+			{
+				int index = mesh.m_indices[i];
+				
+				bool end = false;
+				
+				if (index < 0)
+				{
+					// negative indices mark the end of a polygon
+					
+					index = ~index;
+					
+					end = true;
+				}
+				
+				if (polygonIndex < 0)
+				{
+					polygonIndex = ~polygonIndex;
+					log(logIndent, "polygon [%d]:\n", polygonIndex);
+				}
+				
+				logIndent++;
+				
+				log(logIndent, "[%05d] position = (%+7.2f %+7.2f %+7.2f), normal = (%+5.2f %+5.2f %+5.2f), color = (%4.2f %4.2f %4.2f %4.2f), uv = (%+5.2f %+5.2f)\n",
+					index,
+					mesh.m_vertices[index].px,
+					mesh.m_vertices[index].py,
+					mesh.m_vertices[index].pz,
+					mesh.m_vertices[index].nx,
+					mesh.m_vertices[index].ny,
+					mesh.m_vertices[index].nz,
+					mesh.m_vertices[index].cx,
+					mesh.m_vertices[index].cy,
+					mesh.m_vertices[index].cz,
+					mesh.m_vertices[index].cw,
+					mesh.m_vertices[index].tx,
+					mesh.m_vertices[index].ty);
+							
+				logIndent--;
+				
+				if (end)
+				{
+					log(logIndent, "\n");
+					
+					polygonIndex = ~(polygonIndex + 1);
+				}
+			}
 			
 			logIndent--;
-			
-			if (end)
-			{
-				log(logIndent, "\n");
-				
-				polygonIndex = ~(polygonIndex + 1);
-			}
 		}
-		
-		logIndent--;
 	}
 	
 	return 0;
