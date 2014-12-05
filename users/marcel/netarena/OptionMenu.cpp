a46812340a718a8d6848c1c4ba2e9699b0ba18d2
Marcel <devnull@localhost>
2014-12-05 17:40:03 +0100

    Refactoring OptionMenu to derive from a shared MultiLevelMenuBase, which implements most of the menu logic. MultiLevelMenuBase will eventually be used by the stat timer menu as well.

--- users/marcel/netarena/OptionMenu.cpp
+++ users/marcel/netarena/OptionMenu.cpp
@@ -1,234 +1,50 @@
-#include <algorithm>
 #include "framework.h"
 #include "OptionMenu.h"
 #include "Options.h"
 
-OptionMenu::Node::Node()
-	: m_parent(0)
-	, m_prevSibling(0)
-	, m_nextSibling(0)
-	, m_firstChild(0)
-	, m_currentSelection(0)
-	, m_option(0)
-	, m_timeValue(0.f)
-{
-}
-
-OptionMenu::Node * OptionMenu::Node::FindChild(const std::string & name) const
-{
-	for (Node * node = m_firstChild; node != 0; node = node->m_nextSibling)
-	{
-		if (node->m_name == name)
-			return node;
-	}
-
-	return 0;
-}
-
-//
-
-void OptionMenu::Create()
-{
-	Destroy(m_root);
-	m_root = new Node();
-
-	struct OptionSortable
-	{
-		OptionBase * option;
-
-		bool operator<(const OptionSortable & other) const
-		{
-			return strcmp(option->GetPath(), other.option->GetPath()) > 0;
-		}
-	};
-
-	std::vector<OptionSortable> options;
-
-	for (OptionBase * option = g_optionManager.m_head; option != 0; option = option->GetNext())
-	{
-		OptionSortable sortable;
-		sortable.option = option;
-		options.push_back(sortable);
-	}
-
-	std::sort(options.begin(), options.end());
-
-	for (auto & i : options)
-	{
-		OptionBase * option = i.option;
-
-		Node * node = m_root;
-
-		std::string path = option->GetPath();
-
-		for (;;)
-		{
-			size_t pos = path.find('/');
-
-			if (pos != path.npos)
-			{
-				const std::string childName = path.substr(0, pos);
-
-				Node * child = node->FindChild(childName);
-
-				if (child == 0)
-				{
-					child = new Node();
-
-					child->m_name = childName;
-					child->m_parent = node;
-
-					if (node->m_firstChild)
-					{
-						node->m_firstChild->m_prevSibling = child;
-						child->m_nextSibling = node->m_firstChild;
-					}
-					node->m_firstChild = child;
-					node->m_currentSelection = child;
-				}
-
-				node = child;
-
-				path = path.substr(pos + 1);
-			}
-			else
-			{
-				Node * leaf = new Node();
-
-				leaf->m_name = path;
-				leaf->m_parent = node;
-				leaf->m_option = option;
-
-				if (node->m_firstChild)
-				{
-					node->m_firstChild->m_prevSibling = leaf;
-					leaf->m_nextSibling = node->m_firstChild;
-				}
-				node->m_firstChild = leaf;
-				node->m_currentSelection = leaf;
-
-				break;
-			}
-		}
-	}
-}
-
-void OptionMenu::Destroy(Node * node)
-{
-	if (!node)
-		return;
-	Node * child = node->m_firstChild;
-	while (child != 0)
-	{
-		Node * nextSibling = child->m_nextSibling;
-		Destroy(child);
-		child = nextSibling;
-	}
-	delete node;
-}
-
 OptionMenu::OptionMenu()
-	: m_root(0)
-	, m_currentNode(0)
-	, m_hasActionsPrevUpdate(false)
-	, m_hasActionsCurrUpdate(false)
+	: MultiLevelMenuBase()
 {
 	Create();
-
-	m_currentNode = m_root;
 }
 
 OptionMenu::~OptionMenu()
 {
-	m_currentNode = 0;
-
-	Destroy(m_root);
-	m_root = 0;
 }
 
-bool OptionMenu::HasNavParent() const
+//
+
+std::vector<void*> OptionMenu::GetMenuItems()
 {
-	return (m_currentNode->m_parent != 0);
+	std::vector<void*> result;
+
+	for (OptionBase * option = g_optionManager.m_head; option != 0; option = option->GetNext())
+		result.push_back(option);
+
+	return result;
 }
 
-void OptionMenu::HandleAction(Action action, float dt)
+const char * OptionMenu::GetPath(void * menuItem)
 {
-	Node *& currentSelection = m_currentNode->m_currentSelection;
-
-	if (currentSelection == 0)
-		return; // empty tree
-
-	// make sure the action triggers if it isn't a repeat
-	if (!m_hasActionsPrevUpdate && !m_hasActionsCurrUpdate)
-		dt = 1000.f;
-
-	const float kMoveSpeed = 6.f;
-	const float kChangeSpeed = 60.f / 4.f;
-
-	switch (action)
-	{
-	case Action_NavigateUp:
-		currentSelection->m_timeValue += dt * kMoveSpeed;
-		if (currentSelection->m_timeValue >= 1.f)
-		{
-			currentSelection->m_timeValue = 0.f;
-			if (currentSelection->m_prevSibling)
-				currentSelection = currentSelection->m_prevSibling;
-		}
-		break;
-	case Action_NavigateDown:
-		currentSelection->m_timeValue += dt * kMoveSpeed;
-		if (currentSelection->m_timeValue >= 1.f)
-		{
-			currentSelection->m_timeValue = 0.f;
-			if (currentSelection->m_nextSibling)
-				currentSelection = currentSelection->m_nextSibling;
-		}
-		break;
-	case Action_NavigateSelect:
-		if (currentSelection->m_option == 0)
-			m_currentNode = currentSelection;
-		else
-			currentSelection->m_option->Select();
-		break;
-	case Action_NavigateBack:
-		if (m_currentNode->m_parent)
-			m_currentNode = m_currentNode->m_parent;
-		break;
-	case Action_ValueIncrement:
-		currentSelection->m_timeValue += dt * kChangeSpeed;
-		if (currentSelection->m_timeValue >= 1.f)
-		{
-			currentSelection->m_timeValue = 0.f;
-			if (currentSelection->m_option)
-				currentSelection->m_option->Increment();
-		}
-		break;
-	case Action_ValueDecrement:
-		currentSelection->m_timeValue += dt * kChangeSpeed;
-		if (currentSelection->m_timeValue >= 1.f)
-		{
-			currentSelection->m_timeValue = 0.f;
-			if (currentSelection->m_option)
-				currentSelection->m_option->Decrement();
-		}
-		break;
-	}
-
-	if (!m_hasActionsPrevUpdate && !m_hasActionsCurrUpdate)
-	{
-		if (m_currentNode->m_currentSelection)
-			m_currentNode->m_currentSelection->m_timeValue = -1.f;
-	}
-
-	m_hasActionsCurrUpdate = true;
+	return static_cast<OptionBase*>(menuItem)->GetPath();
 }
 
-void OptionMenu::Update()
+void OptionMenu::Select(void * menuItem)
 {
-	m_hasActionsPrevUpdate = m_hasActionsCurrUpdate;
-	m_hasActionsCurrUpdate = false;
+	static_cast<OptionBase*>(menuItem)->Select();
 }
+
+void OptionMenu::Increment(void * menuItem)
+{
+	static_cast<OptionBase*>(menuItem)->Increment();
+}
+
+void OptionMenu::Decrement(void * menuItem)
+{
+	static_cast<OptionBase*>(menuItem)->Decrement();
+}
+
+//
 
 void OptionMenu::Draw(int x, int y, int sx, int sy)
 {
@@ -252,9 +68,11 @@ void OptionMenu::Draw(int x, int y, int sx, int sy)
 	int index = 0;
 	for (Node * node = m_currentNode->m_firstChild; node != 0; node = node->m_nextSibling, index++)
 	{
+		OptionBase * option = static_cast<OptionBase*>(node->m_menuItem);
+
 		if (node == m_currentNode->m_currentSelection)
 		{
-			if (node->m_option && node->m_option->GetType() == OptionBase::kType_Command)
+			if (option && option->GetType() == OptionBase::kType_Command)
 				setColor(255, 227, 127, 191);
 			else
 				setColor(127, 227, 255, 191);
@@ -263,17 +81,17 @@ void OptionMenu::Draw(int x, int y, int sx, int sy)
 		}
 		else
 		{
-			if (node->m_option && node->m_option->GetType() == OptionBase::kType_Command)
+			if (option && option->GetType() == OptionBase::kType_Command)
 				setColor(255, 227, 127);
 			else
 				setColor(127, 227, 255);
 		}
-		drawText(x + 2, y + index * lineSize, fontSize, +1.f, +1.f, node->m_option ? "%s" : "[ %s ]", node->m_name.c_str());
-		if (node->m_option)
+		drawText(x + 2, y + index * lineSize, fontSize, +1.f, +1.f, option ? "%s" : "[ %s ]", node->m_name.c_str());
+		if (option)
 		{
 			const int bufferSize = 64;
 			char buffer[bufferSize];
-			node->m_option->ToString(buffer, bufferSize);
+			option->ToString(buffer, bufferSize);
 			drawText(x + sx - 1 - 2, y + index * lineSize, fontSize, -1.f, +1.f, buffer);
 		}
 	}
--- users/marcel/netarena/OptionMenu.h
+++ users/marcel/netarena/OptionMenu.h
@@ -1,54 +1,21 @@
 #pragma once
 
-#include <string>
+#include "MultiLevelMenu.h"
 
 class OptionBase;
 
-class OptionMenu
+class OptionMenu : public MultiLevelMenuBase
 {
-	struct Node
-	{
-		Node();
-
-		std::string m_name;
-
-		Node * m_parent;
-		Node * m_prevSibling;
-		Node * m_nextSibling;
-		Node * m_firstChild;
-		Node * m_currentSelection;
-		OptionBase * m_option;
-		float m_timeValue; // used for 'key repeat'
-
-		Node * FindChild(const std::string & name) const;
-	};
-
-	void Create();
-	void Destroy(Node * node);
-
-	Node * m_root;
-	Node * m_currentNode;
-
-	bool m_hasActionsPrevUpdate;
-	bool m_hasActionsCurrUpdate;
+	// MultiLevelMenuBase
+	virtual std::vector<void*> GetMenuItems();
+	virtual const char * GetPath(void * menuItem);
+	virtual void Select(void * menuItem);
+	virtual void Increment(void * menuItem);
+	virtual void Decrement(void * menuItem);
 
 public:
-	enum Action
-	{
-		Action_NavigateUp,
-		Action_NavigateDown,
-		Action_NavigateSelect,
-		Action_NavigateBack,
-		Action_ValueIncrement,
-		Action_ValueDecrement,
-	};
-
 	OptionMenu();
-	~OptionMenu();
+	virtual ~OptionMenu();
 
-	bool HasNavParent() const;
-	void HandleAction(Action action, float dt = 1.f);
-
-	void Update();
 	void Draw(int x, int y, int sx, int sy);
 };
