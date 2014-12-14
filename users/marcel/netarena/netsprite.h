472c81418e812534199240e8a95bfaad1d28dc87
Marcel <devnull@localhost>
2014-12-14 15:10:31 +0100

    Net sprites..

---
+++ users/marcel/netarena/netsprite.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+#define MAX_SPRITES 256
+#define SPRITE_ID_INVALID 0xffff
+
+class Sprite;
+
+class NetSprite
+{
+public:
+	NetSprite();
+
+	void set(const char * filename, int x, int y);
+
+	bool enabled;
+	Sprite * sprite;
+};
+
+class NetSpriteManager
+{
+	uint16_t m_freeList[MAX_SPRITES];
+	uint16_t m_numFree;
+
+public:
+	NetSprite m_sprites[MAX_SPRITES];
+
+	NetSpriteManager();
+
+	void draw();
+
+	uint16_t alloc();
+	void free(uint16_t id);
+};
