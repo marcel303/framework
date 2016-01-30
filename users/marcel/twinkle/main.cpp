557b31fd9f94fd7c44ef0a8248e0b4299b576976
Marcel <devnull@localhost>
2016-01-30 20:41:15 +0100

    Lot's of stuff!

--- users/marcel/twinkle/main.cpp
+++ users/marcel/twinkle/main.cpp
@@ -13,6 +13,8 @@
 #define HEART_SPRITE Spriter("Art Assets/Animation/Planet_heart/Heart_Life.scml")
 #define PLAYER_SPRITE Spriter("Art Assets/Animation/Shoon/Shoon_anim_000.scml")
 #define SUN_SPRITE Spriter("Art Assets/Animation/Planet_heart/Heart_Life.scml")
+#define PARTICLE_SPRITE Spriter("Art Assets/Animation/Particules/Particules.scml")
+#define DIAMOND_SPRITE Spriter("Art Assets/Animation/Diamonds/Diamond.scml")
 
 class Sun
 {
@@ -22,19 +24,45 @@ public:
 		angle = 0.0f;
 		distance = 300.0f;
 		speed = 0.05f;
-		sun_state = 0;
-		color = colorYellow;
+		sun_state = 1;
+		prays_needed = 2;
 	}
 
 	void spawn()
 	{
 		spriteState.startAnim(SUN_SPRITE, 0);
+
+		color = colorYellow;
+		targetColor = color;
+	}
+
+	void change_color_to(Color new_color)
+	{
+		targetColor = new_color;
+		//color.r += (new_color.r > color.r ? 00.1 : -00.1);
+		//color.g += (new_color.g > color.g ? 00.1 : -00.1);
+		//color.b += (new_color.b > color.b ? 00.1 : -00.1);
+		color = color.interp(targetColor, 0.01f);
 	}
 
 	void tick(float dt)
 	{
 		angle += dt * speed;
 		spriteState.updateAnim(SUN_SPRITE, dt);
+		if (prays_needed > 0)
+			sun_state = 0;
+		if (prays_needed < 0)
+			sun_state = 2;
+		if (!prays_needed)
+			sun_state = 1;
+		if (sun_state == 2)
+			change_color_to(colorRed);
+		if (sun_state == 0)
+			change_color_to(colorBlue);
+		if (sun_state == 1)
+			change_color_to(colorYellow);
+		//if(keyboard.wentDown(SDLK_SPACE))
+			//sun_state = (sun_state + 1) % 3;
 	}
 
 	void draw()
@@ -42,7 +70,7 @@ public:
 		spriteState.x = WORLD_X + cosf(angle) * (WORLD_RADIUS + distance);
 		spriteState.y = WORLD_Y + sinf(angle) * (WORLD_RADIUS + distance);
 		spriteState.scale = 0.5f;
-		setColor(colorYellow);
+		setColor(color);
 		SUN_SPRITE.draw(spriteState);
 	}
 
@@ -50,8 +78,10 @@ public:
 	float angle;
 	float distance;
 	float speed;
-	int sun_state; // -1 not happy, 0 it's okey, 1, too happy
+	int sun_state; // 0 not happy, 1 it's okey, 2, too happy
+	int prays_needed;
 	Color color;
+	Color targetColor;
 };
 
 class Lemming
@@ -63,11 +93,15 @@ public:
 		pray = false;
 		angle = 0.0f;
 		state = 2;
+		sunHit = false;
+		sunHitProcessed = false;
 	}
 
 	void spawn()
 	{
 		spriteState.startAnim(LEMMING_SPRITE, state);
+		spriteStateDiamond.startAnim(DIAMOND_SPRITE, 0);
+		spriteStateParticles.startAnim(PARTICLE_SPRITE, 0);
 	}
 
 	void tick(float dt)
@@ -81,16 +115,36 @@ public:
 				state = 2;
 			spriteState.startAnim(LEMMING_SPRITE, state);
 		}
+		if (state == 1)
+		{
+			spriteStateDiamond.updateAnim(DIAMOND_SPRITE, dt);
+			spriteStateParticles.updateAnim(PARTICLE_SPRITE, dt);
+		}
 	}
 
 	void draw()
 	{
 		spriteState.x = WORLD_X + cosf(angle) * WORLD_RADIUS;
 		spriteState.y = WORLD_Y + sinf(angle) * WORLD_RADIUS;
-		spriteState.scale = 0.5f;
+		spriteState.scale = 0.7f;
 		spriteState.angle = angle * Calc::rad2deg + 90;
 		setColor(colorWhite);
 		LEMMING_SPRITE.draw(spriteState);
+		if (state == 1)
+		{
+			spriteStateParticles.x = WORLD_X + cosf(angle) * (WORLD_RADIUS + 100);
+			spriteStateParticles.y = WORLD_Y + sinf(angle) * (WORLD_RADIUS + 100);
+			spriteStateParticles.scale = 0.7f;
+			spriteStateParticles.angle = angle * Calc::rad2deg + 90;
+			setColor(colorWhite);
+			PARTICLE_SPRITE.draw(spriteStateParticles);
+			spriteStateDiamond.x = WORLD_X + cosf(angle) * (WORLD_RADIUS + 200);
+			spriteStateDiamond.y = WORLD_Y + sinf(angle) * (WORLD_RADIUS + 200);
+			spriteStateDiamond.scale = 0.7f;
+			spriteStateDiamond.angle = angle * Calc::rad2deg + 90;
+			setColor(colorWhite);
+			DIAMOND_SPRITE.draw(spriteStateDiamond);
+		}
 	}
 
 	void doPray()
@@ -102,12 +156,14 @@ public:
 
 	bool isActive;
 	SpriterState spriteState;
+	SpriterState spriteStateParticles;
+	SpriterState spriteStateDiamond;
 	float angle;
 	bool pray;
 	int state;
+	bool sunHit;
+	bool sunHitProcessed;
 };
-
-Lemming lemmings[MAX_LEMMINGS];
 
 class Player
 {
@@ -116,6 +172,7 @@ public:
 	float angle;
 	float max_speed;
 	float speed;
+	Lemming *selected_lemming;
 
 
 	Player()
@@ -141,6 +198,8 @@ public:
 			currentAccel += accel;
 		if (keyboard.isDown(SDLK_LEFT))
 			currentAccel -= accel;
+		if (keyboard.wentUp(SDLK_SPACE))
+			selected_lemming = 0;
 
 		speed += dt * currentAccel;
 
@@ -153,7 +212,10 @@ public:
 			speed = speed * powf(0.01f, dt);
 
 		angle += speed * dt;
-
+		if (selected_lemming)
+		{
+			selected_lemming->angle = angle;
+		}
 		spriteState.updateAnim(PLAYER_SPRITE, dt);
 
 		// lemmings
@@ -164,15 +226,19 @@ public:
 	{
 		spriteState.x = WORLD_X + cosf(angle) * WORLD_RADIUS;
 		spriteState.y = WORLD_Y + sinf(angle) * WORLD_RADIUS;
-		spriteState.scale = 1.0f;
+		spriteState.scale = 0.7f;
 		spriteState.angle = angle * Calc::rad2deg + 90;
 		setColor(colorWhite);
 		PLAYER_SPRITE.draw(spriteState);
 	}
+
 };
 
-Lemming	*check_collision(Player player, Lemming lemmings[], int nbr_lem)
+Lemming	*check_collision_player(Player player, Lemming lemmings[], int nbr_lem)
 {
+	Lemming	* result = 0;
+	int nearest = INT_MAX;
+
 	int dx;
 	int dy;
 	for (int i = 0; i < nbr_lem; ++i)
@@ -182,19 +248,69 @@ Lemming	*check_collision(Player player, Lemming lemmings[], int nbr_lem)
 
 		dx = player.spriteState.x - lemmings[i].spriteState.x;
 		dy = player.spriteState.y - lemmings[i].spriteState.y;
-		if (sqrtf(dx * dx + dy * dy) <= 100)
-			return (&lemmings[i]);
+		int d = sqrtf(dx * dx + dy * dy);
+		if (d < 100 && d < nearest)
+		{
+			result = (&lemmings[i]);
+			nearest = d;
+		}
+	}
+	return (result);
+}
+
+int check_collision_sun(Sun sun, Lemming lemmings[], int nbr_lem)
+{
+	const float angle = 10.f * Calc::deg2rad;
+	const float cosAngle = cosf(angle);
+
+	for (int i = 0; i < nbr_lem; ++i)
+	{
+		lemmings[i].sunHit = false;
+
+		if (!lemmings[i].isActive)
+			continue;
+		if (!lemmings[i].pray)
+			continue;
+
+		float dx1 = sun.spriteState.x;
+		float dy1 = sun.spriteState.y;
+		{
+			float d1 = sqrtf(dx1 * dx1 + dy1 * dy1);
+			dx1 /= d1;
+			dy1 /= d1;
+		}
+
+		float dx2 = lemmings[i].spriteState.x;
+		float dy2 = lemmings[i].spriteState.y;
+		{
+			float d2 = sqrtf(dx2 * dx2 + dy2 * dy2);
+			dx2 /= d2;
+			dy2 /= d2;
+		}
+
+		float dot = dx1 * dx2 + dy1 * dy2;
+
+		if (dot >= 0.f && dot >= cosAngle)
+			lemmings[i].sunHit = true;
 	}
 	return (0);
 }
 
 Player player;
+Lemming lemmings[MAX_LEMMINGS];
 Sun sun;
 
 int main(int argc, char * argv[])
 {
-	framework.fullscreen = true;
-	//framework.minification = 2;
+	if (1 == 2)
+	{
+		framework.fullscreen = false;
+		framework.minification = 2;
+	}
+	else
+	{
+		framework.fullscreen = true;
+	}
 
 	if (framework.init(0, 0, SX, SY))
 	{
@@ -241,12 +357,28 @@ int main(int argc, char * argv[])
 			for (int i = 0; i < MAX_LEMMINGS; ++i)
 				if (lemmings[i].isActive)
 					lemmings[i].tick(dt);
-			Lemming *le = check_collision(player, lemmings, MAX_LEMMINGS);
+			Lemming *le = check_collision_player(player, lemmings, MAX_LEMMINGS);
 			if (le)
 			{
+				if (keyboard.wentDown(SDLK_SPACE))
+					player.selected_lemming = le;
 				if (keyboard.wentDown(SDLK_UP))
 					le->doPray();
 			}
+
+			check_collision_sun(sun, lemmings, MAX_LEMMINGS);
+
+			for (int i = 0; i < MAX_LEMMINGS; ++i)
+			{
+				if (!lemmings[i].isActive)
+					continue;
+				if (!lemmings[i].sunHitProcessed && lemmings[i].sunHit)
+				{
+					--sun.prays_needed;
+					lemmings[i].sunHitProcessed = true;
+				}
+			}
+
 			// draw
 
 			framework.beginDraw(0, 0, 0, 0);
