69846ef652ac250a51bad1443cc7acf9df501146
Narf <devnull@localhost>
2016-02-08 12:49:29 +0100

    stash editor gameobject things

--- users/david/NetArenaLevelEditor/gameobject.h
+++ users/david/NetArenaLevelEditor/gameobject.h
@@ -2,6 +2,45 @@
 
 #include "QGraphicsRectItem"
 
+#include <QComboBox>
+#include <QSlider>
+#include <QLineEdit>
+#include <QGroupBox>
+#include <QRadioButton>
+#include <QGridLayout>
+#include <QPushButton>
+
+class ObjectWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	ObjectWidget();
+	~ObjectWidget();
+
+	QGroupBox* m_tps;
+
+	QComboBox* m_type;
+	QLineEdit* m_x;
+	QLineEdit* m_y;
+	QSlider* m_scaleSlider;
+
+	//QGroupBox* m_transBox;
+	//QGroupBox* m_templateControls;
+
+	//QRadioButton *m_mech;
+	//QRadioButton *m_coll;
+
+	QGridLayout* m_grid;
+
+	//QPushButton* m_editModeButton;
+	//QPushButton* m_saveTemplateButton;
+
+public slots:
+
+
+};
+
 class EditorObject : public QGraphicsRectItem //resizable super rectangle deluxe
 {
 
@@ -15,5 +54,16 @@ class EditorObject : public QGraphicsRectItem //resizable super rectangle deluxe
 
 	virtual QRectF boundingRect() const;
     virtual void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget = 0);
+
+
+
+
+	int m_type;
+	int m_state;
+
+	int GetX();
+	int GetY();
+
+
 
 };
