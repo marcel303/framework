8b7a3b3103b05c5a895cf19841b20236f9ca51e9
marcel303 <marcel303@gmail.com>
2016-11-17 19:08:28 +0100

    Expose scaled noise versions and fix simplex noise functions not being included in project file, cause undefined references.

--- libgg/Noise.cpp
+++ libgg/Noise.cpp
@@ -13,3 +13,21 @@ float octave_noise_1d(
 		x,
 		0.f);
 }
+
+float scaled_octave_noise_1d(
+	const float octaves,
+	const float persistence,
+	const float scale,
+	const float loBound,
+	const float hiBound,
+	const float x)
+{
+	return scaled_octave_noise_2d(
+		octaves,
+		persistence,
+		scale,
+		loBound,
+		hiBound,
+		x,
+		0.f);
+}
--- libgg/Noise.h
+++ libgg/Noise.h
@@ -29,3 +29,43 @@ float octave_noise_4d(
 	const float y,
 	const float z,
 	const float w);
+
+//
+
+float scaled_octave_noise_1d(
+	const float octaves,
+	const float persistence,
+	const float scale,
+	const float loBound,
+	const float hiBound,
+	const float x);
+
+float scaled_octave_noise_2d(
+	const float octaves,
+	const float persistence,
+	const float scale,
+	const float loBound,
+	const float hiBound,
+	const float x,
+	const float y);
+
+float scaled_octave_noise_3d(
+	const float octaves,
+	const float persistence,
+	const float scale,
+	const float loBound,
+	const float hiBound,
+	const float x,
+	const float y,
+	const float z);
+
+float scaled_octave_noise_4d(
+	const float octaves,
+	const float persistence,
+	const float scale,
+	const float loBound,
+	const float hiBound,
+	const float x,
+	const float y,
+	const float z,
+	const float w);
