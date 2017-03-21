4c75c7cf289b86c3466ff967e17eac9748d365e4
Marcel Smit <marcel303@gmail.com>
2017-03-21 16:28:09 +0100

    Added optimized decoding for 24 bpp RGB images.

--- users/marcel/framework/image.cpp
+++ users/marcel/framework/image.cpp
@@ -18,41 +18,77 @@ ImageData * loadImage(const char * filename)
 		return 0;
 	}
 	
-	FIBITMAP * bmp32 = FreeImage_ConvertTo32Bits(bmp);
+	const int sx = FreeImage_GetWidth(bmp);
+	const int sy = FreeImage_GetHeight(bmp);
 	
-	if (!bmp32)
-	{
-		logError("failed to convert image to 32 bpp: %s", filename);
-		return 0;
-	}
+	void * data = nullptr;
 
-	const int sx = FreeImage_GetWidth(bmp32);
-	const int sy = FreeImage_GetHeight(bmp32);
-
-	void * data = malloc(sx * sy * 4);
-	char * dest = (char*)data;
+	BITMAPINFO * info = FreeImage_GetInfo(bmp);
 	
-	for (int y = 0; y < sy; ++y)
+	if (info->bmiHeader.biBitCount == 24)
 	{
-		uint32_t * source = (uint32_t*)FreeImage_GetScanLine(bmp32, y);
+		const int sx = FreeImage_GetWidth(bmp);
+		const int sy = FreeImage_GetHeight(bmp);
+
+		data = malloc(sx * sy * 4);
+		char * dest = (char*)data;
 		
-		for (int x = 0; x < sx; ++x)
+		for (int y = 0; y < sy; ++y)
 		{
-			const int r = ((*source) >> FI_RGBA_RED_SHIFT) & 0xff;
-			const int g = ((*source) >> FI_RGBA_GREEN_SHIFT) & 0xff;
-			const int b = ((*source) >> FI_RGBA_BLUE_SHIFT) & 0xff;
-			const int a = ((*source) >> FI_RGBA_ALPHA_SHIFT) & 0xff;
-
-			*dest++ = r;
-			*dest++ = g;
-			*dest++ = b;
-			*dest++ = a;
+			uint8_t * source = (uint8_t*)FreeImage_GetScanLine(bmp, y);
 			
-			source++;
+			for (int x = 0; x < sx; ++x)
+			{
+				const int r = source[2];
+				const int g = source[1];
+				const int b = source[0];
+				const int a = 255;
+
+				*dest++ = r;
+				*dest++ = g;
+				*dest++ = b;
+				*dest++ = a;
+				
+				source += 3;
+			}
 		}
 	}
+	else
+	{
+		FIBITMAP * bmp32 = FreeImage_ConvertTo32Bits(bmp);
+		
+		if (!bmp32)
+		{
+			logError("failed to convert image to 32 bpp: %s", filename);
+			return 0;
+		}
+
+		data = malloc(sx * sy * 4);
+		char * dest = (char*)data;
+		
+		for (int y = 0; y < sy; ++y)
+		{
+			uint32_t * source = (uint32_t*)FreeImage_GetScanLine(bmp32, y);
+			
+			for (int x = 0; x < sx; ++x)
+			{
+				const int r = ((*source) >> FI_RGBA_RED_SHIFT) & 0xff;
+				const int g = ((*source) >> FI_RGBA_GREEN_SHIFT) & 0xff;
+				const int b = ((*source) >> FI_RGBA_BLUE_SHIFT) & 0xff;
+				const int a = ((*source) >> FI_RGBA_ALPHA_SHIFT) & 0xff;
+
+				*dest++ = r;
+				*dest++ = g;
+				*dest++ = b;
+				*dest++ = a;
+				
+				source++;
+			}
+		}
+		
+		FreeImage_Unload(bmp32);
+	}
 	
-	FreeImage_Unload(bmp32);
 	FreeImage_Unload(bmp);
 	
 	ImageData * imageData = new ImageData;
