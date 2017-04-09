97752158f3d0318b3c9fe818c05146d8947410e6
Marcel Smit <marcel303@gmail.com>
2017-04-09 18:37:40 +0200

    Change texture filtering to nearest for font glyphs, as it results in crisper text rendering.

--- users/marcel/framework/internal.cpp
+++ users/marcel/framework/internal.cpp
@@ -1742,8 +1742,8 @@ GlyphCacheElem & GlyphCache::findOrCreate(FT_Face face, int size, int c)
 			
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			checkErrorGL();
 			
 			// restore previous OpenGL states
