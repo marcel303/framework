8815b9a759b776acdb1e535427cb8b134065b98b
Marcel Smit <marcel303@gmail.com>
2017-05-29 01:48:29 +0200

    Spectrum 1D node: Added Fourier transform, normalize option, output scale, output mode and sample location (Y).

--- users/marcel/avgraph/vfxNodes/vfxNodeSpectrum1D.cpp
+++ users/marcel/avgraph/vfxNodes/vfxNodeSpectrum1D.cpp
@@ -1,14 +1,20 @@
+#include "fourier.h"
 #include "framework.h"
 #include "vfxNodeSpectrum1D.h"
 
 VfxNodeSpectrum1D::VfxNodeSpectrum1D()
 	: VfxNodeBase()
 	, texture()
+	, dreal(nullptr)
+	, dimag(nullptr)
 	, imageOutput()
 {
 	resizeSockets(kInput_COUNT, kOutput_COUNT);
-	addInput(kInput_Buffer, kVfxPlugType_Int);
-	addInput(kInput_Size, kVfxPlugType_Int);
+	addInput(kInput_Image, kVfxPlugType_ImageCpu);
+	addInput(kInput_SampleY, kVfxPlugType_Float);
+	addInput(kInput_OutputMode, kVfxPlugType_Int);
+	addInput(kInput_Normalize, kVfxPlugType_Bool);
+	addInput(kInput_Scale, kVfxPlugType_Float);
 	addOutput(kOutput_Image, kVfxPlugType_Image, &imageOutput);
 }
 
@@ -19,43 +25,115 @@ VfxNodeSpectrum1D::~VfxNodeSpectrum1D()
 
 void VfxNodeSpectrum1D::tick(const float dt)
 {
-	// todo : analyze buffer. create/update texture
-
-	const int buffer = getInputInt(kInput_Buffer, 0);
-	const int windowSize = getInputInt(kInput_Size, 64);
+	const VfxImageCpu * image = getInputImageCpu(kInput_Image, nullptr);
+	const int transformSx = image ? Fourier::upperPowerOf2(image->sx) : 0;
+	const float sampleY = getInputFloat(kInput_SampleY, .5f);
+	const OutputMode outputMode = (OutputMode)getInputInt(kInput_OutputMode, kOutputMode_Channel1And2);
+	const bool normalize = getInputBool(kInput_Normalize, true);
+	const float scale = getInputFloat(kInput_Scale, 1.f);
 	
-	if (texture.isChanged(windowSize, 1, GL_R32F))
+	if (image == nullptr)
 	{
-		if (windowSize <= 0)
+		freeTexture();
+	}
+	else if (texture.isChanged(transformSx, 1, GL_R32F))
+	{
+		if (image->sx == 0 || image->sy == 0)
 			freeTexture();
 		else
-			allocateTexture(windowSize);
+			allocateTexture(transformSx);
 	}
 	
 	if (texture.id != 0)
 	{
-		float * values = (float*)alloca(sizeof(float) * windowSize);
-		for (int i = 0; i < windowSize; ++i)
-			values[i] = random(-1.f, +1.f);
+		const VfxImageCpu::Channel & srcChannel = image->channel[0];
+		const int srcY = std::max(0, std::min(image->sy - 1, int(std::round(sampleY * image->sy))));
 		
-		texture.upload(values, 4, windowSize, GL_RED, GL_FLOAT);
+		{
+			const uint8_t * __restrict srcItr = srcChannel.data + srcY * srcChannel.pitch;
+			
+			float * __restrict rreal = dreal;
+			float * __restrict rimag = dimag;
+			
+			for (int x = 0; x < image->sx; ++x)
+			{
+				rreal[x] = *srcItr;
+				rimag[x] = 0.f;
+				
+				srcItr += srcChannel.stride;
+			}
+		}
+		
+		Fourier::fft1D_slow(dreal, dimag, image->sx, transformSx, false, normalize);
+		
+		{
+			float * __restrict rreal = dreal;
+			float * __restrict rimag = dimag;
+			
+			if (outputMode == kOutputMode_Channel1And2)
+			{
+				// todo : write combined real/imag channel
+				
+				for (int x = 0; x < transformSx; ++x)
+				{
+					const float r = rreal[x];
+					const float i = rimag[x];
+					const float s = std::hypotf(r, i);
+					
+					rreal[x] = s * scale;
+				}
+			}
+			else if (outputMode == kOutputMode_Channel1)
+			{
+				for (int x = 0; x < transformSx; ++x)
+				{
+					rreal[x] = rreal[x] * scale;
+				}
+			}
+			else if (outputMode == kOutputMode_Channel2)
+			{
+				for (int x = 0; x < transformSx; ++x)
+				{
+					rreal[x] = rimag[x] * scale;
+				}
+			}
+			else if (outputMode == kOutputMode_Length)
+			{
+				for (int x = 0; x < transformSx; ++x)
+				{
+					const float r = rreal[x];
+					const float i = rimag[x];
+					const float s = std::sqrtf(r * r + i * i);
+					
+					rreal[x] = s * scale;
+				}
+			}
+			else if (outputMode == kOutputMode_LengthSq)
+			{
+				for (int x = 0; x < transformSx; ++x)
+				{
+					const float r = rreal[x];
+					const float i = rimag[x];
+					const float sSq = r * r + i * i;
+					
+					rreal[x] = sSq * scale;
+				}
+			}
+		}
+		
+		texture.upload(dreal, 4, transformSx, GL_RED, GL_FLOAT);
 	}
 }
 
-void VfxNodeSpectrum1D::init(const GraphNode & node)
-{
-	const int windowSize = getInputInt(kInput_Size, 64);
-	
-	if (windowSize > 0)
-		allocateTexture(windowSize);
-}
-
-void VfxNodeSpectrum1D::allocateTexture(const int size)
+void VfxNodeSpectrum1D::allocateTexture(const int sx)
 {
 	freeTexture();
 	
-	texture.allocate(size, 1, GL_R32F);
+	texture.allocate(sx, 1, GL_R32F, true, true);
 	texture.setSwizzle(GL_RED, GL_RED, GL_RED, GL_ONE);
+	
+	dreal = new float[sx];
+	dimag = new float[sx];
 	
 	imageOutput.texture = texture.id;
 }
@@ -63,6 +141,12 @@ void VfxNodeSpectrum1D::allocateTexture(const int size)
 void VfxNodeSpectrum1D::freeTexture()
 {
 	texture.free();
+	
+	delete[] dreal;
+	dreal = nullptr;
+	
+	delete[] dimag;
+	dimag = nullptr;
 	
 	imageOutput.texture = 0;
 }
--- users/marcel/avgraph/vfxNodes/vfxNodeSpectrum1D.h
+++ users/marcel/avgraph/vfxNodes/vfxNodeSpectrum1D.h
@@ -5,10 +5,22 @@
 
 struct VfxNodeSpectrum1D : VfxNodeBase
 {
+	enum OutputMode
+	{
+		kOutputMode_Channel1And2,
+		kOutputMode_Channel1,
+		kOutputMode_Channel2,
+		kOutputMode_Length,
+		kOutputMode_LengthSq
+	};
+	
 	enum Input
 	{
-		kInput_Buffer,
-		kInput_Size,
+		kInput_Image,
+		kInput_SampleY,
+		kInput_OutputMode,
+		kInput_Normalize,
+		kInput_Scale,
 		kInput_COUNT
 	};
 	
@@ -19,6 +31,8 @@ struct VfxNodeSpectrum1D : VfxNodeBase
 	};
 	
 	OpenglTexture texture;
+	float * dreal;
+	float * dimag;
 	
 	VfxImage_Texture imageOutput;
 
@@ -27,8 +41,6 @@ struct VfxNodeSpectrum1D : VfxNodeBase
 	
 	virtual void tick(const float dt) override;
 	
-	virtual void init(const GraphNode & node) override;
-	
-	void allocateTexture(const int size);
+	void allocateTexture(const int sx);
 	void freeTexture();
 };
