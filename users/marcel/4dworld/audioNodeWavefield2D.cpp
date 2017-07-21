720544b78db73867fd71ec5226ce69f9728bba56
Marcel Smit <marcel303@gmail.com>
2017-07-21 10:57:10 +0200

    Added 2D wavefield node.

---
+++ users/marcel/4dworld/audioNodeWavefield2D.cpp
@@ -0,0 +1,173 @@
+/*
+Copyright (C) 2017 Marcel Smit
+marcel303@gmail.com
+https://www.facebook.com/marcel.smit981
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "audioNodeWavefield2D.h"
+#include "Noise.h"
+#include "wavefield.h"
+
+#include "framework.h" // random. todo : remove
+
+AUDIO_NODE_TYPE(wavefield_2d, AudioNodeWavefield2D)
+{
+	typeName = "wavefield.2d";
+	
+	in("size", "int", "16");
+	in("pos.dampen", "audioValue");
+	in("vel.dampen", "audioValue");
+	in("tension", "audioValue", "1");
+	in("wrap", "bool", "0");
+	in("sample.pos.x", "audioValue", "0.5");
+	in("sample.pos.y", "audioValue", "0.5");
+	in("trigger!", "trigger");
+	in("trigger.pos.x", "audioValue", "0.5");
+	in("trigger.pos.y", "audioValue", "0.5");
+	in("trigger.amount", "audioValue", "0.5");
+	in("randomize!", "trigger");
+	out("audio", "audioValue");
+}
+
+AudioNodeWavefield2D::AudioNodeWavefield2D()
+	: AudioNodeBase()
+	, wavefield(nullptr)
+	, audioOutput()
+{
+	resizeSockets(kInput_COUNT, kOutput_COUNT);
+	addInput(kInput_Size, kAudioPlugType_Int);
+	addInput(kInput_PositionDampening, kAudioPlugType_FloatVec);
+	addInput(kInput_VelocityDampening, kAudioPlugType_FloatVec);
+	addInput(kInput_Tension, kAudioPlugType_FloatVec);
+	addInput(kInput_Wrap, kAudioPlugType_Bool);
+	addInput(kInput_SampleLocationX, kAudioPlugType_FloatVec);
+	addInput(kInput_SampleLocationY, kAudioPlugType_FloatVec);
+	addInput(kInput_Trigger, kAudioPlugType_Trigger);
+	addInput(kInput_TriggerLocationX, kAudioPlugType_FloatVec);
+	addInput(kInput_TriggerLocationY, kAudioPlugType_FloatVec);
+	addInput(kInput_TriggerAmount, kAudioPlugType_FloatVec);
+	addOutput(kOutput_Audio, kAudioPlugType_FloatVec, &audioOutput);
+
+	wavefield = new Wavefield2D();
+}
+
+AudioNodeWavefield2D::~AudioNodeWavefield2D()
+{
+	delete wavefield;
+	wavefield = nullptr;
+}
+
+void AudioNodeWavefield2D::draw()
+{
+	const AudioFloat defaultTension(1.f);
+	
+	const AudioFloat * positionDampening = getInputAudioFloat(kInput_PositionDampening, &AudioFloat::Zero);
+	const AudioFloat * velocityDampening = getInputAudioFloat(kInput_VelocityDampening, &AudioFloat::Zero);
+	const AudioFloat * tension = getInputAudioFloat(kInput_Tension, &defaultTension);
+	const bool wrap = getInputBool(kInput_Wrap, false);
+	const AudioFloat * sampleLocationX = getInputAudioFloat(kInput_SampleLocationX, &AudioFloat::Half);
+	const AudioFloat * sampleLocationY = getInputAudioFloat(kInput_SampleLocationY, &AudioFloat::Half);
+	const int size = getInputInt(kInput_Size, 16);
+	
+	//
+	
+	if (size != wavefield->numElems)
+	{
+		wavefield->init(size);
+	}
+	
+	//
+	
+	audioOutput.setVector();
+	
+	positionDampening->expand();
+	velocityDampening->expand();
+	tension->expand();
+	sampleLocationX->expand();
+	sampleLocationY->expand();
+	
+	//
+	
+	const double dt = 1.0 / double(SAMPLE_RATE);
+	
+	const double maxTension = 2000000000.0;
+	
+	for (int i = 0; i < AUDIO_UPDATE_SIZE; ++i)
+	{
+		const double c = clamp<double>(tension->samples[i] * 1000000.0, -maxTension, +maxTension);
+		
+		wavefield->tick(dt, c, 1.0 - positionDampening->samples[i], 1.0 - velocityDampening->samples[i], wrap == false);
+		
+		audioOutput.samples[i] = wavefield->sample(
+			sampleLocationX->samples[i] * wavefield->numElems,
+			sampleLocationY->samples[i] * wavefield->numElems);
+	}
+}
+
+void AudioNodeWavefield2D::handleTrigger(const int inputSocketIndex, const AudioTriggerData & data)
+{
+	if (inputSocketIndex == kInput_Trigger)
+	{
+		const float triggerPositionX = getInputAudioFloat(kInput_TriggerLocationX, &AudioFloat::Half)->getMean();
+		const float triggerPositionY = getInputAudioFloat(kInput_TriggerLocationY, &AudioFloat::Half)->getMean();
+		const float triggerAmount = getInputAudioFloat(kInput_TriggerAmount, &AudioFloat::Half)->getMean();
+		
+		if (wavefield->numElems > 0)
+		{
+			const int elemX = int(std::round(std::abs(triggerPositionX) * wavefield->numElems)) % wavefield->numElems;
+			const int elemY = int(std::round(std::abs(triggerPositionY) * wavefield->numElems)) % wavefield->numElems;
+			
+			wavefield->d[elemX][elemY] += triggerAmount;
+		}
+	}
+	else if (inputSocketIndex == kInput_Randomize)
+	{
+		const double xRatio = random(0.0, 1.0 / 10.0);
+		const double yRatio = random(0.0, 1.0 / 10.0);
+		const double randomFactor = random(0.0, 1.0);
+		//const double cosFactor = random(0.0, 1.0);
+		const double cosFactor = 0.0;
+		const double perlinFactor = random(0.0, 1.0);
+		
+		for (int x = 0; x < wavefield->numElems; ++x)
+		{
+			for (int y = 0; y < wavefield->numElems; ++y)
+			{
+				wavefield->p[x][y] = 0.0;
+				wavefield->v[x][y] = 0.0;
+				wavefield->d[x][y] = 0.0;
+				
+				wavefield->f[x][y] = 1.0;
+				
+				wavefield->f[x][y] *= lerp<double>(1.0, random(0.f, 1.f), randomFactor);
+				wavefield->f[x][y] *= lerp<double>(1.0, (std::cos(x * xRatio + y * yRatio) + 1.0) / 2.0, cosFactor);
+				//wavefield->f[x][y] = 1.0 - std::pow(wavefield->f[x][y], 2.0);
+				
+				//wavefield->f[x][y] = 1.0 - std::pow(random(0.f, 1.f), 2.0) * (std::cos(x / 4.32) + 1.0)/2.0 * (std::cos(y / 3.21) + 1.0)/2.0;
+				wavefield->f[x][y] *= lerp<double>(1.0, scaled_octave_noise_2d(16, .4f, 1.f / 20.f, 0.f, 1.f, x, y), perlinFactor);
+			}
+		}
+	}
+}
+
