5516518f6d3bf5ac643ee220e21467b9b4e16daa
Marcel Smit <marcel303@gmail.com>
2017-07-24 13:48:52 +0200

    Added normalize node.

---
+++ users/marcel/4dworld/audioNodes/audioNodeNormalize.cpp
@@ -0,0 +1,85 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "audioNodeNormalize.h"
+#include "Noise.h"
+#include <cmath>
+
+AUDIO_NODE_TYPE(normalize, AudioNodeNormalize)
+{
+	typeName = "normalize";
+	
+	in("value", "audioValue");
+	in("level", "float", "1");
+	in("maxAmp", "float", "10");
+	in("decay", "float", "0.001");
+	out("result", "audioValue");
+}
+
+void AudioNodeNormalize::tick(const float dt)
+{
+	const AudioFloat * value = getInputAudioFloat(kInput_Value, &AudioFloat::Zero);
+	const float level = getInputFloat(kInput_Level, 1.f);
+	const float maxAmp = getInputFloat(kInput_MaxAmplification, 10.f);
+	const float decayPerMs = std::max(0.f, std::min(1.f, getInputFloat(kInput_Decay, .001f)));
+	
+	//
+
+	const float dtMs = 1000.f / SAMPLE_RATE;
+	const float retainPerSample = std::pow(1.f - decayPerMs, dtMs);
+
+	//
+	
+	if (level <= 0.f || maxAmp <= 0.f)
+	{
+		resultOutput.setScalar(0.f);
+	}
+	else
+	{
+		value->expand();
+
+		resultOutput.setVector();
+		
+		for (int i = 0; i < AUDIO_UPDATE_SIZE; ++i)
+		{
+			const float input = value->samples[i];
+
+			const float inputMag = std::fmaxf(level / maxAmp, std::fabsf(input));
+
+			if (inputMag > measuredMax)
+			{
+				measuredMax = inputMag;
+			}
+
+			const float output = input * level / measuredMax;
+
+			resultOutput.samples[i] = output;
+
+			measuredMax *= retainPerSample;
+		}
+	}
+}
---
+++ users/marcel/4dworld/audioNodes/audioNodeNormalize.h
@@ -0,0 +1,67 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "audioNodeBase.h"
+
+struct AudioNodeNormalize : AudioNodeBase
+{
+	enum Input
+	{
+		kInput_Value,
+		kInput_Level,
+		kInput_MaxAmplification,
+		kInput_Decay,
+		kInput_COUNT
+	};
+	
+	enum Output
+	{
+		kOutput_Result,
+		kOutput_COUNT
+	};
+	
+	float measuredMax;
+	
+	AudioFloat resultOutput;
+	
+	AudioNodeNormalize()
+		: AudioNodeBase()
+		, measuredMax(0.f)
+		, resultOutput()
+	{
+		resizeSockets(kInput_COUNT, kOutput_COUNT);
+		addInput(kInput_Value, kAudioPlugType_FloatVec);
+		addInput(kInput_Level, kAudioPlugType_Float);
+		addInput(kInput_MaxAmplification, kAudioPlugType_Float);
+		addInput(kInput_Decay, kAudioPlugType_Float);
+		addOutput(kOutput_Result, kAudioPlugType_FloatVec, &resultOutput);
+	}
+	
+	virtual void tick(const float dt) override;
+};
