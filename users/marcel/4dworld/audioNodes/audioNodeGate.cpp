327b824373dbec2a52843cac066477e809d2b2e9
Marcel Smit <marcel303@gmail.com>
2017-07-29 21:06:05 +0200

    Added gate node.

---
+++ users/marcel/4dworld/audioNodes/audioNodeGate.cpp
@@ -0,0 +1,76 @@
+/*
+Copyright (C) 2017 Marcel Smit
+marcel303@gmail.com
+https://www.facebook.com/marcel.smit981
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "audioNodeGate.h"
+
+AUDIO_NODE_TYPE(gate, AudioNodeGate)
+{
+	typeName = "gate";
+	
+	in("in", "audioValue");
+	in("open!", "trigger");
+	in("close!", "trigger");
+	out("out", "audioValue");
+}
+
+AudioNodeGate::AudioNodeGate()
+	: AudioNodeBase()
+	, isOpen(true)
+	, resultOutput()
+{
+	resizeSockets(kInput_COUNT, kOutput_COUNT);
+	addInput(kInput_Input, kAudioPlugType_FloatVec);
+	addInput(kInput_Open, kAudioPlugType_Trigger);
+	addInput(kInput_Close, kAudioPlugType_Trigger);
+	addOutput(kOutput_Output, kAudioPlugType_FloatVec, &resultOutput);
+}
+
+void AudioNodeGate::tick(const float dt)
+{
+	const AudioFloat * input = getInputAudioFloat(kInput_Input, &AudioFloat::Zero);
+
+	if (isOpen)
+	{
+		resultOutput.set(*input);
+	}
+	else
+	{
+		resultOutput.setScalar(0.f);
+	}
+}
+
+void AudioNodeGate::handleTrigger(const int inputSocketIndex, const AudioTriggerData & data)
+{
+	if (inputSocketIndex == kInput_Open)
+	{
+		isOpen = true;
+	}
+	else if (inputSocketIndex == kInput_Close)
+	{
+		isOpen = false;
+	}
+}
