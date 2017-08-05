8188c1ea2c7f22410a03b7f4ed3da9a7077f8caa
Marcel Smit <marcel303@gmail.com>
2017-08-05 12:06:03 +0200

    Added ramp node.

---
+++ users/marcel/4dworld/audioNodes/audioNodeRamp.cpp
@@ -0,0 +1,114 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "audioNodeRamp.h"
+
+AUDIO_NODE_TYPE(ramp, AudioNodeRamp)
+{
+	typeName = "ramp";
+	
+	in("startRamped", "bool", "1");
+	in("value", "audioValue", "1");
+	in("rampTime", "audioValue", "1");
+	in("rampUp!", "trigger");
+	in("rampDown!", "trigger");
+	out("value", "audioValue");
+	out("rampedUp!", "trigger");
+	out("rampedDown!", "trigger");
+}
+
+void AudioNodeRamp::tick(const float dt)
+{
+	const AudioFloat * input = getInputAudioFloat(kInput_Value, &AudioFloat::One);
+	const AudioFloat * rampTime = getInputAudioFloat(kInput_RampTime, &AudioFloat::One);
+	
+	if (isPassthrough)
+	{
+		valueOutput.setScalar(0.f);
+	}
+	else
+	{
+		input->expand();
+		rampTime->expand();
+		
+		//
+		
+		if (ramp == true && value == 1.0)
+			valueOutput.set(*input);
+		else if (ramp == false && value == 0.0)
+			valueOutput.setZero();
+		else
+		{
+			valueOutput.setVector();
+			
+			for (int i = 0; i < AUDIO_UPDATE_SIZE; ++i)
+			{
+				const double step = 1.0 / (double(SAMPLE_RATE) * rampTime->samples[i]);
+				
+				if (ramp)
+					value = std::min(1.0, value + step);
+				else
+					value = std::max(0.0, value - step);
+				
+				valueOutput.samples[i] = input->samples[i] * value;
+			}
+			
+			if (ramp == true && value == 1.0)
+				trigger(kOutput_RampedUp);
+			else if (ramp == false && value == 0.0)
+				trigger(kOutput_RampedDown);
+		}
+	}
+}
+
+void AudioNodeRamp::init(const GraphNode & node)
+{
+	const bool startRamped = getInputBool(kInput_StartRamped, true);
+	
+	if (startRamped)
+	{
+		ramp = true;
+		value = 1.0;
+	}
+	else
+	{
+		ramp = false;
+		value = 0.0;
+	}
+}
+
+void AudioNodeRamp::handleTrigger(const int inputSocketIndex, const AudioTriggerData & data)
+{
+	if (inputSocketIndex == kInput_RampUp)
+	{
+		ramp = true;
+	}
+	else if (inputSocketIndex == kInput_RampDown)
+	{
+		ramp = false;
+	}
+}
---
+++ users/marcel/4dworld/audioNodes/audioNodeRamp.h
@@ -0,0 +1,81 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "audioNodeBase.h"
+
+struct AudioNodeRamp : AudioNodeBase
+{
+	enum Input
+	{
+		kInput_StartRamped,
+		kInput_Value,
+		kInput_RampTime,
+		kInput_RampUp,
+		kInput_RampDown,
+		kInput_COUNT
+	};
+	
+	enum Output
+	{
+		kOutput_Value,
+		kOutput_RampedUp,
+		kOutput_RampedDown,
+		kOutput_COUNT
+	};
+	
+	bool ramp;
+	double value;
+	
+	AudioFloat valueOutput;
+	AudioTriggerData dummyTriggerData;
+	
+	AudioNodeRamp()
+		: AudioNodeBase()
+		, ramp(false)
+		, value(0.0)
+		, valueOutput()
+		, dummyTriggerData()
+	{
+		resizeSockets(kInput_COUNT, kOutput_COUNT);
+		addInput(kInput_StartRamped, kAudioPlugType_Bool);
+		addInput(kInput_Value, kAudioPlugType_FloatVec);
+		addInput(kInput_RampTime, kAudioPlugType_FloatVec);
+		addInput(kInput_RampUp, kAudioPlugType_Trigger);
+		addInput(kInput_RampDown, kAudioPlugType_Trigger);
+		addOutput(kOutput_Value, kAudioPlugType_FloatVec, &valueOutput);
+		addOutput(kOutput_RampedUp, kAudioPlugType_Trigger, &dummyTriggerData);
+		addOutput(kOutput_RampedDown, kAudioPlugType_Trigger, &dummyTriggerData);
+	}
+	
+	virtual void tick(const float dt) override;
+	
+	virtual void init(const GraphNode & node) override;
+
+	virtual void handleTrigger(const int inputSocketIndex, const AudioTriggerData & data) override;
+};
