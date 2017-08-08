29c4afefdb216df18ea6dddf1726a94a32ee480f
Marcel Smit <marcel303@gmail.com>
2017-08-08 14:23:49 +0200

    CIPIC sample duplication to fill the holes in the 2D elevation & azimuth map used for Delaunay triangulation.

--- users/marcel/4dworld/objects/binaural_cipic.cpp
+++ users/marcel/4dworld/objects/binaural_cipic.cpp
@@ -1,3 +1,30 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "binaural.h"
 #include "binaural_cipic.h"
 #include <map>
@@ -197,15 +224,28 @@ namespace binaural
 				
 				const float elevation = -45 + 5.625 * i;
 				
+			#if 1
 				float x, y, z;
 				elevationAndAzimuthToCartesian(elevation, azimuth, x, y, z);
 				float newElevation;
 				float newAzimuth;
 				cartesianToElevationAndAzimuth(x, y, z, newElevation, newAzimuth);
+			#else
+				float newElevation = elevation;
+				float newAzimuth = azimuth;
+			#endif
 				
 				if (sampleSet.addHrirSampleFromSoundData(soundData, newElevation, newAzimuth, false))
 				{
 					numAdded++;
+				}
+				
+				if (i == 0 || ((newElevation > -51.f && newElevation < -48.f) && (newAzimuth < -80 || newAzimuth > +80)))
+				{
+					if (sampleSet.addHrirSampleFromSoundData(soundData, -90, newAzimuth, false))
+					{
+						numAdded++;
+					}
 				}
 				
 				soundData.sampleData = nullptr;
