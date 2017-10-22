c7de5be41ae3d105cf412c663c2896612a059f81
Marcel Smit <marcel303@gmail.com>
2017-10-22 12:47:56 +0200

    Fix for incorrect flag value ToggleIsFolded.

--- users/marcel/avgraph/graph.h
+++ users/marcel/avgraph/graph.h
@@ -911,7 +911,7 @@ struct GraphEdit : GraphEditConnection
 		kFlag_Drag = 1 << 8,
 		kFlag_Zoom = 1 << 9,
 		kFlag_ToggleIsPassthrough = 1 << 10,
-		kFlag_ToggleIsFolded = 1 << 1,
+		kFlag_ToggleIsFolded = 1 << 11,
 		kFlag_All = ~0
 	};
 	
