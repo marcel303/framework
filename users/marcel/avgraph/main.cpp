20791a3e62654bf145aba00b17adb6cac0f4420d
Marcel Smit <marcel303@gmail.com>
2017-10-24 21:50:47 +0200

    HQ primitive gradient and texture test.

--- users/marcel/avgraph/main.cpp
+++ users/marcel/avgraph/main.cpp
@@ -260,11 +260,39 @@ VFX_NODE_TYPE(resource_test, VfxNodeResourceTest)
 
 //
 
+#include "Noise.h"
+
 static void testGradientShader()
 {
+	const int kNumCircles = 32;
+	
+	Vec3 circles[kNumCircles];
+	
+	for (int i = 0; i < kNumCircles; ++i)
+	{
+		circles[i][0] = random(0, GFX_SX);
+		circles[i][1] = random(0, GFX_SY);
+		circles[i][2] = random(0.f, 5.f); // life
+	}
+	
 	do
 	{
 		framework.process();
+		
+		for (int i = 0; i < kNumCircles; ++i)
+		{
+			circles[i][2] -= framework.timeStep;
+			
+			if (circles[i][2] <= 0.f)
+			{
+				circles[i][0] = random(0, GFX_SX);
+				circles[i][1] = random(0, GFX_SY);
+				circles[i][2] = random(0.f, 5.f); // life
+			}
+			
+			circles[i][0] += scaled_octave_noise_3d(4, .5f, .01f, -500.f, +500.f, i, circles[i][0], circles[i][1]) * framework.timeStep;
+			circles[i][1] += scaled_octave_noise_3d(4, .5f, .01f, -500.f, +500.f, -i, circles[i][0], circles[i][1]) * framework.timeStep;
+		}
 		
 		framework.beginDraw(0, 0, 0, 0);
 		{
@@ -274,28 +302,38 @@ static void testGradientShader()
 				.Translate(-GFX_SX/2, -GFX_SY/2, 0);
 			const Mat4x4 tmat = Mat4x4(true)
 				.Translate(.5f, .5f, 0.f)
-				.Scale(1.f / 100.f, 1.f / 100.f, 1.f)
+				.Scale(1.f / 400.f, 1.f / 400.f, 1.f)
 				.RotateZ(+framework.time)
 				.Translate(-GFX_SX/2, -GFX_SY/2, 0);
 			
-			const float innerRadius = mouse.x / float(GFX_SX);
-			const float colorGradientType = mouse.isDown(BUTTON_LEFT) ? 2.f : 1.f;
-			
-			Shader shader("test-gradient");
-			setShader(shader);
-			shader.setImmediate("cmat1", cmat(0, 0), cmat(0, 1), cmat(0, 2), cmat(0, 3));
-			shader.setImmediate("cmat2", cmat(1, 0), cmat(1, 1), cmat(1, 2), cmat(1, 3));
-			shader.setImmediate("cmat3", cmat(2, 0), cmat(2, 1), cmat(2, 2), cmat(2, 3));
-			shader.setImmediate("cmat4", cmat(3, 0), cmat(3, 1), cmat(3, 2), cmat(3, 3));
-			shader.setImmediate("tmat1", tmat(0, 0), tmat(0, 1), tmat(0, 2), tmat(0, 3));
-			shader.setImmediate("tmat2", tmat(1, 0), tmat(1, 1), tmat(1, 2), tmat(1, 3));
-			shader.setImmediate("tmat3", tmat(2, 0), tmat(2, 1), tmat(2, 2), tmat(2, 3));
-			shader.setImmediate("tmat4", tmat(3, 0), tmat(3, 1), tmat(3, 2), tmat(3, 3));
-			shader.setImmediate("gradientParams", colorGradientType, innerRadius, 0, 1);
-			shader.setImmediate("color1", 1, 1, 1, 1);
-			shader.setImmediate("color2", 0, 0, 1, 1);
-			shader.setTexture("source", 0, getTexture("happysun.jpg"));
-			drawRect(0, 0, GFX_SX, GFX_SY);
+			const GRADIENT_TYPE gradientType = mouse.isDown(BUTTON_LEFT) ? GRADIENT_RADIAL : GRADIENT_LINEAR;
+			const float gradientBias = mouse.x / float(GFX_SX);
+			const float gradientScale = gradientBias == 1.f ? 0.f : 1.f / (1.f - gradientBias);
+			hqSetGradient(gradientType, cmat, colorWhite, colorBlack, gradientBias, gradientScale);
+			hqSetTexture(tmat, getTexture("happysun.jpg"));
+			for (int i = 0; i < 360; i += 20)
+			{
+				gxPushMatrix();
+				gxRotatef(i + framework.time, 0, 0, 1);
+				hqBegin(HQ_LINES);
+				{
+					setColor(colorWhite);
+					hqLine(0, 0, 10, GFX_SX, GFX_SY, 50);
+				}
+				hqEnd();
+				gxPopMatrix();
+			}
+			hqBegin(HQ_FILLED_CIRCLES);
+			{
+				for (int i = 0; i < kNumCircles; ++i)
+				{
+					gxColor4f(1, 1, 1, Calc::Clamp(circles[i][2], 0.f, 1.f));
+					hqFillCircle(circles[i][0], circles[i][1], 20.f);
+				}
+			}
+			hqEnd();
+			hqClearGradient();
+			hqClearTexture();
 		}
 		framework.endDraw();
 		
