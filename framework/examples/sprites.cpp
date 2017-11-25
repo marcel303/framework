e2af037600c4f196111b138dd2ab4cf31a2dc128
Marcel Smit <marcel303@gmail.com>
2017-11-25 20:50:57 +0100

    Updated sprite test.

--- framework/examples/sprites.cpp
+++ framework/examples/sprites.cpp
@@ -33,8 +33,6 @@
 
 int main(int argc, char * argv[])
 {
-	changeDirectory("data");
-	
 	framework.fullscreen = false;
 	if (!framework.init(argc, (const char**)argv, VIEW_SX, VIEW_SY))
 		return -1;
