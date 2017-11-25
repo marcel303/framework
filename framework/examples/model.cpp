a28abd65d61a3e5bd285877c0395ae8bb3ed22dc
Marcel Smit <marcel303@gmail.com>
2017-11-25 20:53:32 +0100

    Updated model test. - Use SDF font rendering. - Added on-screen instructions. - Fix compile issue. - Make sure enableDepthBuffer is true. - Tweaked window size. - Changed log to logDebug. - Added S key to toggle stress test mode (7x7x7 models). - Changed F1-F4 keys to 1-4 since on Macbooks the function keys aren't so readily accessible. - Removed ugly OpenGX test triangles.

--- framework/examples/model.cpp
+++ framework/examples/model.cpp
@@ -26,13 +26,12 @@
 */
 
 #include "framework.h"
-#include "internal.h"
 
 #include <time.h>
 static int getTimeUS() { return clock() * 1000000 / CLOCKS_PER_SEC; }
 
-#define VIEW_SX (1600/2)
-#define VIEW_SY (1600/2)
+#define VIEW_SX 1200
+#define VIEW_SY 600
 
 #if 0
 	#define X1 0
@@ -73,13 +72,16 @@ public:
 
 int main(int argc, char * argv[])
 {
-	changeDirectory("data");
+	framework.enableDepthBuffer = true;
 	
-	framework.fullscreen = false;
 	if (!framework.init(argc, (const char **)argv, VIEW_SX, VIEW_SY))
 		return -1;
 	
+	pushFontMode(FONT_SDF);
+	
 	std::map<CoordKey, Model*> models;
+	
+	logDebug("loading model");
 	
 	for (int x = X1; x <= X2; ++x)
 	{
@@ -100,9 +102,10 @@ int main(int argc, char * argv[])
 	const std::vector<std::string> animList = models[CoordKey(0, 0, 0)]->getAnimList();
 	
 	for (size_t i = 0; i < animList.size(); ++i)
-		log("anim: %s", animList[i].c_str());
+		logDebug("anim: %s", animList[i].c_str());
 	
 	int drawFlags = DrawMesh | DrawColorNormals;
+	bool stressTest = false;
 	bool wireframe = false;
 	bool rotate = false;
 	bool loop = false;
@@ -117,6 +120,8 @@ int main(int argc, char * argv[])
 		
 		bool startRandomAnim = false;
 		
+		if (keyboard.wentDown(SDLK_s))
+			stressTest = !stressTest;
 		if (keyboard.wentDown(SDLK_w))
 			wireframe = !wireframe;
 		if (keyboard.wentDown(SDLK_b))
@@ -125,13 +130,13 @@ int main(int argc, char * argv[])
 			drawFlags = drawFlags ^ DrawPoseMatrices;
 		if (keyboard.wentDown(SDLK_n))
 			drawFlags = drawFlags ^ DrawNormals;
-		if (keyboard.wentDown(SDLK_F1))
+		if (keyboard.wentDown(SDLK_1))
 			drawFlags ^= DrawColorBlendWeights;
-		if (keyboard.wentDown(SDLK_F2))
+		if (keyboard.wentDown(SDLK_2))
 			drawFlags ^= DrawColorBlendIndices;
-		if (keyboard.wentDown(SDLK_F3))
+		if (keyboard.wentDown(SDLK_3))
 			drawFlags ^= DrawColorTexCoords;
-		if (keyboard.wentDown(SDLK_F4))
+		if (keyboard.wentDown(SDLK_4))
 			drawFlags ^= DrawColorNormals;
 		if (keyboard.wentDown(SDLK_SPACE))
 			startRandomAnim = true;
@@ -142,9 +147,9 @@ int main(int argc, char * argv[])
 		if (keyboard.wentDown(SDLK_p))
 			autoPlay = !autoPlay;
 		if (keyboard.wentDown(SDLK_UP))
-			animSpeed *= 2.f;
+			animSpeed *= 1.5f;
 		if (keyboard.wentDown(SDLK_DOWN))
-			animSpeed /= 2.f;
+			animSpeed /= 1.5f;
 		
 		for (int x = X1; x <= X2; ++x)
 		{
@@ -167,6 +172,9 @@ int main(int argc, char * argv[])
 							{
 								for (int z = Z1; z <= Z2; ++z)
 								{
+									if (stressTest == false && (x != 0 || y != 0 || z != 0))
+										continue;
+									
 									const std::string & name = animList[rand() % animList.size()];
 									
 									model->startAnim(name.c_str(), loop ? -1 : 1);
@@ -187,13 +195,13 @@ int main(int argc, char * argv[])
 		// set 3D transform
 		
 		const float fov = 90.f * M_PI / 180.f;
-		const float aspect = 1.f;
+		const float aspect = VIEW_SY / float(VIEW_SX);
 		
 		Mat4x4 transform3d;
 		transform3d.MakePerspectiveGL(fov, aspect, .1f, +2000.f);
 		setTransform3d(transform3d);
 		
-		framework.beginDraw(31, 0, 0, 0);
+		framework.beginDraw(31, 31, 31, 0);
 		{
 			glClearDepth(1.f);
 			glClear(GL_DEPTH_BUFFER_BIT);
@@ -204,6 +212,7 @@ int main(int argc, char * argv[])
 			
 			glDepthFunc(GL_LESS);
 			glEnable(GL_DEPTH_TEST);
+			checkErrorGL();
 			
 			glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
 			checkErrorGL();
@@ -212,11 +221,9 @@ int main(int argc, char * argv[])
 			{
 				gxTranslatef(0.f, -100.f, 600.f);
 				gxRotatef(angle, 0.f, 1.f, 0.f);
-				checkErrorGL();
 				
-				setBlend(BLEND_OPAQUE);
+				pushBlend(BLEND_OPAQUE);
 				setColor(255, 255, 255);
-				checkErrorGL();
 				
 				int time = 0;
 				time -= getTimeUS();
@@ -227,6 +234,9 @@ int main(int argc, char * argv[])
 					{
 						for (int z = Z1; z <= Z2; ++z)
 						{
+							if (stressTest == false && (x != 0 || y != 0 || z != 0))
+								continue;
+					
 							Model * model = models[CoordKey(x, y, z)];
 							
 							model->draw(drawFlags);
@@ -235,37 +245,64 @@ int main(int argc, char * argv[])
 				}
 				
 				time += getTimeUS();
-				log("draw took %.2fms", time / 1000.f);
+				logDebug("draw took %.2fms", time / 1000.f);
 				
-				Font font("calibri.ttf");
-				setFont(font);
-				setColor(255, 255, 0);
+				setFont("calibri.ttf");
+				setColor(200, 200, 200);
 				const Vec2 s = transformToScreen(Vec3(0.f, 0.f, 0.f));
-				debugDrawText(s[0], s[1], 18, 0, 0, "root");
+				debugDrawText(s[0], s[1], 14, 0, 0, "root");
 				setColor(255, 255, 255);
+				
+				popBlend();
 			}
 			gxPopMatrix();
-			glDisable(GL_DEPTH_TEST);
 			
-			// OpenGX (tm)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			checkErrorGL();
+			
+			glDisable(GL_DEPTH_TEST);
+			checkErrorGL();
+			
+			// show instructions
 			
 			setTransform(TRANSFORM_SCREEN);
 			
-			gxColor4f(1.f, 1.f, 1.f, 1.f);
-			gxBegin(GL_TRIANGLES);
-			{
-				gxVertex2f(0.f, 0.f);
-				gxVertex2f(100.f, 100.f);
-				gxVertex2f(0.f, 100.f);
-				gxVertex2f(200.f, 0.f);
-				gxVertex2f(200.f, 100.f);
-				gxVertex2f(100.f, 100.f);
-			}
-			gxEnd();
+			gxTranslatef(11, 11, 0);
+			
+			setColor(0, 0, 0, 180);
+			hqBegin(HQ_FILLED_ROUNDED_RECTS);
+			hqFillRoundedRect(0, 0, 270, 276, 14);
+			hqEnd();
+			
+			setFont("calibri.ttf");
+			setColor(colorWhite);
+			
+			const int fontSize = 14;
+			const int incrementY = fontSize + 2;
+			int x = 17;
+			int y = 17;
+			
+			y -= incrementY;
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "S: stress test [%s]", stressTest ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "W: wireframe [%s]", wireframe ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "B: draw bones [%s]", (drawFlags & DrawBones) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "M: draw pose matrices [%s]", (drawFlags & DrawPoseMatrices) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "N: draw normals [%s]", (drawFlags & DrawNormals) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "1: color blend weights [%s]", (drawFlags & DrawColorBlendWeights) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "2: color blend indices [%s]", (drawFlags & DrawColorBlendIndices) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "3: color texture coordinates [%s]", (drawFlags & DrawColorTexCoords) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "4: color normals [%s]", (drawFlags & DrawColorNormals) ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "SPACE: trigger a random animation");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "A: rotate view [%s]", rotate ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "L: loop animations [%s]", loop ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "P: auto play animations [%s]", autoPlay ? "on" : "off");
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "UP: increase animation speed [%.2f]", animSpeed);
+			y += incrementY; drawText(x, y, fontSize, +1, +1, "DOWN: decrease animation speed [%.2f]", animSpeed);
 		}
 		framework.endDraw();
 	}
 	
+	popFontMode();
 	framework.shutdown();
 	
 	return 0;
