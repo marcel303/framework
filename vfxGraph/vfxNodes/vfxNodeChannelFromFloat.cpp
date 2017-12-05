72138f6f498a1ca3c429ab0c82c51b743773138c
Marcel Smit <marcel303@gmail.com>
2017-12-05 21:00:53 +0100

    Added channel.fromFloat and channel.literal vfx nodes.

---
+++ vfxGraph/vfxNodes/vfxNodeChannelFromFloat.cpp
@@ -0,0 +1,76 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "vfxNodeBase.h"
+
+struct VfxNodeChannelFromFloat : VfxNodeBase
+{
+	enum Input
+	{
+		kInput_Value1,
+		kInput_COUNT
+	};
+	
+	enum Output
+	{
+		kOutput_Channels,
+		kOutput_COUNT
+	};
+	
+	VfxChannelData channelData;
+	
+	VfxChannels channelOutput;
+	
+	VfxNodeChannelFromFloat()
+	{
+		resizeSockets(kInput_COUNT, kOutput_COUNT);
+		addInput(kInput_Value1, kVfxPlugType_Float);
+		addOutput(kOutput_Channels, kVfxPlugType_Channels, &channelOutput);
+	}
+	
+	virtual void tick(const float dt) override
+	{
+		const float value1 = getInputFloat(kInput_Value1, 0.f);
+		
+		if (true)
+		{
+			channelData.allocOnSizeChange(1);
+			
+			channelData.data[0] = value1;
+			
+			channelOutput.setDataContiguous(channelData.data, false, channelData.size, 1);
+		}
+	}
+};
+
+VFX_NODE_TYPE(VfxNodeChannelFromFloat)
+{
+	typeName = "channel.fromFloat";
+	
+	in("value1", "float");
+	out("channels", "channels");
+}
---
+++ vfxGraph/vfxNodes/vfxNodeChannelLiteral.cpp
@@ -0,0 +1,93 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "Parse.h"
+#include "vfxNodeBase.h"
+
+extern void splitString(const std::string & str, std::vector<std::string> & result, char c);
+
+struct VfxNodeChannelLiteral : VfxNodeBase
+{
+	enum Input
+	{
+		kInput_Text,
+		kInput_COUNT
+	};
+	
+	enum Output
+	{
+		kOutput_Channels,
+		kOutput_COUNT
+	};
+	
+	VfxChannelData channelData;
+	
+	VfxChannels channelOutput;
+	
+	VfxNodeChannelLiteral()
+	{
+		resizeSockets(kInput_COUNT, kOutput_COUNT);
+		addInput(kInput_Text, kVfxPlugType_String);
+		addOutput(kOutput_Channels, kVfxPlugType_Channels, &channelOutput);
+	}
+	
+	virtual void tick(const float dt) override
+	{
+		const char * text = getInputString(kInput_Text, nullptr);
+		
+		if (text == nullptr)
+		{
+			channelData.free();
+			
+			channelOutput.reset();
+		}
+		else
+		{
+			std::vector<std::string> parts;
+			splitString(text, parts, ' ');
+			
+			channelData.allocOnSizeChange(parts.size());
+			
+			for (size_t i = 0; i < parts.size(); ++i)
+			{
+				const float value = Parse::Float(parts[i]);
+				
+				channelData.data[i] = value;
+			}
+			
+			channelOutput.setDataContiguous(channelData.data, false, channelData.size, 1);
+		}
+	}
+};
+
+VFX_NODE_TYPE(VfxNodeChannelLiteral)
+{
+	typeName = "channel.literal";
+	
+	in("text", "string");
+	out("channels", "channels");
+}
