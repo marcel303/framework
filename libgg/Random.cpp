2bd9680f8f2637e86e7225c78e694572675e3b5b
Marcel Smit <marcel303@gmail.com>
2017-12-25 22:04:11 +0100

    - Added Mersenne Twister RNG object to Random. - Moved PinkNumber object from audio graph to Random.

--- libgg/Random.cpp
+++ libgg/Random.cpp
@@ -26,15 +26,77 @@ namespace RNG
 	}
 	
 	// ----------------------------------------
+	// Mersenne Twister object implementation
+	// ----------------------------------------
+	
+	#define MT_MATRIX_A 0x9908b0dfUL   /* constant vector a */
+	#define MT_UPPER_MASK 0x80000000UL /* most significant w-r bits */
+	#define MT_LOWER_MASK 0x7fffffffUL /* least significant r bits */
+	
+	void MersenneTwister::init(const uint32_t seed)
+	{
+		mt[0] = seed & 0xffffffffUL;
+
+		for (mti = 1; mti < MT_N; mti++)
+		{
+			mt[mti] = (1812433253UL * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti);
+
+			/* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+			/* In the previous versions, MSBs of the seed affect   */
+			/* only MSBs of the array mt[].                        */
+			/* 2002/01/09 modified by Makoto Matsumoto             */
+			mt[mti] &= 0xffffffffUL;
+			/* for >32 bit machines */
+		}
+	}
+	
+	uint32_t MersenneTwister::next()
+	{
+		uint32_t y;
+		const uint32_t mag01[2] = { 0x0UL, MT_MATRIX_A };
+		/* mag01[x] = x * MATRIX_A  for x=0,1 */
+	
+		if (mti >= MT_N) { /* generate N words at one time */
+			int kk;
+	
+//			if (mti == MT_N + 1)   /* if init_genrand() has not been called, */
+//				Rand_MT_Initialize(5489UL); /* a default initial seed is used */
+	
+			for (kk = 0; kk < MT_N - MT_M; ++kk)
+			{
+				y = (mt[kk] & MT_UPPER_MASK) | (mt[kk + 1] & MT_LOWER_MASK);
+				mt[kk] = mt[kk + MT_M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+			}
+			for (; kk < MT_N - 1; ++kk)
+			{
+				y = (mt[kk] & MT_UPPER_MASK) | (mt[kk + 1] & MT_LOWER_MASK);
+				mt[kk] = mt[kk + (MT_M - MT_N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+			}
+			
+			y = (mt[MT_N - 1] & MT_UPPER_MASK) | (mt[0] & MT_LOWER_MASK);
+			mt[MT_N - 1] = mt[MT_M - 1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+	
+			mti = 0;
+		}
+		
+		y = mt[mti++];
+	
+		/* Tempering */
+		y ^= (y >> 11);
+		y ^= (y << 7) & 0x9d2c5680UL;
+		y ^= (y << 15) & 0xefc60000UL;
+		y ^= (y >> 18);
+	
+		return y;
+	}
+	
+	// ----------------------------------------
 	// Mersenne Twister implementation
 	// ----------------------------------------
 
 	/* Period parameters */  
 	#define MT_N 624
 	#define MT_M 397
-	#define MT_MATRIX_A 0x9908b0dfUL   /* constant vector a */
-	#define MT_UPPER_MASK 0x80000000UL /* most significant w-r bits */
-	#define MT_LOWER_MASK 0x7fffffffUL /* least significant r bits */
 	
 	static uint32_t mt[MT_N]; /* the array for the state vector  */
 	static int mti = MT_N + 1; /* mti==N+1 means mt[N] is not initialized */
@@ -292,5 +354,52 @@ namespace RNG
 		}
 		
 		return result;
+	}
+	
+	//
+	
+	PinkNumber::PinkNumber(const int _range)
+	{
+		rng.init(rand());
+		
+		maxKey = 0x1f; // five bits set
+		
+		range = _range;
+		key = 0;
+		
+		for (int i = 0; i < 5; ++i)
+		{
+			whiteValues[i] = rng.next() % (range/5);
+		}
+	}
+
+	int PinkNumber::next()
+	{
+		const int lastKey = key;
+
+		key++;
+		
+		if (key > maxKey)
+			key = 0;
+		
+		// exclusive-or previous value with current value. this gives a list of bits that have changed
+		
+		int diff = lastKey ^ key;
+		
+		int sum = 0;
+		
+		for (int i = 0; i < 5; ++i)
+		{
+			// if bit changed get new random number for corresponding whiteValue
+			
+			if (diff & (1 << i))
+			{
+				whiteValues[i] = rand() % (range/5);
+			}
+			
+			sum += whiteValues[i];
+		}
+		
+		return sum;
 	}
 }
--- libgg/Random.h
+++ libgg/Random.h
@@ -4,6 +4,9 @@
 
 namespace RNG
 {
+	// todo : make thread-safe random number generators. document fixed time or with occasional seed time here
+	// todo : remove the old callback based versions
+	
 	typedef void (*RandCB_Initialize)(uint32_t seed);
 	typedef uint32_t (*RandCB_Next)();
 	
@@ -24,4 +27,41 @@ namespace RNG
 	void Rand_Initialize(uint32_t seed);
 	
 	Random Rand_Create(RandomType type);
+	
+	// Mersenne Twister RNG. good distribution. requires some time to initialize. okay next() speed
+	
+	struct MersenneTwister
+	{
+		static const int MT_N = 624;
+		static const int MT_M = 397;
+		
+		uint32_t mt[MT_N]; /* the array for the state vector  */
+		int mti = MT_N + 1; /* mti==N+1 means mt[N] is not initialized */
+		
+		void init(const uint32_t seed);
+		uint32_t next();
+		
+		float nextf(const float min, const float max)
+		{
+			const float t = (next() % 1000) / 999.f;
+			
+			return min * t + max * (1.f - t);
+		}
+	};
+	
+	// Voss' pink number object to be used for pink noise generation
+
+	struct PinkNumber
+	{
+		RNG::MersenneTwister rng;
+		
+		int maxKey;
+		int key;
+		int whiteValues[5];
+		int range;
+		
+		PinkNumber(const int _range = 128);
+		
+		int next();
+	};
 }
