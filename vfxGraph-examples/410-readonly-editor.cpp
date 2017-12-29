000f91fae5bd10dc0fcd96c5364615b7e4c73aad
Marcel Smit <marcel303@gmail.com>
2017-12-29 14:55:45 +0100

    Added new vfx graph examples: How to use the editor, how to use the editor in read-only mode, and an example showing real-time editing using vfx graph in action.

---
+++ vfxGraph-examples/410-readonly-editor.cpp
@@ -0,0 +1,84 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "framework.h"
+#include "graph.h"
+#include "vfxGraph.h"
+#include "../libparticle/ui.h"
+
+#define FILENAME "testRibbon2.xml"
+
+extern const int GFX_SX;
+extern const int GFX_SY;
+
+const int GFX_SX = 1024;
+const int GFX_SY = 768;
+
+//
+
+int main(int argc, char * argv[])
+{
+	framework.enableRealTimeEditing = true;
+	
+	if (framework.init(0, nullptr, GFX_SX, GFX_SY))
+	{
+		initUi();
+		
+		GraphEdit_TypeDefinitionLibrary typeDefinitionLibrary;
+		createVfxTypeDefinitionLibrary(typeDefinitionLibrary);
+		
+		GraphEdit graphEdit(GFX_SX, GFX_SY, &typeDefinitionLibrary);
+		graphEdit.load(FILENAME);
+		
+		// only allow navigation and selecting nodes and links
+		graphEdit.flags = GraphEdit::kFlag_Drag | GraphEdit::kFlag_Zoom | GraphEdit::kFlag_Select;
+
+		while (!framework.quitRequested)
+		{
+			framework.process();
+
+			if (keyboard.wentDown(SDLK_ESCAPE))
+				framework.quitRequested = true;
+			
+			const float timeStep = std::min(framework.timeStep, 1.f / 15.f);
+
+			graphEdit.tick(timeStep, false);
+			
+			framework.beginDraw(0, 0, 0, 0);
+			{
+				graphEdit.draw();
+			}
+			framework.endDraw();
+		}
+		
+		shutUi();
+		
+		framework.shutdown();
+	}
+
+	return 0;
+}
---
+++ vfxGraph-examples/420-realtime-editing.cpp
@@ -0,0 +1,98 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "framework.h"
+#include "graph.h"
+#include "vfxGraph.h"
+#include "vfxGraphRealTimeConnection.h"
+#include "../libparticle/ui.h"
+
+#define FILENAME "testRibbon2.xml"
+
+extern const int GFX_SX;
+extern const int GFX_SY;
+
+const int GFX_SX = 1024;
+const int GFX_SY = 768;
+
+//
+
+int main(int argc, char * argv[])
+{
+	framework.enableRealTimeEditing = true;
+	
+	if (framework.init(0, nullptr, GFX_SX, GFX_SY))
+	{
+		initUi();
+		
+		GraphEdit_TypeDefinitionLibrary typeDefinitionLibrary;
+		createVfxTypeDefinitionLibrary(typeDefinitionLibrary);
+		
+		VfxGraph * vfxGraph = nullptr;
+		
+		RealTimeConnection realTimeConnection(vfxGraph);
+		
+		GraphEdit graphEdit(GFX_SX, GFX_SY, &typeDefinitionLibrary, &realTimeConnection);
+		graphEdit.load(FILENAME);
+		
+		// exclude saving and loading
+		graphEdit.flags &= ~(GraphEdit::kFlag_SaveLoad);
+
+		while (!framework.quitRequested)
+		{
+			framework.process();
+
+			if (keyboard.wentDown(SDLK_ESCAPE))
+				framework.quitRequested = true;
+			
+			const float timeStep = std::min(framework.timeStep, 1.f / 15.f);
+
+			graphEdit.tick(timeStep, false);
+			
+			vfxGraph->tick(GFX_SX, GFX_SY, timeStep);
+			
+			framework.beginDraw(0, 0, 0, 0);
+			{
+				vfxGraph->draw(GFX_SX, GFX_SY);
+				
+				graphEdit.tickVisualizers(timeStep);
+				
+				graphEdit.draw();
+			}
+			framework.endDraw();
+		}
+		
+		delete vfxGraph;
+		vfxGraph = nullptr;
+		
+		shutUi();
+		
+		framework.shutdown();
+	}
+
+	return 0;
+}
