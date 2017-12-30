3552472c74164e986b105fb2ec5cfdd46c504606
Marcel Smit <marcel303@gmail.com>
2017-12-30 15:10:43 +0100

    Converted RNG code into RNG objects, to allow them to be used thread-safely and in parallel.

--- libgg/Random.h
+++ libgg/Random.h
@@ -4,31 +4,46 @@
 
 namespace RNG
 {
-	// todo : make thread-safe random number generators. document fixed time or with occasional seed time here
-	// todo : remove the old callback based versions
+	// Trivial RNG. poor distribution but super fast
 	
-	typedef void (*RandCB_Initialize)(uint32_t seed);
-	typedef uint32_t (*RandCB_Next)();
-	
-	typedef struct Random
+	struct Trivial
 	{
-		RandCB_Initialize Initialize;
-		RandCB_Next Next;
-	} Random;
-	
-	enum RandomType
-	{
-		RandomType_Trivial,
-		RandomType_MT,
-		RandomType_XORSHIFT,
-		RandomType_R250_521
+		uint32_t value;
+		
+		//
+		
+		Trivial()
+		{
+			init(rand());
+		}
+		
+		Trivial(const uint32_t seed)
+		{
+			init(seed);
+		}
+		
+		void init(const uint32_t seed)
+		{
+			value = seed;
+		}
+		
+		uint32_t next()
+		{
+			value += 1;
+			value *= 16807;
+
+			return value;
+		}
+		
+		float nextf(const float min, const float max)
+		{
+			const float t = (next() % 1000) / 999.f;
+			
+			return min * t + max * (1.f - t);
+		}
 	};
 	
-	void Rand_Initialize(uint32_t seed);
-	
-	Random Rand_Create(RandomType type);
-	
-	// Mersenne Twister RNG. good distribution. requires some time to initialize. okay next() speed
+	// Mersenne Twister RNG. good distribution. requires some time to initialize. okay speed, but needs to repopulate table from time to time, resulting in a not perfectly smooth running time
 	
 	struct MersenneTwister
 	{
@@ -38,8 +53,103 @@ namespace RNG
 		uint32_t mt[MT_N]; /* the array for the state vector  */
 		int mti = MT_N + 1; /* mti==N+1 means mt[N] is not initialized */
 		
+		//
+		
+		MersenneTwister()
+		{
+			init(rand());
+		}
+		
+		MersenneTwister(const uint32_t seed)
+		{
+			init(seed);
+		}
+		
 		void init(const uint32_t seed);
+		
 		uint32_t next();
+		
+		float nextf(const float min, const float max)
+		{
+			const float t = (next() % 1000) / 999.f;
+			
+			return min * t + max * (1.f - t);
+		}
+	};
+	
+	// R250_521 RNG. okay distribution and speedy
+	
+	struct R250_521
+	{
+		uint32_t r250_buffer[250];
+		uint32_t r521_buffer[521];
+
+		int r250_index = 0;
+		int r521_index = 0;
+		
+		R250_521()
+		{
+			init(rand());
+		}
+		
+		R250_521(const uint32_t seed)
+		{
+			init(seed);
+		}
+		
+		void init(const uint32_t seed);
+		
+		uint32_t next();
+		
+		float nextf(const float min, const float max)
+		{
+			const float t = (next() % 1000) / 999.f;
+			
+			return min * t + max * (1.f - t);
+		}
+	};
+	
+	// XOR-shift RNG. not so great distribution but fast
+	
+	struct XorShift
+	{
+		uint32_t XORSHIFT_x = 123;
+		uint32_t XORSHIFT_y = 234;
+		uint32_t XORSHIFT_z = 345;
+		uint32_t XORSHIFT_w = 456;
+
+		XorShift()
+		{
+			init(rand());
+		}
+		
+		XorShift(const uint32_t seed)
+		{
+			init(seed);
+		}
+		
+		void init(const uint32_t seed)
+		{
+			srand(seed);
+			
+			XORSHIFT_x = (uint32_t)rand();
+			XORSHIFT_y = (uint32_t)rand();
+			XORSHIFT_z = (uint32_t)rand();
+			XORSHIFT_w = (uint32_t)rand();
+		}
+
+		uint32_t next()
+		{
+			const uint32_t t = (XORSHIFT_x ^ (XORSHIFT_x << 11));
+
+			XORSHIFT_x = XORSHIFT_y;
+			XORSHIFT_y = XORSHIFT_z;
+			XORSHIFT_z = XORSHIFT_w;
+
+			XORSHIFT_w = (XORSHIFT_w ^ (XORSHIFT_w >> 19)) ^ (t ^ (t >> 8));
+
+			return XORSHIFT_w;
+		}
 		
 		float nextf(const float min, const float max)
 		{
@@ -60,7 +170,7 @@ namespace RNG
 		int whiteValues[5];
 		int range;
 		
-		PinkNumber(const int _range = 128);
+		PinkNumber(const int range = 128);
 		
 		int next();
 	};
