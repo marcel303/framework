9248e540cc8f650e926500981ce37f71bc371f5d
Marcel Smit <marcel303@gmail.com>
2018-01-29 18:11:52 +0100

    SSE-optimized blob detection mask generation from three channels.

--- vfxGraph/vfxNodes/vfxNodeBlobDetector.cpp
+++ vfxGraph/vfxNodes/vfxNodeBlobDetector.cpp
@@ -97,17 +97,22 @@ VfxNodeBlobDetector::~VfxNodeBlobDetector()
 
 #if __SSE2__
 
-static int processLine_SSE2_1channel(const uint8_t * __restrict src, const int srcSize, const int tresholdValue, uint8_t * __restrict dst)
+static int processLine_SSE2_1channel(
+	const uint8_t * __restrict src,
+	const int srcSize,
+	const int tresholdValue,
+	uint8_t * __restrict dst)
 {
 	const __m128i * src_16 = (const __m128i*)src;
 	__m128i * dst_16 = (__m128i*)dst;
 	
 	const int srcSize_16 = srcSize / 16;
-	const __m128i tresholdValue_8 = _mm_set1_epi16(tresholdValue);
 	
+	const int sub = tresholdValue;
 	const int max = (255 - tresholdValue) * 1;
 	const int mul = max ? (255 * 256 / max) : 0;
 	
+	const __m128i sub_8 = _mm_set1_epi16(sub);
 	const __m128i mul_8 = _mm_set1_epi16(mul);
 	
 	for (int x = 0; x < srcSize_16; ++x)
@@ -117,8 +122,61 @@ static int processLine_SSE2_1channel(const uint8_t * __restrict src, const int s
 		__m128i valueL = _mm_unpacklo_epi8(value, _mm_setzero_si128());
 		__m128i valueR = _mm_unpackhi_epi8(value, _mm_setzero_si128());
 		
-		valueL = _mm_subs_epu16(valueL, tresholdValue_8);
-		valueR = _mm_subs_epu16(valueR, tresholdValue_8);
+		valueL = _mm_subs_epu16(valueL, sub_8);
+		valueR = _mm_subs_epu16(valueR, sub_8);
+		
+		valueL = _mm_mullo_epi16(valueL, mul_8);
+		valueR = _mm_mullo_epi16(valueR, mul_8);
+		
+		valueL = _mm_srli_epi16(valueL, 8);
+		valueR = _mm_srli_epi16(valueR, 8);
+		
+		dst_16[x] = _mm_packus_epi16(valueL, valueR);
+	}
+	
+	return srcSize_16 * 16;
+}
+
+static int processLine_SSE2_3channel(
+	const uint8_t * __restrict src1,
+	const uint8_t * __restrict src2,
+	const uint8_t * __restrict src3,
+	const int srcSize,
+	const int tresholdValue,
+	uint8_t * __restrict dst)
+{
+	const __m128i * src1_16 = (const __m128i*)src1;
+	const __m128i * src2_16 = (const __m128i*)src2;
+	const __m128i * src3_16 = (const __m128i*)src3;
+	__m128i * dst_16 = (__m128i*)dst;
+	
+	const int srcSize_16 = srcSize / 16;
+	
+	const int sub = tresholdValue * 3;
+	const int max = (255 - tresholdValue) * 3;
+	const int mul = max ? (255 * 256 / max) : 0;
+	
+	const __m128i sub_8 = _mm_set1_epi16(sub);
+	const __m128i mul_8 = _mm_set1_epi16(mul);
+	
+	for (int x = 0; x < srcSize_16; ++x)
+	{
+		const __m128i value1 = src1_16[x];
+		const __m128i value2 = src2_16[x];
+		const __m128i value3 = src3_16[x];
+		
+		__m128i value1L = _mm_unpacklo_epi8(value1, _mm_setzero_si128());
+		__m128i value1R = _mm_unpackhi_epi8(value1, _mm_setzero_si128());
+		__m128i value2L = _mm_unpacklo_epi8(value2, _mm_setzero_si128());
+		__m128i value2R = _mm_unpackhi_epi8(value2, _mm_setzero_si128());
+		__m128i value3L = _mm_unpacklo_epi8(value3, _mm_setzero_si128());
+		__m128i value3R = _mm_unpackhi_epi8(value3, _mm_setzero_si128());
+		
+		__m128i valueL = _mm_add_epi16(value1L, _mm_add_epi16(value2L, value3L));
+		__m128i valueR = _mm_add_epi16(value1R, _mm_add_epi16(value2R, value3R));
+		
+		valueL = _mm_subs_epu16(valueL, sub_8);
+		valueR = _mm_subs_epu16(valueR, sub_8);
 		
 		valueL = _mm_mullo_epi16(valueL, mul_8);
 		valueR = _mm_mullo_epi16(valueR, mul_8);
@@ -174,15 +232,13 @@ void VfxNodeBlobDetector::tick(const float dt)
 		const int max3 = (255 - tresholdValue) * 3;
 		const int mul3 = max3 ? (255 * 256 / max3) : 0;
 		
-		// todo : this could be optimized a lot assuming data is planar
-		
 		if (image->numChannels == 1)
 		{
 			// note : single channel images. we just copy the data directly into the mask
 			
 			const VfxImageCpu::Channel * source = &image->channel[0];
 
-			for (int y = 0; y < image->sy; ++y)
+			for (int y = 0; y < maskSy; ++y)
 			{
 				const uint8_t * __restrict src = source->data + y * source->pitch;
 					  uint8_t * __restrict dst  = mask + y * maskSx;
@@ -190,10 +246,10 @@ void VfxNodeBlobDetector::tick(const float dt)
 				int begin = 0;
 				
 			#if __SSE2__
-				begin = processLine_SSE2_1channel(src, image->sx, tresholdValue, dst);
+				begin = processLine_SSE2_1channel(src, maskSx, tresholdValue, dst);
 			#endif
 			
-				for (int x = begin; x < image->sx; ++x)
+				for (int x = begin; x < maskSx; ++x)
 				{
 					int value = src[x];
 					
@@ -216,7 +272,14 @@ void VfxNodeBlobDetector::tick(const float dt)
 				const uint8_t * __restrict srcB = image->channel[2].data + y * image->channel[2].pitch;
 					  uint8_t * __restrict dst  = mask + y * maskSx;
 				
-				for (int x = 0; x < maskSx; ++x)
+				int begin = 0;
+				
+			#if __SSE2__
+				// without SSE: 1200-1400us -> 290-300us with SSE
+				begin = processLine_SSE2_3channel(srcR, srcG, srcB, image->sx, tresholdValue, dst);
+			#endif
+			
+				for (int x = begin; x < maskSx; ++x)
 				{
 					const int r = srcR[x];
 					const int g = srcG[x];
