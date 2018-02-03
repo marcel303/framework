f96577d413c249572549a76e166f2d68f72f2b62
Marcel Smit <marcel303@gmail.com>
2018-02-03 15:39:44 +0100

    Began adding an app to play audio graphs from the command line.

---
+++ audioGraph-examples/710-go.cpp
@@ -0,0 +1,122 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "audioGraph.h"
+#include "audioGraphManager.h"
+#include "audioUpdateHandler.h"
+#include "Debugging.h"
+#include "soundmix.h"
+#include <SDL2/SDL.h>
+
+#define CHANNEL_COUNT 64
+
+static void showHelp()
+{
+	printf("usage:\n");
+	printf("\t710-go <filename>\n");
+	printf("instantiates the audio graph given by <filename> and sends the synthesized sound to the default audio output interface.\n");
+}
+
+int main(int argc, char * argv[])
+{
+	const char * filename = nullptr;
+
+	if (argc < 2)
+	{
+		showHelp();
+		return 0;
+	}
+
+	filename = argv[1];
+
+	if (SDL_Init(0) >= 0)
+	{
+		// initialize audio related systems
+		
+		SDL_mutex * mutex = SDL_CreateMutex();
+		Assert(mutex != nullptr);
+
+		AudioVoiceManagerBasic voiceMgr;
+		voiceMgr.init(mutex, CHANNEL_COUNT, CHANNEL_COUNT);
+		voiceMgr.outputStereo = true;
+
+		AudioGraphManager_Basic audioGraphMgr(true);
+		audioGraphMgr.init(mutex, &voiceMgr);
+
+		AudioUpdateHandler audioUpdateHandler;
+		audioUpdateHandler.init(mutex, nullptr, 0);
+		audioUpdateHandler.voiceMgr = &voiceMgr;
+		audioUpdateHandler.audioGraphMgr = &audioGraphMgr;
+
+		PortAudioObject pa;
+		pa.init(SAMPLE_RATE, 2, 0, AUDIO_UPDATE_SIZE, &audioUpdateHandler);
+		
+		// create an audio graph instance
+		
+		AudioGraphInstance * instance = audioGraphMgr.createInstance(filename);
+		
+		if (instance == nullptr)
+		{
+			printf("failed to open %s\n", filename);
+		}
+		else
+		{
+			// todo : keyboard/console based ui to 
+			// - change control values
+			// - trigger events
+			// - quit the app
+			
+			for (;;)
+			{
+				printf("CPU usage: %d%%\n", int(audioUpdateHandler.msecsPerSecond / 1000000.0 * 100));
+				
+				SDL_Delay(1100);
+			}
+		}
+		
+		// free the audio graph instance
+		
+		audioGraphMgr.free(instance, false);
+		
+		// shut down audio related systems
+
+		pa.shut();
+		
+		audioUpdateHandler.shut();
+
+		audioGraphMgr.shut();
+		
+		voiceMgr.shut();
+
+		SDL_DestroyMutex(mutex);
+		mutex = nullptr;
+		
+		SDL_Quit();
+	}
+
+	return 0;
+}
