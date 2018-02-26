586cdd1358607dd874f01527924eed4ca6a57508
Marcel Smit <marcel303@gmail.com>
2018-02-26 16:46:08 +0100

    - Added a separate camera polling thread. - Added support for multiple cameras. - Added controller UI to set exposure and gain for the cameras (first pass).

--- users/marcel/veem-camapp/main.cpp
+++ users/marcel/veem-camapp/main.cpp
@@ -5,13 +5,21 @@
 #include "ip/UdpSocket.h"
 #include "osc/OscOutboundPacketStream.h"
 
+#define GFX_SX 640
+#define GFX_SY 240
+
 #define OSC_BUFFER_SIZE 2048
 
 #define DO_CONTROLLER 1
 
 #if DO_CONTROLLER
 	#include "framework.h"
+#else
+	#include <SDL2/SDL.h>
 #endif
+
+#include <atomic>
+#include <string>
 
 using namespace ps3eye;
 
@@ -21,17 +29,34 @@ struct OscSender
 {
 	UdpTransmitSocket * transmitSocket;
 	
-	void init(const char * ipAddress, const int udpPort)
+	bool init(const char * ipAddress, const int udpPort)
 	{
-		transmitSocket = new UdpTransmitSocket(IpEndpointName(ipAddress, udpPort));
+		try
+		{
+			transmitSocket = new UdpTransmitSocket(IpEndpointName(ipAddress, udpPort));
+			
+			return true;
+		}
+		catch (std::exception & e)
+		{
+			LOG_ERR("failed to create UDP transmit socket: %s", e.what());
+			return false;
+		}
 	}
 	
 	void shut()
 	{
 		if (transmitSocket != nullptr)
 		{
-			delete transmitSocket;
-			transmitSocket = nullptr;
+			try
+			{
+				delete transmitSocket;
+				transmitSocket = nullptr;
+			}
+			catch (std::exception & e)
+			{
+				LOG_DBG("%s", e.what());
+			}
 		}
 	}
 	
@@ -39,14 +64,23 @@ struct OscSender
 	{
 		if (transmitSocket != nullptr)
 		{
-			transmitSocket->Send((char*)data, dataSize);
+			try
+			{
+				transmitSocket->Send((char*)data, dataSize);
+			}
+			catch (std::exception & e)
+			{
+				LOG_DBG("failed to send UDP packet: %s", e.what());
+			}
 		}
 	}
 };
 
+static OscSender * s_oscSender = nullptr;
+
 //
 
-void sendCameraData(const uint8_t * frameData, OscSender & sender)
+void sendCameraData(const uint8_t * frameData, OscSender & sender, const char * addressPrefix)
 {
 	// output : 16 x 12 values (float)
 	
@@ -84,7 +118,10 @@ void sendCameraData(const uint8_t * frameData, OscSender & sender)
 		char buffer[OSC_BUFFER_SIZE];
 		osc::OutboundPacketStream p(buffer, OSC_BUFFER_SIZE);
 		
-		p << osc::BeginMessage("/env/light/raw");
+		char address[64];
+		sprintf(address, "%s/raw", addressPrefix);
+		
+		p << osc::BeginMessage(address);
 		{
 			for (int y = 0; y < sy; ++y)
 				for (int x = 0; x < sx; ++x)
@@ -111,7 +148,10 @@ void sendCameraData(const uint8_t * frameData, OscSender & sender)
 		char buffer[OSC_BUFFER_SIZE];
 		osc::OutboundPacketStream p(buffer, OSC_BUFFER_SIZE);
 		
-		p << osc::BeginMessage("/env/light/average");
+		char address[64];
+		sprintf(address, "%s/average", addressPrefix);
+		
+		p << osc::BeginMessage(address);
 		p << average;
 		p << osc::EndMessage;
 		
@@ -119,13 +159,135 @@ void sendCameraData(const uint8_t * frameData, OscSender & sender)
 	}
 }
 
+struct Recorder
+{
+	PS3EYECam::PS3EYERef eye;
+	
+	uint8_t * frameData = nullptr;
+	
+	std::string oscAddressPrefix;
+	
+	SDL_Thread * receiveThread = nullptr;
+	
+	std::atomic<bool> quitRequested;
+	
+	std::atomic<float> exposure;
+	std::atomic<float> gain;
+	
+	Recorder()
+		: quitRequested(false)
+		, exposure(1.f)
+		, gain(0.f)
+	{
+	}
+	
+	~Recorder()
+	{
+		shut();
+	}
+	
+	bool init(const int deviceIndex, const char * _oscAddressPrefix)
+	{
+		auto devices = PS3EYECam::getDevices();
+		
+		LOG_INF("found %d devices", devices.size());
+		
+		if (deviceIndex >= devices.size())
+		{
+			LOG_ERR("no PS3 eye camera connected at index=%d", deviceIndex);
+			shut();
+			return false;
+		}
+		
+		eye = devices[deviceIndex];
+		devices.clear();
+		
+		const bool result = eye->init(320, 240, 60, PS3EYECam::EOutputFormat::Gray);
+
+		if (!result)
+		{
+			LOG_ERR("failed to initialize PS3 eye camera", 0);
+			shut();
+			return false;
+		}
+
+		eye->start();
+		
+		const int sx = eye->getWidth();
+		const int sy = eye->getHeight();
+		
+		frameData = new uint8_t[sx * sy];
+		
+		oscAddressPrefix = _oscAddressPrefix;
+		
+		receiveThread = SDL_CreateThread(receiveThreadProc, "PS3 Eye Camera", this);
+		
+		return true;
+	}
+	
+	void shut()
+	{
+		if (receiveThread != nullptr)
+		{
+			quitRequested = true;
+			
+			SDL_WaitThread(receiveThread, nullptr);
+			receiveThread = nullptr;
+			
+			quitRequested = false;
+		}
+		
+		if (frameData != nullptr)
+		{
+			delete [] frameData;
+			frameData = nullptr;
+		}
+		
+		if (eye != nullptr)
+		{
+			eye->stop();
+			eye = nullptr;
+		}
+	}
+	
+	static int receiveThreadProc(void * obj)
+	{
+		auto self = (Recorder*)obj;
+		
+		while (self->quitRequested == false)
+		{
+			self->eye->setAutogain(false);
+			self->eye->setAutoWhiteBalance(false);
+			self->eye->setExposure(self->exposure * 255.f);
+			self->eye->setGain(self->gain * 63.f);
+			//self->eye->setAutogain(true);
+			
+			self->eye->getFrame(self->frameData);
+			
+			//LOG_DBG("got frame data!", 0);
+			
+			// todo : make thread safe
+			
+			sendCameraData(self->frameData, *s_oscSender, self->oscAddressPrefix.c_str());
+		}
+		
+		return 0;
+	}
+};
+
 #if DO_CONTROLLER
 
 struct Controller
 {
-	void init()
+	Recorder * recorder1;
+	Recorder * recorder2;
+	
+	void init(Recorder * _recorder1, Recorder * _recorder2)
 	{
-		framework.init(0, nullptr, 320, 240);
+		framework.init(0, nullptr, GFX_SX, GFX_SY);
+		
+		recorder1 = _recorder1;
+		recorder2 = _recorder2;
 	}
 	
 	void shut()
@@ -133,35 +295,76 @@ struct Controller
 		framework.shutdown();
 	}
 	
-	void tick()
+	bool tick()
 	{
 		framework.process();
 		
-		const float dt = framework.timeStep;
+		if (keyboard.wentDown(SDLK_ESCAPE))
+			framework.quitRequested = true;
+			
+		if (mouse.isDown(BUTTON_LEFT))
+		{
+			if (mouse.y < 120)
+				recorder1->exposure = mouse.x / 320.f;
+			else
+				recorder1->gain = mouse.x / 320.f;
+		}
+		
+		return !framework.quitRequested;
 	}
 	
-	void draw(const uint8_t * frameData)
+	void drawCamera(const int index)
+	{
+		Recorder * recorder = nullptr;
+		
+		if (index == 0)
+			recorder = recorder1;
+		if (index == 1)
+			recorder = recorder2;
+		
+		if (recorder != nullptr && recorder->frameData != nullptr)
+		{
+			GLuint texture = createTextureFromR8(recorder->frameData, 320, 240, false, true);
+			
+			glBindTexture(GL_TEXTURE_2D, texture);
+			GLint swizzleMask[4] = { GL_RED, GL_RED, GL_RED, GL_ONE };
+			glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+			checkErrorGL();
+			glBindTexture(GL_TEXTURE_2D, 0);
+			checkErrorGL();
+
+			gxSetTexture(texture);
+			setColor(colorWhite);
+			drawRect(0, 0, 320, 240);
+			gxSetTexture(0);
+			
+			glDeleteTextures(1, &texture);
+			texture = 0;
+		}
+	}
+	
+	void draw()
 	{
 		framework.beginDraw(0, 0, 0, 0);
 		{
 			setFont("calibri.ttf");
 			pushFontMode(FONT_SDF);
 			{
-				GLuint texture = createTextureFromR8(frameData, 320, 240, false, true);
+				gxPushMatrix();
+				{
+					drawCamera(0);
+					gxTranslatef(320, 0, 0);
+					
+					drawCamera(1);
+					gxTranslatef(320, 0, 0);
+				}
+				gxPopMatrix();
+
+				setColor(0, 0, 255, 63);
+				drawRect(0, 0, 320 * recorder1->exposure, 120);
 				
-				glBindTexture(GL_TEXTURE_2D, texture);
-				GLint swizzleMask[4] = { GL_RED, GL_RED, GL_RED, GL_ONE };
-				glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
-				checkErrorGL();
-				glBindTexture(GL_TEXTURE_2D, 0);
-				checkErrorGL();
-	
-				gxSetTexture(texture);
-				drawRect(0, 0, 320, 240);
-				gxSetTexture(0);
-				
-				glDeleteTextures(1, &texture);
-				texture = 0;
+				setColor(255, 0, 0, 63);
+				drawRect(0, 120, 320 * recorder1->gain, 240);
 			}
 			popFontMode();
 		}
@@ -169,68 +372,60 @@ struct Controller
 	}
 };
 
+#else
+
+struct Controller
+{
+	void init(Recorder * _recorder1, Recorder * _recorder2)
+	{
+		SDL_Init(0);
+	}
+	
+	void shut()
+	{
+		SDL_Quit();
+	}
+	
+	bool tick()
+	{
+		return true;
+	}
+	
+	void draw()
+	{
+	}
+};
+
 #endif
 
 int main(int argc, char * argv[])
 {
-	Controller controller;
-	controller.init();
-	
 	OscSender sender;
-	sender.init("255.255.255.255", 8000);
-	
-	PS3EYECam::PS3EYERef eye;
-	
-	auto devices = PS3EYECam::getDevices();
-	
-	LOG_INF("found %d devices", devices.size());
-	
-	if (devices.empty())
-	{
-		LOG_ERR("no PS3 eye camera connected", 0);
+	if (!sender.init("255.255.255.255", 8000))
 		return -1;
-	}
+	s_oscSender = &sender;
 	
-	eye = devices[0];
-	devices.clear();
-	
-	const bool result = eye->init(320, 240, 2, PS3EYECam::EOutputFormat::Gray);
-
-	if (!result)
-	{
-		LOG_ERR("failed to initialize PS3 eye camera", 0);
+	Recorder recorder1;
+	if (!recorder1.init(0, "/env/light"))
 		return -1;
-	}
-
-	eye->start();
-
-	const int sx = eye->getWidth();
-	const int sy = eye->getHeight();
-
-	uint8_t * frameData = new uint8_t[sx * sy];
-
+	
+	Recorder recorder2;
+	if (!recorder2.init(1, "/room/light") && false)
+		return -1;
+	
+	Controller controller;
+	controller.init(&recorder1, &recorder2);
+	
 	for (;;)
 	{
-		eye->setAutogain(false);
-		eye->setAutoWhiteBalance(false);
-		//eye->setExposure(0);
+		if (!controller.tick())
+			break;
 		
-		eye->getFrame(frameData);
-		
-		LOG_DBG("got frame data!", 0);
-		
-		sendCameraData(frameData, sender);
-		
-		controller.tick();
-		
-		controller.draw(frameData);
+		controller.draw();
 	}
-
-	delete [] frameData;
-	frameData = nullptr;
-
-	eye->stop();
-	eye = nullptr;
+	
+	recorder1.shut();
+	recorder2.shut();
 	
 	sender.shut();
 	
