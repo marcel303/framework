8e09ba280b221eeb78b60619c8c47a331315b4fc
Marcel Smit <marcel303@gmail.com>
2018-03-02 15:18:09 +0100

    - Use doubles for rotating the mechanism. - Wrap mechanism angles around 360, to ensure good quality for sin/cos math using when constructing rotation matrices. - Added a mechanism audio node.

--- users/marcel/veem-sound/main.cpp
+++ users/marcel/veem-sound/main.cpp
@@ -1,5 +1,6 @@
 #include "audioGraph.h"
 #include "audioGraphManager.h"
+#include "audioNodeBase.h"
 #include "audioUpdateHandler.h"
 #include "audioVoiceManager4D.h"
 #include "framework.h"
@@ -168,19 +169,23 @@ struct Mechanism
 	
 	//
 	
-	float xAngle = 0.f;
-	float yAngle = 0.f;
-	float zAngle = 0.f;
+	double xAngle = 0.0;
+	double yAngle = 0.0;
+	double zAngle = 0.0;
 	
-	float xAngleSpeed = 0.f;
-	float yAngleSpeed = 0.f;
-	float zAngleSpeed = 0.f;
+	double xAngleSpeed = 0.0;
+	double yAngleSpeed = 0.0;
+	double zAngleSpeed = 0.0;
 	
 	void tick(const float dt)
 	{
 		xAngle += xAngleSpeed * dt;
 		yAngle += yAngleSpeed * dt;
 		zAngle += zAngleSpeed * dt;
+		
+		xAngle = std::fmod(xAngle, 360.0);
+		yAngle = std::fmod(yAngle, 360.0);
+		zAngle = std::fmod(zAngle, 360.0);
 	}
 	
 	void evaluateMatrix(const int ringIndex, Mat4x4 & matrix, float & radius) const
@@ -190,19 +195,19 @@ struct Mechanism
 		
 		if (ringIndex >= 1)
 		{
-			matrix = matrix.RotateX(-xAngle * M_PI / 180.f);
+			matrix = matrix.RotateX(-xAngle * M_PI / 180.0);
 			radius = kRadius2;
 		}
 		
 		if (ringIndex >= 2)
 		{
-			matrix = matrix.RotateY(-yAngle * M_PI / 180.f);
+			matrix = matrix.RotateY(-yAngle * M_PI / 180.0);
 			radius = kRadius3;
 		}
 		
 		if (ringIndex >= 3)
 		{
-			matrix = matrix.RotateX(-zAngle * M_PI / 180.f);
+			matrix = matrix.RotateX(-zAngle * M_PI / 180.0);
 			radius = kRadius4;
 		}
 	}
@@ -256,6 +261,97 @@ struct Mechanism
 		gxPopMatrix();
 	}
 };
+
+static Mechanism * s_mechanism = nullptr;
+
+struct AudioNodeMechanism : AudioNodeBase
+{
+	Mechanism mechanism;
+	
+	enum Input
+	{
+		kInput_Ring,
+		kInput_Speed,
+		kInput_Angle,
+		kInput_COUNT
+	};
+	
+	enum Output
+	{
+		kOutput_X,
+		kOutput_Y,
+		kOutput_Z,
+		kOutput_COUNT
+	};
+	
+	AudioFloat xOutput;
+	AudioFloat yOutput;
+	AudioFloat zOutput;
+	
+	float time;
+	
+	AudioNodeMechanism()
+		: AudioNodeBase()
+		, xOutput(0.f)
+		, yOutput(0.f)
+		, zOutput(0.f)
+		, time(0.f)
+	{
+		resizeSockets(kInput_COUNT, kOutput_COUNT);
+		addInput(kInput_Ring, kAudioPlugType_Int);
+		addInput(kInput_Speed, kAudioPlugType_FloatVec);
+		addInput(kInput_Angle, kAudioPlugType_FloatVec);
+		addOutput(kOutput_X, kAudioPlugType_FloatVec, &xOutput);
+		addOutput(kOutput_Y, kAudioPlugType_FloatVec, &yOutput);
+		addOutput(kOutput_Z, kAudioPlugType_FloatVec, &zOutput);
+		
+		s_mechanism = &mechanism;
+	};
+	
+	virtual void tick(const float dt)
+	{
+		const int ring = getInputInt(kInput_Ring, 0);
+		const AudioFloat * speed = getInputAudioFloat(kInput_Speed, &AudioFloat::One);
+		const AudioFloat * angle = getInputAudioFloat(kInput_Angle, &AudioFloat::Zero);
+		
+		angle->expand();
+		
+		xOutput.setVector();
+		yOutput.setVector();
+		zOutput.setVector();
+		
+		const double dtSample = double(dt) * speed->getMean() / AUDIO_UPDATE_SIZE;
+		
+		for (int i = 0; i < AUDIO_UPDATE_SIZE; ++i)
+		{
+			const Vec3 p = mechanism.evaluatePoint(ring, angle->samples[i]);
+			
+			xOutput.samples[i] = p[0];
+			yOutput.samples[i] = p[1];
+			zOutput.samples[i] = p[2];
+			
+			mechanism.tick(dtSample);
+		}
+		
+		time += dt;
+		
+		mechanism.xAngleSpeed = scaled_octave_noise_2d(8, .5f, .01f, -90.f, +90.f, 0.f, time);
+		mechanism.yAngleSpeed = scaled_octave_noise_2d(8, .5f, .01f, -90.f, +90.f, 1.f, time);
+		mechanism.zAngleSpeed = scaled_octave_noise_2d(8, .5f, .01f, -90.f, +90.f, 2.f, time);
+	}
+};
+
+AUDIO_NODE_TYPE(mechanism, AudioNodeMechanism)
+{
+	typeName = "mechanism";
+	
+	in("ring", "int");
+	in("speed", "audioValue");
+	in("angle", "audioValue");
+	out("x", "audioValue");
+	out("y", "audioValue");
+	out("z", "audioValue");
+}
 
 struct Thermalizer
 {
@@ -331,6 +427,7 @@ int main(int argc, char * argv[])
 	paObject.init(SAMPLE_RATE, 2, 0, AUDIO_UPDATE_SIZE, &audioUpdateHandler);
 	
 	Mechanism mechanism;
+	s_mechanism = &mechanism;
 	
 	Thermalizer thermalizer;
 	
