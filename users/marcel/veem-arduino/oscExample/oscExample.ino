1a37c05cd02bf77027474755706d01713c692bd2
Marcel Smit <marcel303@gmail.com>
2018-03-07 12:23:37 +0100

    Added Arduino/NodeMCU OSC example.

---
+++ users/marcel/veem-arduino/oscExample/oscExample.ino
@@ -0,0 +1,54 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+
+char ssid[] = "youarenotalone";
+char pass[] = "hitorijanai";
+
+WiFiUDP Udp;
+const IPAddress outIp(255, 255, 255, 255);
+const unsigned int outPort = 8000;
+const unsigned int localPort = 8888;
+
+void setup() {
+    Serial.begin(115200);
+
+    // Connect to WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Starting UDP");
+    Udp.begin(localPort);
+    Serial.print("Local port: ");
+#ifdef ESP32
+    Serial.println(localPort);
+#else
+    Serial.println(Udp.localPort());
+#endif
+
+}
+
+void loop() {
+    OSCMessage msg("/test");
+    msg.add(1.f);
+    msg.add(2.f);
+    msg.add(3.f);
+    Udp.beginPacket(outIp, outPort);
+    msg.send(Udp);
+    Udp.endPacket();
+    msg.empty();
+    delay(500);
+}
