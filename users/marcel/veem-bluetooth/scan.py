8e975005a007dbfe83044a4f22d8abdb138748f0
Marcel Smit <marcel303@gmail.com>
2018-03-18 15:24:13 +0100

    Make Bluetooth script suitable to run as a cron job. Removed while loop from Bluetooth scanning script, and reduced sleep to two seconds.

--- users/marcel/veem-bluetooth/scan.py
+++ users/marcel/veem-bluetooth/scan.py
@@ -6,10 +6,6 @@ from pythonosc import udp_client
 
 client = udp_client.SimpleUDPClient("255.255.255.255", 8000, True)
 
-while (True):
-    nearby_devices = bluetooth.discover_devices(lookup_names=False)
-    print("%d" % len(nearby_devices))
-    
-    client.send_message("/bluetooth/count", len(nearby_devices) + 0.0)
-    
-    time.sleep(60)
+nearby_devices = bluetooth.discover_devices(lookup_names=False)
+client.send_message("/bluetooth/count", len(nearby_devices) + 0.0)
+time.sleep(2)
