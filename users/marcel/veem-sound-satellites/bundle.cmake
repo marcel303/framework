04ee8bad8ca48bcca3ed711d3fabb2b977bd4a5c
Marcel Smit <marcel303@gmail.com>
2018-03-19 23:52:11 +0100

    Fixed resource copy script satellites app.

--- users/marcel/veem-sound-satellites/bundle.cmake
+++ users/marcel/veem-sound-satellites/bundle.cmake
@@ -1,8 +1,8 @@
 include(BundleUtilities)
 
-set(resource_path "/Users/thecat/Google Drive/SDN0-Veem/veem-sound/")
+set(resource_path "/Users/thecat/Google Drive/SDN0-Veem/veem-sound-satellites/")
 
-set(appresources_path "${CMAKE_BINARY_DIR}/Release/veem-sound.app/Contents/Resources")
+set(appresources_path "${CMAKE_BINARY_DIR}/Release/veem-sound-satellites.app/Contents/Resources")
 message("resources: ${resource_path}")
 message("app resources: ${appresources_path}")
 
@@ -10,4 +10,4 @@ execute_process(COMMAND mkdir "-p" "${appresources_path}")
 
 execute_process(COMMAND rsync "-r" "${resource_path}" "${appresources_path}")
 
-FIXUP_BUNDLE("${CMAKE_BINARY_DIR}/Release/veem-sound.app" "" "")
+FIXUP_BUNDLE("${CMAKE_BINARY_DIR}/Release/veem-sound-satellites.app" "" "")
