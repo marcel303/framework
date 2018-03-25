35b94d3efb43bc834c0ce0f6122d25aac2a89153
Marcel Smit <marcel303@gmail.com>
2018-03-25 15:50:55 +0000

    Adding OpenGL attributes. Apparently 32 bit depth buffer is off-limits.

--- users/marcel/rpi3d/main.cpp
+++ users/marcel/rpi3d/main.cpp
@@ -24,6 +24,21 @@ int main(int argc, char * argv[])
 		return -1;
 	}
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+	//SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
+	//SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 	auto context = SDL_GL_CreateContext(window);
 	
 	if (context == nullptr)
