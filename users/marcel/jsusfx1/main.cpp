8d6767ed0352d3a66da14f98e59c01c6d0c5d50b
Marcel Smit <marcel303@gmail.com>
2018-04-15 21:57:09 +0200

    - Added import resolve hack to make JSFX-Kawa work. - Implemented gfx_triangle. - Implemented gfx_drawstr. Doesn't do index based lookups yet. - Added drag and drop support. - Added missing JsusFx::init call. Registers string functions, fft, etc.

--- users/marcel/jsusfx1/main.cpp
+++ users/marcel/jsusfx1/main.cpp
@@ -7,6 +7,17 @@
 
 #include <fstream>
 
+#include "WDL/wdlstring.h"
+
+/*
+
+GFX status:
+- Most of Liteon scripts appear to work.
+- JSFX-Kawa (https://github.com/kawaCat/JSFX-kawa) mostly works. Some text and blitting not working yet.
+- ATK for Reaper loads ok, but doesn't output anything to the screen at all.
+
+*/
+
 const int GFX_SX = 640;
 const int GFX_SY = 800;
 
@@ -20,8 +31,15 @@ struct JsusFxPathLibraryTest : JsusFxPathLibrary {
 		const size_t pos = parentPath.rfind('/', '\\');
 		if (pos != std::string::npos)
 			resolvedPath = parentPath.substr(0, pos + 1);
-		resolvedPath += importPath;
-		return fileExists(resolvedPath);
+		if (fileExists(resolvedPath + importPath))
+		{
+			resolvedPath = resolvedPath + importPath;
+			return true;
+		}
+		resolvedPath = resolvedPath + "lib/" + importPath; // fixme : this is a hack to make JSFX-Kawa work, without having to deal with search paths more generally
+		if (fileExists(resolvedPath))
+			return true;
+		return false;
 	}
 	
 	virtual std::istream* open(const std::string &path) override {
@@ -63,7 +81,9 @@ public:
     }
 };
 
-#define STUB printf("function %s not implemented", __FUNCTION__)
+static JsusFxTest * s_fx = nullptr;
+
+#define STUB printf("function %s not implemented\n", __FUNCTION__)
 
 struct JsusFxGfx_Framework : JsusFxGfx
 {
@@ -162,9 +182,51 @@ struct JsusFxGfx_Framework : JsusFxGfx
 		}
 	}
 	
-	virtual void gfx_triangle(EEL_F ** params, int np)
+	virtual void gfx_triangle(EEL_F ** parms, int np)
 	{
-		STUB;
+		if (np >= 6)
+		{
+			np &= ~1;
+			
+			if (np == 6)
+			{
+				const int x1 = (int)parms[0][0];
+				const int y1 = (int)parms[1][0];
+				const int x2 = (int)parms[2][0];
+				const int y2 = (int)parms[3][0];
+				const int x3 = (int)parms[4][0];
+				const int y3 = (int)parms[5][0];
+				
+				updateColor();
+				
+				hqBegin(HQ_FILLED_TRIANGLES);
+				hqFillTriangle(x1, y1, x2, y2, x3, y3);
+				hqEnd();
+			}
+			else
+			{
+				// convex polygon
+				
+				const int x1 = (int)parms[0][0];
+				const int y1 = (int)parms[1][0];
+				const int x2 = (int)parms[2][0];
+				const int y2 = (int)parms[3][0];
+				
+				updateColor();
+				
+				hqBegin(HQ_FILLED_TRIANGLES);
+				{
+					for (int i = 2; i < np / 2; ++i)
+					{
+						const int x3 = (int)parms[i * 2 + 0][0];
+						const int y3 = (int)parms[i * 2 + 1][0];
+						
+						hqFillTriangle(x1, y1, x2, y2, x3, y3);
+					}
+				}
+				hqEnd();
+			}
+		}
 	}
 	
 	virtual void gfx_lineto(EEL_F xpos, EEL_F ypos, EEL_F useaa) override
@@ -210,7 +272,10 @@ struct JsusFxGfx_Framework : JsusFxGfx
 		STUB;
 	}
 	
-	virtual void gfx_grad_or_muladd_rect(int mod, int np, EEL_F ** params) { }
+	virtual void gfx_grad_or_muladd_rect(int mod, int np, EEL_F ** params)
+	{
+		STUB;
+	}
 	
 	virtual void gfx_drawnumber(EEL_F n, int nd) override
 	{
@@ -247,9 +312,85 @@ struct JsusFxGfx_Framework : JsusFxGfx
 		*m_gfx_x += sx;
 	}
 	
-	virtual void gfx_drawstr(void * opaque, EEL_F ** parms, int np, int mode) // mode=1 for format, 2 for purely measure no format, 3 for measure char
+	virtual void gfx_drawstr(void * opaque, EEL_F ** parms, int nparms, int formatmode) // mode=1 for format, 2 for purely measure no format, 3 for measure char
 	{
-		STUB;
+		int nfmtparms = nparms - 1;
+		
+		EEL_F ** fmtparms = parms + 1;
+		
+		const char * funcname =
+			  formatmode == 1 ? "gfx_printf"
+			: formatmode == 2 ? "gfx_measurestr"
+			: formatmode == 3 ? "gfx_measurechar"
+			: "gfx_drawstr";
+
+		WDL_FastString * fs = nullptr;
+		
+		char buf[4096];
+
+		const char * s = nullptr;
+		int s_len = 0;
+		
+		char temp[32];
+		if (formatmode == 3)
+		{
+			temp[0] = (int)parms[0][0];
+			temp[1] = 0;
+			s_len = 1;
+			s = temp;
+		}
+		else
+		{
+			// todo : handle string lookups by index
+			//s = s_fx->m_string_context->GetStringForIndex(parms[0][0], &fs);
+			s = nullptr;
+			
+		#ifdef EEL_STRING_DEBUGOUT
+			if (!s)
+				EEL_STRING_DEBUGOUT("gfx_%s: invalid string identifier %f",funcname,parms[0][0]);
+		#endif
+		
+			if (!s)
+			{
+				s="<bad string>";
+				s_len = 12;
+			}
+			else if (formatmode == 1)
+			{
+				extern int eel_format_strings(void *, const char *s, const char *ep, char *, int, int, EEL_F **);
+				
+				s_len = eel_format_strings(opaque,s,fs?(s+fs->GetLength()):NULL,buf,sizeof(buf),nfmtparms,fmtparms);
+				if (s_len < 1)
+					return;
+				s = buf;
+			}
+			else
+			{
+				s_len = fs ? fs->GetLength() : (int)strlen(s);
+			}
+		}
+
+		if (s_len)
+		{
+			float sx;
+			float sy;
+			measureText(m_fontSize, sx, sy, "%s", s);
+			
+			if (formatmode >= 2)
+			{
+				if (nfmtparms == 2)
+				{
+					fmtparms[0][0] = sx;
+					fmtparms[1][0] = sy;
+				}
+			}
+			else
+			{
+				updateColor();
+				drawText(*m_gfx_x, *m_gfx_y + m_fontSize, m_fontSize, +1, -1, "%s", s);
+				*m_gfx_x += sx;
+			}
+		}
 	}
 	
 	virtual void gfx_setpixel(EEL_F r, EEL_F g, EEL_F b) override
@@ -334,12 +475,32 @@ static void doSlider(JsusFx & fx, Slider & slider, int x, int y)
 	drawRectLine(0, 0, sx, sy);
 }
 
+//
+
+static void handleAction(const std::string & action, const Dictionary & d)
+{
+	if (action == "filedrop")
+	{
+		auto filename = d.getString("file", "");
+		
+		JsusFxPathLibraryTest pathLibrary;
+		s_fx->compile(pathLibrary, filename);
+		
+		s_fx->prepare(44100, 64);
+	}
+}
+
 int main(int argc, char * argv[])
 {
+	framework.filedrop = true;
+	framework.actionHandler = handleAction;
+	
 	if (!framework.init(0, nullptr, GFX_SX, GFX_SY))
 		return -1;
 	
+	JsusFx::init();
 	JsusFxTest fx;
+	s_fx = &fx;
 	JsusFxGfx_Framework gfx;
 	//JsusFxGfx_Log gfx;
 	fx.gfx = &gfx;
