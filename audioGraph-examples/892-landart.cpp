5923f14572cf4ca88c9729cc01a0c155def52e30
Marcel Smit <marcel303@gmail.com>
2018-05-11 18:46:34 +0200

    - Added He Who Has To videos 2 and 3. - Added keyboard 1-3 to switch videos.

--- audioGraph-examples/892-landart.cpp
+++ audioGraph-examples/892-landart.cpp
@@ -174,8 +174,8 @@ VFX_NODE_TYPE(VfxNodeTriggerFilter)
 static const char * videoFilenames[NUM_VIDEOS] =
 {
 	"landschappen/hewhohasto.mp4",
-	"landschappen/landschapbomentest.mp4",
-	"landschappen/landschapwoestijntest.mp4",
+	"landschappen/hewhohasto2.mp4",
+	"landschappen/hewhohasto3.mp4",
 };
 
 MediaPlayer mediaPlayers[NUM_VIDEOS][2];
@@ -258,6 +258,13 @@ struct VfxNodeLandscape : VfxNodeBase
 	
 	virtual void tick(const float dt) override
 	{
+		if (keyboard.wentDown(SDLK_1))
+			s_landscape->activeVideo = 0;
+		else if (keyboard.wentDown(SDLK_2))
+			s_landscape->activeVideo = 1;
+		else if (keyboard.wentDown(SDLK_3))
+			s_landscape->activeVideo = 2;
+			
 		s_landscape->scrollSpeed = getInputFloat(kInput_ScrollSpeed, 1.f);
 		
 		imageOutput.texture = s_landscape->getVideoTexture();
@@ -265,7 +272,10 @@ struct VfxNodeLandscape : VfxNodeBase
 	
 	virtual void handleTrigger(const int index) override
 	{
-		s_landscape->activeVideo = (s_landscape->activeVideo + 1) % NUM_VIDEOS;
+		if (index == kInput_NextVideo)
+		{
+			//s_landscape->activeVideo = (s_landscape->activeVideo + 1) % NUM_VIDEOS;
+		}
 	}
 };
 
