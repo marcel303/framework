16c1a199985c700063693e7c671739acf1f546c9
Marcel Smit <marcel303@gmail.com>
2018-05-30 16:46:37 +0000

    Adding GPIO object.

--- users/marcel/raspi-gpio1/gpio1.cpp
+++ users/marcel/raspi-gpio1/gpio1.cpp
@@ -1,6 +1,3 @@
-#define BCM2708_PERI_BASE 0x3F000000
-#define GPIO_BASE (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,59 +5,51 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+struct GPIO
+{
+	void * gpio_map = nullptr;
+	volatile uint32_t * gpio = nullptr;
+	
+	bool init();
+	void shut();
+	
+	void setInput(const int index);
+	void setOutput(const int index);
+	
+	void set(const uint32_t mask); // sets bits which are 1 ignores bits which are 0
+	void unset(const uint32_t mask); // clears bits which are 1 ignores bits which are 0
+};
+
+#define BCM2708_PERI_BASE 0x3F000000
+#define GPIO_BASE (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
-
-static int  mem_fd = 0;
-static void * gpio_map = nullptr;
-
-// I/O access
-static volatile uint32_t * gpio = nullptr;
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
-// sets bits which are 1 ignores bits which are 0
-inline void GPIO_SET(const uint32_t mask)
-{
-	gpio[7] = mask;
-}
-
-// clears bits which are 1 ignores bits which are 0
-inline void GPIO_CLR(const uint32_t mask)
-{
-	gpio[10] = mask;
-}
-
 #define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
 
 #define GPIO_PULL *(gpio+37) // Pull up/pull down
 #define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
 
-bool setup_io();
-
 int main(int argc, char **argv)
 {
-	// Set up gpi pointer for direct register access
-	if (!setup_io())
+	GPIO gpio;
+	
+	if (!gpio.init())
+	{
+		printf("GPIO init failed\n");
 		return -1;
+	}
 
-	// Switch GPIO 7..11 to output mode
-
-/************************************************************************\
- * You are about to change the GPIO settings of your computer.          *
- * Mess this up and it will stop working!                               *
- * It might be a good idea to 'sync' before running this program        *
- * so at least you still have your code changes written to the SD-card! *
-\************************************************************************/
-
-	// Set GPIO pins 7-11 to output
+	// set GPIO pins 7-11 to output
 	for (int g = 7; g <= 11; ++g)
 	{
-		INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
-		OUT_GPIO(g);
+		gpio.setOutput(g);
 	}
 
 	int n[12];
@@ -94,10 +83,12 @@ int main(int argc, char **argv)
 			else
 				unset |= 1 << i;
 		}
-
-		GPIO_SET(set);
-		GPIO_CLR(unset);
+		
+		gpio.set(set);
+		gpio.unset(unset);
 	}
+	
+	gpio.shut();
 
 	return 0;
 }
@@ -106,12 +97,12 @@ int main(int argc, char **argv)
 // Set up a memory regions to access GPIO
 //
 
-bool setup_io()
+bool GPIO::init()
 {
-	/* open /dev/mem */
-	if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0)
+	int mem_fd = open("/dev/mem", O_RDWR|O_SYNC);
+	
+	if (mem_fd < 0)
 	{
-		printf("can't open /dev/mem\n");
 		return false;
 	}
 
@@ -126,14 +117,40 @@ bool setup_io()
 	);
 
 	close(mem_fd); //No need to keep mem_fd open after mmap
-	mem_fd = 0;
+	mem_fd = -1;
 
-	if (gpio_map == MAP_FAILED) {
-		printf("mmap error %d\n", (int)gpio_map);//errno also set!
+	if (gpio_map == MAP_FAILED)
+	{
 		return false;
 	}
-
-	// Always use volatile pointer!
+	
 	gpio = (volatile unsigned *)gpio_map;
+	
 	return true;
+}
+
+void GPIO::shut()
+{
+	// todo : release memory mapped memory
+}
+
+void GPIO::setInput(const int index)
+{
+	INP_GPIO(index);
+}
+
+void GPIO::setOutput(const int index)
+{
+	INP_GPIO(index); // must use INP_GPIO before we can use OUT_GPIO
+	OUT_GPIO(index);
+}
+
+void GPIO::set(const uint32_t mask)
+{
+	gpio[7] = mask;
+}
+
+void GPIO::unset(const uint32_t mask)
+{
+	gpio[10] = mask;
 }
