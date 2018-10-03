ee169affa4d452ced9cdf7910dde2103542cbe39
Marcel Smit <marcel303@gmail.com>
2018-10-03 12:02:48 +0200

    Macos app bundle for 330-reflections app.

--- audioGraph-examples/bundle.cmake
+++ audioGraph-examples/bundle.cmake
@@ -1,8 +1,8 @@
 include(BundleUtilities)
 
 set(resource_path "/Users/thecat/Google Drive/The Grooop - The Tribe/")
-
 set(appresources_path "${CMAKE_BINARY_DIR}/Release/892-landart.app/Contents/Resources")
+
 execute_process(COMMAND mkdir "-p" "${appresources_path}")
 execute_process(COMMAND rsync "-r" "${resource_path}" "${appresources_path}")
 
@@ -10,9 +10,10 @@ FIXUP_BUNDLE("${CMAKE_BINARY_DIR}/Release/892-landart.app" "" "")
 
 #
 
+set(resource_path "/Users/thecat/Google Drive/The Grooop - The Tribe/")
 set(sdresource_path "/Users/thecat/Sexyshow/media/")
-
 set(appresources_path "${CMAKE_BINARY_DIR}/Release/890-performance.app/Contents/Resources")
+
 execute_process(COMMAND mkdir "-p" "${appresources_path}")
 execute_process(COMMAND rsync "-r" "${resource_path}" "${appresources_path}")
 
@@ -22,3 +23,13 @@ execute_process(COMMAND rsync "-r" "${sdresource_path}" "${appresources_path}/me
 #define MEDIA_PATH "/Users/thecat/Sexyshow/media/"
 
 FIXUP_BUNDLE("${CMAKE_BINARY_DIR}/Release/890-performance.app" "" "")
+
+#
+
+set(resource_path "/Users/thecat/framework/audioGraph-examples/data/")
+set(appresources_path "${CMAKE_BINARY_DIR}/Release/330-reflections.app/Contents/Resources")
+
+execute_process(COMMAND mkdir "-p" "${appresources_path}")
+execute_process(COMMAND rsync "-r" "${resource_path}" "${appresources_path}")
+
+FIXUP_BUNDLE("${CMAKE_BINARY_DIR}/Release/330-reflections.app" "" "")
