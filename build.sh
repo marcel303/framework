10dadaf65ecd3aa2125bd74066f7b10a2bf6199b
Marcel Smit <marcel303@gmail.com>
2018-10-06 10:23:18 +0200

    build.sh: - Added step to build the chibi executable. Removed hacky absolute path in script. - Added step to generate Xcode project file.

--- build.sh
+++ build.sh
@@ -1,7 +1,31 @@
-# generate cmake files using chibi
-mkdir chibi-build/cmake-files
-"/Users/thecat/Code Projects/Chibi/Debug/chibi" . chibi-build/cmake-files
+root=$PWD
+chibi_bin="./chibi-build/chibi/chibi"
 
-# build the binaries
-mkdir chibi-build/bin
+# detect operating system
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     os=linux;;
+    Darwin*)    os=mac;;
+    *)          os="unknown"
+esac
+
+# build chibi binary
+mkdir -p chibi-build/chibi
+cd chibi-build/chibi && cmake -DCMAKE_BUILD_TYPE=Release ../../chibi && cmake --build .
+cd "$root"
+
+# generate cmake files using chibi
+mkdir -p chibi-build/cmake-files
+"$chibi_bin" . chibi-build/cmake-files
+
+if [ "$os" == "mac" ]; then
+	# generate Xcode project file
+	mkdir -p chibi-build/xcode
+	cd chibi-build/xcode && cmake -G "Xcode" ../cmake-files
+	cd "$root"
+fi
+
+# build all of the libraries and example and test app binaries. this will take a while
+mkdir -p chibi-build/bin
 cd chibi-build/bin && cmake -DCMAKE_BUILD_TYPE=Release ../cmake-files && cmake --build .
+cd "$root"
