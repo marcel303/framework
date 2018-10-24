5aad05554295fcf3db8c106b937d6da8b39ee689
Marcel Smit <marcel303@gmail.com>
2018-10-24 09:48:39 +0200

    - Added uniform inspection and editing. - Create shader only once, to ensure it remembers its uniform values. - Experimental parsing of error messages. No protection against buffer overflows yet!!

--- 3rdparty/ImGuiColorTextEdit-support/realtime-shader-editing.cpp
+++ 3rdparty/ImGuiColorTextEdit-support/realtime-shader-editing.cpp
@@ -33,6 +33,8 @@ int main(int argc, char * argv[])
 		
 		Window window("Preview", 600, 600, true);
 		
+		Shader shader("shader");
+		
 		while (!framework.quitRequested)
 		{
 			framework.process();
@@ -49,17 +51,122 @@ int main(int argc, char * argv[])
 						ImGuiWindowFlags_NoResize |
 						ImGuiWindowFlags_HorizontalScrollbar);
 				{
-					Shader shader("shader");
 					std::vector<std::string> errorMessages;
 					TextEditor::ErrorMarkers errorMarkers;
 					if (shader.getErrorMessages(errorMessages))
 					{
 						for (size_t i = 0; i < errorMessages.size(); ++i)
-							errorMarkers[i + 1] = errorMessages[i];
+						{
+							char type[128];
+							int lineNumber;
+							int fileId;
+							char message[4096];
+							
+							// fixme : this is very unsafe. there is no gaurantee we won't cause a buffer overflow in either type or message buffers!
+							
+							const int n = sscanf(errorMessages[i].c_str(), "%s %d:%d: %s", type, &fileId, &lineNumber, message);
+							
+							if (n == 4 && fileId == 0 && strstr(type, "ERROR") != nullptr)
+							{
+								errorMarkers[lineNumber + 1] = message;
+							}
+						}
 					}
 					textEditor.SetErrorMarkers(errorMarkers);
 
-					textEditor.Render("TextEditor");
+					textEditor.Render("TextEditor", ImVec2(0, GFX_SY - 130));
+					
+					ImGui::BeginChild("ShaderConstants", ImVec2(0, 100));
+					{
+						const GLuint program = shader.getProgram();
+						
+						GLint numUniforms = 0;
+						glGetProgramiv(program, GL_ACTIVE_UNIFORMS, &numUniforms);
+						checkErrorGL();
+						
+						for (auto i = 0; i < numUniforms; ++i)
+						{
+							GLint size = 0;
+							GLenum type = GL_INVALID_ENUM;
+							const GLsizei kMaxNameSize = 64;
+							GLchar name[kMaxNameSize];
+							GLsizei nameSize = 0;
+							
+							glGetActiveUniform(program, (GLuint)i, kMaxNameSize, &nameSize, &size, &type, name);
+							checkErrorGL();
+							
+							if (nameSize <= 0)
+								continue;
+							
+							const GLint location = glGetUniformLocation(program, name);
+							
+							if (location == 0)
+								continue;
+							
+							ImGui::PushID(i);
+							
+							if (type == GL_FLOAT)
+							{
+								float value[1] = { 0.f };
+								glGetUniformfv(program, location, value);
+								checkErrorGL();
+								
+								if (ImGui::InputFloat(name, value))
+									shader.setImmediate(name, value[0]);
+							}
+							else if (type == GL_FLOAT_VEC2)
+							{
+								float value[2] = { 0.f };
+								glGetUniformfv(program, location, value);
+								checkErrorGL();
+								
+								if (ImGui::InputFloat2(name, value))
+									shader.setImmediate(name, value[0], value[1]);
+							}
+							else if (type == GL_FLOAT_VEC3)
+							{
+								float value[3] = { 0.f };
+								glGetUniformfv(program, location, value);
+								checkErrorGL();
+								
+								if (ImGui::InputFloat3(name, value))
+									shader.setImmediate(name, value[0], value[1], value[2]);
+							}
+							else if (type == GL_FLOAT_VEC4)
+							{
+								float value[4] = { 0.f };
+								glGetUniformfv(program, location, value);
+								checkErrorGL();
+								
+								if (ImGui::InputFloat4(name, value))
+									shader.setImmediate(name, value[0], value[1], value[2], value[3]);
+							}
+							else if (type == GL_SAMPLER_2D)
+							{
+								GLint value = 0;
+								glGetUniformiv(program, location, &value);
+								checkErrorGL();
+								
+								glActiveTexture(GL_TEXTURE0 + value);
+								checkErrorGL();
+								
+								GLint texture = 0;
+								glGetIntegerv(GL_TEXTURE_BINDING_2D, &texture);
+								checkErrorGL();
+								
+								if (ImGui::InputInt(name, &texture))
+									shader.setTexture(name, value, texture);
+								
+								glActiveTexture(GL_TEXTURE0);
+								checkErrorGL();
+							}
+							
+							clearShader();
+							
+							ImGui::PopID();
+						}
+					}
+					ImGui::EndChild();
 				}
 				ImGui::End();
 			
@@ -88,18 +195,18 @@ int main(int argc, char * argv[])
 			{
 				framework.beginDraw(0, 0, 0, 0);
 				{
-					Shader shader("shader");
+					setShader(shader);
 					shader.setImmediate("time", framework.time);
 					shader.setImmediate("mouse", mouse.x, mouse.y);
 					shader.setImmediate("mouse_down", mouse.isDown(BUTTON_LEFT));
 					
 					if (shader.isValid())
 					{
-						setShader(shader);
 						setColor(colorWhite);
 						drawRect(0, 0, window.getWidth(), window.getHeight());
-						clearShader();
 					}
+					
+					clearShader();
 				}
 				framework.endDraw();
 			}
