5a782d968d3e9eb1c2a39bcc717aa7f6a3191883
Marcel Smit <marcel303@gmail.com>
2018-12-21 16:31:20 +0100

    Win32: Build file tweaks.

--- build.bat
+++ build.bat
@@ -1,27 +1,26 @@
+@echo off
+
 rem todo : verify all submodules are synced somehow
 rem git submodule update --init --recursive
 
 set chibi_bin="chibi-build/chibi/Debug/chibi.exe"
 
-echo %chibi_bin%
-echo %~dp0
-
 rem build chibi binary
 mkdir "chibi-build\chibi"
-cd chibi-build/chibi && cmake -DCMAKE_BUILD_TYPE=Release ../../chibi && cmake --build .
-cd %~dp0
+cd chibi-build/chibi && cmake -DCMAKE_BUILD_TYPE=Release ../../chibi && cmake --build . || exit /b
+cd %~dp0 || exit /b
 
 rem generate cmake files using chibi
 mkdir "chibi-build\cmake-files"
-%chibi_bin% . chibi-build/cmake-files
-cd %~dp0
+%chibi_bin% . chibi-build/cmake-files || exit /b
+cd %~dp0 || exit /b
 
 rem generate Visual Studio project file
 mkdir "chibi-build\vs2015"
 cd chibi-build/vs2015 && cmake -G "Visual Studio 14 2015" ../cmake-files
-cd %~dp0
+cd %~dp0 || exit /b
 
 rem build all of the libraries and example and test app binaries. this will take a while
 mkdir "chibi-build\bin"
-cd chibi-build/bin && cmake -DCMAKE_BUILD_TYPE=Release ../cmake-files && cmake --build .
-cd %~dp0
+cd chibi-build/bin && cmake -DCMAKE_BUILD_TYPE=Release ../cmake-files && cmake --build . || exit /b
+cd %~dp0 || exit /b
