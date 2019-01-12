54fa698152b9e29312af892fb07592634f4a7a0e
Marcel Smit <marcel303@gmail.com>
2019-01-12 17:34:21 +0100

    Added calibration rectangle/vertices.

--- users/marcel/etherdream1/calibration.cpp
+++ users/marcel/etherdream1/calibration.cpp
@@ -24,6 +24,40 @@ void drawCalibrationImage_rectangle(LaserPoint * points, const int numPoints)
 	}
 }
 
+void drawCalibrationImage_rectanglePoints(LaserPoint * points, const int numPoints)
+{
+	const LaserPoint corners[4] =
+	{
+		{ -1.f, -1.f, 1.f, 0.f, 0.f },
+		{ +1.f, -1.f, 1.f, 1.f, 0.f },
+		{ +1.f, +1.f, 1.f, 0.f, 1.f },
+		{ -1.f, +1.f, 1.f, 1.f, 1.f },
+	};
+	
+	const int filler = numPoints * 6 / 10;
+	
+	for (int i = 0; i < filler; ++i)
+	{
+		const float t = i / float(filler - 1);
+		
+		points[i].x = cosf(t * float(M_PI) * 2.f) * .5f;
+		points[i].y = sinf(t * float(M_PI) * 2.f) * .5f;
+		
+		points[i].r = 1.f;
+		points[i].g = 0.f;
+		points[i].b = 0.f;
+	}
+	
+	const int remainder = numPoints - filler;
+	
+	for (int i = filler; i < numPoints; ++i)
+	{
+		const int c = 4 * (i - filler) / remainder;
+		
+		points[i] = corners[c];
+	}
+}
+
 void drawCalibrationImage_line_vscroll(LaserPoint * points, const int numPoints, const float phase)
 {
 	for (int i = 0; i < numPoints; ++i)
