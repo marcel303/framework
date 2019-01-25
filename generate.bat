285e149b530ec336849d64b133a589ed9a31b5aa
Marcel Smit <marcel303@gmail.com>
2019-01-25 19:04:47 +0100

    - Added check to generate script to see if CMake is installed. - Added step at the end where the generated solution file is opened and selected in explorer.

--- generate.bat
+++ generate.bat
@@ -1,5 +1,12 @@
 @echo off
 
+where /q cmake
+IF ERRORLEVEL 1 (
+	echo CMake not found. Please install CMake and make sure to add its location to the system path.
+	pause
+	exit /b
+)
+
 rem todo : verify all submodules are synced somehow
 rem git submodule update --init --recursive
 
@@ -23,3 +30,5 @@ cd %~dp0 || exit /b
 mkdir "chibi-build\vs2017"
 cd chibi-build/vs2017 && cmake -G "Visual Studio 15 2017" ../cmake-files
 cd %~dp0 || exit /b
+
+%SystemRoot%\explorer.exe /select,.\chibi-build\vs2017\Project.sln
