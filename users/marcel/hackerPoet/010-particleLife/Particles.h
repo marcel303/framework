0aad0d57b343a795d0149ea9969b64eed4447129
Marcel Smit <marcel303@gmail.com>
2019-01-30 18:26:15 +0100

    - Spaces to tabs. - Added framework.shutdown call.

--- users/marcel/hackerPoet/010-particleLife/Particles.h
+++ users/marcel/hackerPoet/010-particleLife/Particles.h
@@ -5,33 +5,33 @@
 #include <vector>
 
 struct Particle {
-  float x, y;
-  float vx, vy;
-  uint8_t type;
+	float x, y;
+	float vx, vy;
+	uint8_t type;
 };
 
 class ParticleTypes {
 public:
-  void Resize(size_t size) {
-    m_col.resize(size);
-    m_attract.resize(size * size);
-    m_minR.resize(size * size);
-    m_maxR.resize(size * size);
-  }
+	void Resize(size_t size) {
+		m_col.resize(size);
+		m_attract.resize(size * size);
+		m_minR.resize(size * size);
+		m_maxR.resize(size * size);
+	}
 
-  size_t Size() const { return m_col.size(); }
-  const ::Color& Color(size_t i) const { return m_col[i]; }
-  ::Color& Color(size_t i) { return m_col[i]; }
-  float Attaract(size_t i, size_t j) const { return m_attract[i*m_col.size() + j]; }
-  float& Attaract(size_t i, size_t j) { return m_attract[i*m_col.size() + j]; }
-  float MinR(size_t i, size_t j) const { return m_minR[i*m_col.size() + j]; }
-  float& MinR(size_t i, size_t j) { return m_minR[i*m_col.size() + j]; }
-  float MaxR(size_t i, size_t j) const { return m_maxR[i*m_col.size() + j]; }
-  float& MaxR(size_t i, size_t j) { return m_maxR[i*m_col.size() + j]; }
+	size_t Size() const { return m_col.size(); }
+	const ::Color& Color(size_t i) const { return m_col[i]; }
+	::Color& Color(size_t i) { return m_col[i]; }
+	float Attaract(size_t i, size_t j) const { return m_attract[i*m_col.size() + j]; }
+	float& Attaract(size_t i, size_t j) { return m_attract[i*m_col.size() + j]; }
+	float MinR(size_t i, size_t j) const { return m_minR[i*m_col.size() + j]; }
+	float& MinR(size_t i, size_t j) { return m_minR[i*m_col.size() + j]; }
+	float MaxR(size_t i, size_t j) const { return m_maxR[i*m_col.size() + j]; }
+	float& MaxR(size_t i, size_t j) { return m_maxR[i*m_col.size() + j]; }
 
 private:
-  std::vector<::Color> m_col;
-  std::vector<float> m_attract;
-  std::vector<float> m_minR;
-  std::vector<float> m_maxR;
+	std::vector<::Color> m_col;
+	std::vector<float> m_attract;
+	std::vector<float> m_minR;
+	std::vector<float> m_maxR;
 };
--- users/marcel/hackerPoet/010-particleLife/Universe.h
+++ users/marcel/hackerPoet/010-particleLife/Universe.h
@@ -5,48 +5,48 @@
 
 class Universe {
 public:
-  Universe(size_t num_types, size_t num_particles, int width, int height);
+	Universe(size_t num_types, size_t num_particles, int width, int height);
 
-  void ReSeed(float attract_mean, float attract_std, float minr_lower, float minr_upper,
-    float maxr_lower, float maxr_upper, float friction, bool flat_force);
+	void ReSeed(float attract_mean, float attract_std, float minr_lower, float minr_upper,
+		float maxr_lower, float maxr_upper, float friction, bool flat_force);
 
-  void SetPopulation(size_t num_types, size_t num_particles);
-  void SetSize(float width, float height) { m_width = width; m_height = height; }
-  void SetRandomTypes();
-  void SetRandomParticles();
-  void ToggleWrap() { m_wrap = !m_wrap; }
+	void SetPopulation(size_t num_types, size_t num_particles);
+	void SetSize(float width, float height) { m_width = width; m_height = height; }
+	void SetRandomTypes();
+	void SetRandomParticles();
+	void ToggleWrap() { m_wrap = !m_wrap; }
 
-  void Step();
-  void Draw(float opacity) const;
-  void Zoom(float cx, float cy, float zoom);
+	void Step();
+	void Draw(float opacity) const;
+	void Zoom(float cx, float cy, float zoom);
 
-  int GetIndex(int x, int y) const;
-  float GetParticleX(int index) const;
-  float GetParticleY(int index) const;
-  void ToCenter(int x, int y, float& cx, float& cy) const;
+	int GetIndex(int x, int y) const;
+	float GetParticleX(int index) const;
+	float GetParticleY(int index) const;
+	void ToCenter(int x, int y, float& cx, float& cy) const;
 
-  void PrintParams() const;
+	void PrintParams() const;
 
 private:
-  float m_center_x;
-  float m_center_y;
-  float m_zoom;
-  bool m_wrap;
+	float m_center_x;
+	float m_center_y;
+	float m_zoom;
+	bool m_wrap;
 
-  std::vector<Particle> m_particles;
-  ParticleTypes m_types;
-  float m_width;
-  float m_height;
+	std::vector<Particle> m_particles;
+	ParticleTypes m_types;
+	float m_width;
+	float m_height;
 
-  std::mt19937 m_rand_gen;
+	std::mt19937 m_rand_gen;
 
-  //Random settings
-  float m_attract_mean;
-  float m_attract_std;
-  float m_minr_lower;
-  float m_minr_upper;
-  float m_maxr_lower;
-  float m_maxr_upper;
-  float m_friction;
-  bool m_flat_force;
+	//Random settings
+	float m_attract_mean;
+	float m_attract_std;
+	float m_minr_lower;
+	float m_minr_upper;
+	float m_maxr_lower;
+	float m_maxr_upper;
+	float m_friction;
+	bool m_flat_force;
 };
