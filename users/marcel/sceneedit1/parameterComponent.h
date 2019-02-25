60fb5aab226c17191db3fe272d770b6c67d199bd
Marcel Smit <marcel303@gmail.com>
2019-02-25 12:34:40 +0100

    sceneedit: Adding custom impl int and float parameters, so they can have limits set on them.

--- users/marcel/sceneedit1/parameterComponent.h
+++ users/marcel/sceneedit1/parameterComponent.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "component.h"
+#include "Vec2.h"
+#include "Vec3.h"
+#include "Vec4.h"
 #include <string>
 #include <typeindex>
 #include <vector>
@@ -21,7 +24,7 @@ struct ParameterBase
 	ParameterType type;
 	std::string name;
 	
-	ParameterBase(const ParameterType in_type, const std::string & in_name)
+	explicit ParameterBase(const ParameterType in_type, const std::string & in_name)
 		: type(in_type)
 		, name(in_name)
 	{
@@ -36,7 +39,7 @@ struct Parameter : ParameterBase
 	T value;
 	T defaultValue;
 	
-	Parameter(const char * name, const T & in_defaultValue)
+	explicit Parameter(const char * name, const T & in_defaultValue)
 		: ParameterBase(kType, name)
 		, value(in_defaultValue)
 		, defaultValue(in_defaultValue)
@@ -49,13 +52,71 @@ struct Parameter : ParameterBase
 	}
 };
 
-typedef Parameter<bool, kParameterType_Bool> ParameterBool;
-typedef Parameter<bool, kParameterType_Int> ParameterInt;
-typedef Parameter<bool, kParameterType_Float> ParameterFloat;
-typedef Parameter<bool, kParameterType_Vec3> ParameterVec2;
-typedef Parameter<bool, kParameterType_Vec3> ParameterVec3;
-typedef Parameter<bool, kParameterType_Vec4> ParameterVec4;
-typedef Parameter<bool, kParameterType_String> ParameterString;
+struct ParameterBool : Parameter<bool, kParameterType_Bool>
+{
+	ParameterBool(const char * name, const bool defaultValue)
+		: Parameter(name, defaultValue)
+	{
+	}
+};
+
+struct ParameterInt : Parameter<int, kParameterType_Int>
+{
+	bool hasLimits = false;
+	int min = 0;
+	int max = 0;
+	
+	ParameterInt(const char * name, const int defaultValue)
+		: Parameter(name, defaultValue)
+	{
+	}
+	
+	ParameterInt & setLimits(const int in_min, const int in_max)
+	{
+		hasLimits = true;
+		min = in_min;
+		max = in_max;
+		
+		return *this;
+	}
+};
+
+struct ParameterFloat : Parameter<float, kParameterType_Float>
+{
+	bool hasLimits = false;
+	float min = 0.f;
+	float max = 0.f;
+	
+	float editingCurveExponential = 1.f;
+	
+	ParameterFloat(const char * name, const float defaultValue)
+		: Parameter(name, defaultValue)
+	{
+	}
+	
+	ParameterFloat & setLimits(const float in_min, const float in_max)
+	{
+		hasLimits = true;
+		min = in_min;
+		max = in_max;
+		
+		return *this;
+	}
+	
+	ParameterFloat & setEditingCurveExponential(const float in_exponential)
+	{
+		editingCurveExponential = in_exponential;
+		
+		return *this;
+	}
+};
+
+typedef Parameter<Vec2, kParameterType_Vec2> ParameterVec2;
+typedef Parameter<Vec3, kParameterType_Vec3> ParameterVec3;
+typedef Parameter<Vec4, kParameterType_Vec4> ParameterVec4;
+typedef Parameter<std::string, kParameterType_String> ParameterString;
+
+//
 
 struct ParameterComponent : Component<ParameterComponent>
 {
@@ -71,6 +132,8 @@ struct ParameterComponent : Component<ParameterComponent>
 	ParameterInt * addInt(const char * name, const int defaultValue);
 	ParameterFloat * addFloat(const char * name, const float defaultValue);
 };
+
+//
 
 struct ParameterComponentMgr : ComponentMgr<ParameterComponent>
 {
