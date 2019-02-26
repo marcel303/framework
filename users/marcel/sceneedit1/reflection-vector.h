b79caf2477533f864963413b2bf966305b7e8edc
Marcel Smit <marcel303@gmail.com>
2019-02-26 22:53:10 +0100

    sceneedit: Moving reflection support for std::vector<T> to a separate file.

---
+++ users/marcel/sceneedit1/reflection-vector.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "reflection.h"
+#include <vector>
+
+struct Member_VectorInterface : Member
+{
+	Member_VectorInterface(const char * in_name, const bool in_isVector)
+		: Member(in_name, in_isVector)
+	{
+	}
+	
+	virtual std::type_index vector_type() const = 0;
+	virtual size_t vector_size(const void * object) const = 0;
+	virtual void vector_resize(void * object, const size_t size) = 0;
+	virtual void * vector_access(void * object, const size_t index) const = 0;
+};
+
+template <typename T>
+struct Member_Vector : Member_VectorInterface
+{
+	typedef std::vector<T> VectorType;
+	
+	std::type_index typeIndex;
+	size_t offset;
+	
+	Member_Vector(const char * in_name, const size_t in_offset)
+		: Member_VectorInterface(in_name, true)
+		, typeIndex(typeid(VectorType))
+		, offset(in_offset)
+	{
+	}
+	
+	VectorType & getVector(void * object) const
+	{
+		const uintptr_t address = (uintptr_t)object;
+		
+		object = (void*)(address + offset);
+		
+		return *(VectorType*)object;
+	}
+	
+	const VectorType & getVector(const void * object) const
+	{
+		const uintptr_t address = (uintptr_t)object;
+		
+		object = (void*)(address + offset);
+		
+		return *(VectorType*)object;
+	}
+	
+	virtual std::type_index vector_type() const
+	{
+		return std::type_index(typeid(T));
+	}
+	
+	virtual size_t vector_size(const void * object) const
+	{
+		return getVector(object).size();
+	}
+	
+	virtual void vector_resize(void * object, const size_t size)
+	{
+		return getVector(object).resize(size);
+	}
+	
+	virtual void * vector_access(void * object, const size_t index) const
+	{
+		return &getVector(object).at(index);
+	}
+};
