993626d27957807d10dab26a07e3c9c47a15b623
Marcel Smit <marcel303@gmail.com>
2019-02-27 16:15:34 +0100

    sceneedit: - Removed component property related structs. - Added support for type system member flags for setting int/float limits and editing curve.

--- users/marcel/sceneedit1/componentType.h
+++ users/marcel/sceneedit1/componentType.h
@@ -14,259 +14,9 @@ class Vec4;
 	struct ComponentJson;
 #endif
 
-struct ComponentPropertyBase;
 struct ComponentTypeBase;
 
 struct ResourcePtr;
-
-// component properties
-
-enum ComponentPropertyType
-{
-	kComponentPropertyType_Bool,
-	kComponentPropertyType_Int32,
-	kComponentPropertyType_Float,
-	kComponentPropertyType_Vec2,
-	kComponentPropertyType_Vec3,
-	kComponentPropertyType_Vec4,
-	kComponentPropertyType_String,
-	kComponentPropertyType_AngleAxis,
-	kComponentPropertyType_ResourcePtr
-};
-
-struct ComponentPropertyBase
-{
-	std::string name;
-	ComponentPropertyType type;
-	
-	ComponentPropertyBase(const char * in_name, const ComponentPropertyType in_type)
-		: name(in_name)
-		, type(in_type)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) = 0;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) = 0;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) = 0;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) = 0;
-	virtual bool from_text(ComponentBase * component, const char * text) = 0;
-};
-
-template <typename T>
-struct ComponentProperty : ComponentPropertyBase
-{
-	typedef std::function<void(ComponentBase * component, const T&)> Setter;
-	typedef std::function<T&(ComponentBase * component)> Getter;
-	
-	Getter getter;
-	Setter setter;
-	
-	ComponentProperty(const char * name, const ComponentPropertyType type)
-		: ComponentPropertyBase(name, type)
-	{
-	}
-};
-
-struct ComponentPropertyBool : ComponentProperty<bool>
-{
-	ComponentPropertyBool(const char * name)
-		: ComponentProperty<bool>(name, kComponentPropertyType_Bool)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyInt : ComponentProperty<int>
-{
-	bool hasLimits = false;
-	int min;
-	int max;
-	
-	ComponentPropertyInt(const char * name)
-		: ComponentProperty<int>(name, kComponentPropertyType_Int32)
-	{
-	}
-	
-	ComponentPropertyInt & setLimits(const int in_min, const int in_max)
-	{
-		hasLimits = true;
-		min = in_min;
-		max = in_max;
-		
-		return *this;
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyFloat : ComponentProperty<float>
-{
-	bool hasLimits = false;
-	float min;
-	float max;
-	
-	float editingCurveExponential = 1.f;
-	
-	ComponentPropertyFloat(const char * name)
-		: ComponentProperty<float>(name, kComponentPropertyType_Float)
-	{
-	}
-	
-	ComponentPropertyFloat & setLimits(const float in_min, const float in_max)
-	{
-		hasLimits = true;
-		min = in_min;
-		max = in_max;
-		
-		return *this;
-	}
-	
-	ComponentPropertyFloat & setEditingCurveExponential(const float in_exponential)
-	{
-		editingCurveExponential = in_exponential;
-		
-		return *this;
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyVec2 : ComponentProperty<Vec2>
-{
-	ComponentPropertyVec2(const char * name)
-		: ComponentProperty<Vec2>(name, kComponentPropertyType_Vec2)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyVec3 : ComponentProperty<Vec3>
-{
-	ComponentPropertyVec3(const char * name)
-		: ComponentProperty<Vec3>(name, kComponentPropertyType_Vec3)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyVec4 : ComponentProperty<Vec4>
-{
-	ComponentPropertyVec4(const char * name)
-		: ComponentProperty<Vec4>(name, kComponentPropertyType_Vec4)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyString : ComponentProperty<std::string>
-{
-	ComponentPropertyString(const char * name)
-		: ComponentProperty<std::string>(name, kComponentPropertyType_String)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyAngleAxis : ComponentProperty<AngleAxis>
-{
-	ComponentPropertyAngleAxis(const char * name)
-		: ComponentProperty<AngleAxis>(name, kComponentPropertyType_AngleAxis)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
-
-struct ComponentPropertyResourcePtr : ComponentProperty<ResourcePtr>
-{
-	ComponentPropertyResourcePtr(const char * name)
-		: ComponentProperty<ResourcePtr>(name, kComponentPropertyType_ResourcePtr)
-	{
-	}
-	
-	virtual void setToDefault(ComponentBase * component) override final;
-	
-#if ENABLE_COMPONENT_JSON
-	virtual void to_json(ComponentBase * component, ComponentJson & j) override final;
-	virtual void from_json(ComponentBase * component, const ComponentJson & j) override final;
-#endif
-
-	virtual void to_text(ComponentBase * component, std::string & text) override final;
-	virtual bool from_text(ComponentBase * component, const char * text) override final;
-};
 
 // component types
 
@@ -280,8 +30,6 @@ enum ComponentPriority
 
 struct ComponentTypeBase : StructuredType
 {
-	// todo : remove properties array, and property types defined above. we should use the type system!
-	std::vector<ComponentPropertyBase*> _properties;
 	int tickPriority = kComponentPriority_Default;
 	
 	ComponentMgrBase * componentMgr = nullptr;
@@ -289,6 +37,69 @@ struct ComponentTypeBase : StructuredType
 	ComponentTypeBase(const char * in_typeName)
 		: StructuredType(in_typeName)
 	{
+	}
+};
+
+struct ComponentMemberFlag_IntLimits : MemberFlag<ComponentMemberFlag_IntLimits>
+{
+	int min;
+	int max;
+};
+
+struct ComponentMemberFlag_FloatLimits : MemberFlag<ComponentMemberFlag_FloatLimits>
+{
+	float min;
+	float max;
+};
+
+struct ComponentMemberFlag_FloatEditorCurveExponential : MemberFlag<ComponentMemberFlag_FloatEditorCurveExponential>
+{
+	float exponential;
+};
+
+template <typename T>
+struct ComponentMemberAdder
+{
+	Member * member;
+	
+	ComponentMemberAdder(Member * in_member)
+		: member(in_member)
+	{
+	}
+	
+	ComponentMemberAdder & setLimits(const T min, const T max)
+	{
+		if (std::is_same<T, int>())
+		{
+			ComponentMemberFlag_IntLimits * limits = new ComponentMemberFlag_IntLimits();
+			limits->min = min;
+			limits->max = max;
+		
+			member->addFlag(limits);
+		}
+		else if (std::is_same<T, float>())
+		{
+			ComponentMemberFlag_FloatLimits * limits = new ComponentMemberFlag_FloatLimits();
+			limits->min = min;
+			limits->max = max;
+		
+			member->addFlag(limits);
+		}
+		
+		return *this;
+	}
+	
+	ComponentMemberAdder & setEditingCurveExponential(const T value)
+	{
+		if (std::is_same<T, float>())
+		{
+			ComponentMemberFlag_FloatEditorCurveExponential * curveExponential = new ComponentMemberFlag_FloatEditorCurveExponential();
+			curveExponential->exponential = value;
+		
+			member->addFlag(curveExponential);
+		}
+		
+		return *this;
 	}
 };
 
@@ -300,140 +111,54 @@ struct ComponentType : ComponentTypeBase
 	{
 	}
 	
-	ComponentPropertyBool & in(const char * name, bool T::* member)
+	void in(const char * name, bool T::* member)
+	{
+		add(name, member);
+	}
+	
+	ComponentMemberAdder<int> in(const char * name, int T::* member)
 	{
 		add(name, member);
 		
-		//
-		
-		auto p = new ComponentPropertyBool(name);
-		p->getter = [=](ComponentBase * comp) -> bool & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const bool & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
+		return ComponentMemberAdder<int>(members_tail);
 	}
 	
-	ComponentPropertyInt & in(const char * name, int T::* member)
+	ComponentMemberAdder<float> in(const char * name, float T::* member)
 	{
 		add(name, member);
 		
-		//
-		
-		auto p = new ComponentPropertyInt(name);
-		p->getter = [=](ComponentBase * comp) -> int & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const int & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
+		return ComponentMemberAdder<float>(members_tail);
 	}
 	
-	ComponentPropertyFloat & in(const char * name, float T::* member)
+	void in(const char * name, Vec2 T::* member)
 	{
 		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyFloat(name);
-		p->getter = [=](ComponentBase * comp) -> float & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const float & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
 	}
 	
-	ComponentPropertyVec2 & in(const char * name, Vec2 T::* member)
+	void in(const char * name, Vec3 T::* member)
 	{
 		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyVec2(name);
-		p->getter = [=](ComponentBase * comp) -> Vec2 & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const Vec2 & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
 	}
 	
-	ComponentPropertyVec3 & in(const char * name, Vec3 T::* member)
+	void in(const char * name, Vec4 T::* member)
 	{
 		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyVec3(name);
-		p->getter = [=](ComponentBase * comp) -> Vec3 & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const Vec3 & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
 	}
 	
-	ComponentPropertyVec4 & in(const char * name, Vec4 T::* member)
+	void in(const char * name, std::string T::* member)
 	{
 		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyVec4(name);
-		p->getter = [=](ComponentBase * comp) -> Vec4 & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const Vec4 & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
 	}
 	
-	ComponentPropertyString & in(const char * name, std::string T::* member)
+	void in(const char * name, AngleAxis T::* member)
 	{
 		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyString(name);
-		p->getter = [=](ComponentBase * comp) -> std::string & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const std::string & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
 	}
 	
-	ComponentPropertyAngleAxis & in(const char * name, AngleAxis T::* member)
-	{
-		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyAngleAxis(name);
-		p->getter = [=](ComponentBase * comp) -> AngleAxis & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const AngleAxis & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
-	}
-	
-	ComponentPropertyResourcePtr & in(const char * name, ResourcePtr T::* member)
+	void in(const char * name, ResourcePtr T::* member)
 	{
 		// todo : add reflection type for ResourcePtr
 		
 		add(name, member);
-		
-		//
-		
-		auto p = new ComponentPropertyResourcePtr(name);
-		p->getter = [=](ComponentBase * comp) -> ResourcePtr & { return static_cast<T*>(comp)->*member; };
-		p->setter = [=](ComponentBase * comp, const ResourcePtr & s) { static_cast<T*>(comp)->*member = s; };
-		
-		_properties.push_back(p);
-		
-		return *p;
 	}
 };
