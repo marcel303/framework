ab38449dc3293318c741ee679d4be6419b485226
Marcel Smit <marcel303@gmail.com>
2019-02-27 16:37:04 +0100

    sceneedit: Consolidate component property editor.

--- users/marcel/sceneedit1/main.cpp
+++ users/marcel/sceneedit1/main.cpp
@@ -5,6 +5,7 @@
 #include "transformComponent.h"
 
 #include "component.h"
+#include "componentPropertyUi.h"
 #include "componentType.h"
 #include "framework.h"
 #include "imgui-framework.h"
@@ -303,130 +304,8 @@ struct SceneEditor
 					
 					for (auto * member = componentType->members_head; member != nullptr; member = member->next)
 					{
-						if (member->isVector == false)
-						{
-							auto * member_scalar = static_cast<Member_Scalar*>(member);
-							
-							auto * member_type = g_typeDB.findType(member_scalar->typeIndex);
-							auto * member_object = member_scalar->scalar_access(component);
-							
-							if (member_type->isStructured == false)
-							{
-								const PlainType * plain_type = static_cast<const PlainType*>(member_type);
-								
-								switch (plain_type->dataType)
-								{
-								case kDataType_Bool:
-									{
-										auto & value = plain_type->access<bool>(member_object);
-										
-										if (ImGui::Checkbox(member->name, &value))
-											component->propertyChanged(&value);
-									}
-									break;
-								case kDataType_Int:
-									{
-										auto & value = plain_type->access<int>(member_object);
-										
-										auto * limits = member->findFlag<ComponentMemberFlag_IntLimits>();
-										
-										if (limits != nullptr)
-										{
-											if (ImGui::SliderInt(member->name, &value, limits->min, limits->max))
-												component->propertyChanged(&value);
-										}
-										else
-										{
-											if (ImGui::InputInt(member->name, &value))
-												component->propertyChanged(&value);
-										}
-									}
-									break;
-								case kDataType_Float:
-									{
-										auto & value = plain_type->access<float>(member_object);
-										
-										auto * limits = member->findFlag<ComponentMemberFlag_FloatLimits>();
-			
-										if (limits != nullptr)
-										{
-											auto * curveExponential = member->findFlag<ComponentMemberFlag_FloatEditorCurveExponential>();
-											
-											if (ImGui::SliderFloat(member->name, &value, limits->min, limits->max, "%.3f",
-												curveExponential == nullptr ? 1.f : curveExponential->exponential))
-												component->propertyChanged(&value);
-										}
-										else
-										{
-											if (ImGui::InputFloat(member->name, &value))
-												component->propertyChanged(&value);
-										}
-									}
-									break;
-								case kDataType_Vec2:
-									{
-										auto & value = plain_type->access<Vec2>(member_object);
-							
-										if (ImGui::InputFloat2(member->name, &value[0]))
-											component->propertyChanged(&value);
-									}
-									break;
-								case kDataType_Vec3:
-									{
-										auto & value = plain_type->access<Vec3>(member_object);
-							
-										if (ImGui::InputFloat3(member->name, &value[0]))
-											component->propertyChanged(&value);
-									}
-									break;
-								case kDataType_Vec4:
-									{
-										auto & value = plain_type->access<Vec4>(member_object);
-							
-										if (ImGui::InputFloat4(member->name, &value[0]))
-											component->propertyChanged(&value);
-									}
-									break;
-								case kDataType_String:
-									{
-										auto & value = plain_type->access<std::string>(member_object);
-							
-										char buffer[1024];
-										strcpy_s(buffer, sizeof(buffer), value.c_str());
-										
-										if (ImGui::InputText(member->name, buffer, sizeof(buffer)))
-										{
-											value = buffer;
-											
-											component->propertyChanged(&value);
-										}
-									}
-									break;
-								default:
-									// todo : remove and replace with structured type
-									if (strcmp(plain_type->typeName, "AngleAxis") == 0)
-									{
-										auto & value = plain_type->access<AngleAxis>(member_object);
-										
-										if (ImGui::SliderAngle(member->name, &value.angle))
-											component->propertyChanged(&value);
-										ImGui::PushID(&value.axis);
-										if (ImGui::SliderFloat3(member->name, &value.axis[0], -1.f, +1.f))
-											component->propertyChanged(&value);
-										ImGui::PopID();
-									}
-									break;
-								}
-							}
-							else
-							{
-								// todo : allow editing of structured data
-							}
-						}
-						else
-						{
-							// todo : allow editing of arrays
-						}
+						bool isSet = true;
+						doComponentProperty(member, component, true, isSet, nullptr);
 					}
 				}
 			}
