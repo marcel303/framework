5c5ebf2ece47ce9fa542a504a633f31df2d80e9c
Marcel Smit <marcel303@gmail.com>
2019-02-28 11:07:48 +0100

    Linux compile fix.

--- users/marcel/cuber/script.h
+++ users/marcel/cuber/script.h
@@ -3,5 +3,14 @@
 class EffectCtx;
 class Effect;
 
+#if defined(WIN32)
+
 typedef Effect * (__cdecl * CreateFunction)(EffectCtx & ctx);
 typedef void (__cdecl * DestroyFunction)(Effect * effect);
+
+#else
+
+typedef Effect * (* CreateFunction)(EffectCtx & ctx);
+typedef void (* DestroyFunction)(Effect * effect);
+
+#endif
