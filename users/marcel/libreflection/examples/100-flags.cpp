bf9cef96086103173a255d831eebf272d020aeb7
Marcel Smit <marcel303@gmail.com>
2019-04-06 20:05:55 +0200

    libreflection: Make flags example app at least compile.

--- users/marcel/libreflection/examples/100-flags.cpp
+++ users/marcel/libreflection/examples/100-flags.cpp
@@ -1,7 +1,16 @@
 #include "reflection.h"
 #include <stdio.h>
+#include <vector>
 
-struct ChromaticSomething // aka rainbow
+/*
+
+This example shows how to fill a type database,
+query a type from the database, and iterate the
+members of a structured type.
+
+*/
+
+struct ChromaticSomething // aka a rainbow
 {
 	bool chromatic = true;
 	std::vector<int> hues;
@@ -17,11 +26,26 @@ int main(int argc, char * argv[])
 	typeDB.addPlain<float>("float", kDataType_Float);
 		
 	typeDB.addStructured<ChromaticSomething>("ChromaticSomething")
-		.add("chromatic", &InterstellarRainbow::chromatic)
-		.add("hues", &InterstellarRainbow::hues)
-		.add("density", &InterstellarRainbow::density);
+		.add("chromatic", &ChromaticSomething::chromatic)
+		.add("hues", &ChromaticSomething::hues)
+		.add("density", &ChromaticSomething::density);
 
 	// we now have a type database. whoohoo!
+	// let's fetch a type and inspect it
+	
+	const Type * type = typeDB.findType<ChromaticSomething>();
+	
+	if (type->isStructured)
+	{
+		const StructuredType * structured_type = static_cast<const StructuredType*>(type);
+		
+		printf("type: %s\n", structured_type->typeName);
+		
+		for (const Member * member = structured_type->members_head; member != nullptr; member = member->next)
+		{
+			printf("\tmember: %s\n", member->name);
+		}
+	}
 
 	return 0;
 }
