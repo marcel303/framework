122cb85bf87216e7e503b6af14229dd0f1669a29
Marcel Smit <marcel303@gmail.com>
2019-04-20 13:11:18 +0200

    Coding challenge 132: - Use GxTexture so we can use swizzle and don't alloc/free the texture all the time. - Scale the texture to fill the screen. - Slightly more efficient linear solver. Still takes 90+% of the time. - Removed some weird stuff from project2d (less multiplications, more efficient setting to zero of p array; hopefully without changing behavior). - Added protection against writing out of bounds addDensity and addVelocity methods.

--- users/marcel/codingChallenges/132-fluidSimulation.cpp
+++ users/marcel/codingChallenges/132-fluidSimulation.cpp
@@ -1,12 +1,14 @@
 #include "framework.h"
 #include <vector>
 
+#define SCALE 3
+
 #define THREE_DIMENSIONAL 0
 
 #define IX_2D(x, y) ((x) + (y) * N)
 #define IX_3D(x, y, z) ((x) + (y) * N + (z) * N * N)
 
-static void set_bnd2d(int b, float *x, int N)
+static void set_bnd2d(const int b, float * x, const int N)
 {
 	for (int i = 1; i < N - 1; ++i)
 	{
@@ -26,7 +28,7 @@ static void set_bnd2d(int b, float *x, int N)
     x[IX_2D(N-1, N-1)]   = 0.5f * (x[IX_2D(N-2, N-1)] + x[IX_2D(N-1, N-2)]);
 }
 
-static void set_bnd3d(int b, float *x, int N)
+static void set_bnd3d(const int b, float * x, const int N)
 {
     for(int j = 1; j < N - 1; j++) {
         for(int i = 1; i < N - 1; i++) {
@@ -75,7 +77,7 @@ static void set_bnd3d(int b, float *x, int N)
                                   + x[IX_3D(N-1, N-1, N-2)]);
 }
 
-static void lin_solve2d(const int b, float * x, float * x0, const float a, const float c, const int iter, const int N)
+static void lin_solve2d(const int b, float * x, const float * x0, const float a, const float c, const int iter, const int N)
 {
     float cRecip = 1.f / c;
 
@@ -83,8 +85,27 @@ static void lin_solve2d(const int b, float * x, float * x0, const float a, const
     {
 		for (int j = 1; j < N - 1; ++j)
 		{
+			const float * prev_line = x0 + IX_2D(0, j    );
+			const float * src_line1 = x  + IX_2D(0, j - 1);
+			const float * src_line2 = x  + IX_2D(0, j    );
+			const float * src_line3 = x  + IX_2D(0, j + 1);
+			      float * dest_line = x  + IX_2D(0, j    );
+			
 			for (int i = 1; i < N - 1; ++i)
 			{
+			#if 1
+				dest_line[i] =
+					(
+						prev_line[i]
+						+ a *
+							(
+								+src_line2[i + 1]
+								+src_line2[i - 1]
+								+src_line3[i    ]
+								+src_line1[i    ]
+							)
+					) * cRecip;
+			#else
 				x[IX_2D(i, j)] =
 					(
 						x0[IX_2D(i, j)]
@@ -96,6 +117,7 @@ static void lin_solve2d(const int b, float * x, float * x0, const float a, const
 								+x[IX_2D(i  , j-1)]
 							)
 					) * cRecip;
+			#endif
 			}
 		}
 
@@ -103,7 +125,7 @@ static void lin_solve2d(const int b, float * x, float * x0, const float a, const
     }
 }
 
-static void lin_solve3d(const int b, float * x, float * x0, const float a, const float c, const int iter, const int N)
+static void lin_solve3d(const int b, float * x, const float * x0, const float a, const float c, const int iter, const int N)
 {
     float cRecip = 1.f / c;
 
@@ -128,13 +150,13 @@ static void lin_solve3d(const int b, float * x, float * x0, const float a, const
     }
 }
 
-static void diffuse2d(const int b, float * x, float * x0, const float diff, const float dt, const int iter, const int N)
+static void diffuse2d(const int b, float * x, const float * x0, const float diff, const float dt, const int iter, const int N)
 {
 	const float a = dt * diff * (N - 2);
 	lin_solve2d(b, x, x0, a, 1 + 4 * a, iter, N);
 }
 
-static void diffuse3d(const int b, float * x, float * x0, const float diff, const float dt, const int iter, const int N)
+static void diffuse3d(const int b, float * x, const float * x0, const float diff, const float dt, const int iter, const int N)
 {
 	const float a = dt * diff * (N - 2) * (N - 2);
 	lin_solve3d(b, x, x0, a, 1 + 6 * a, iter, N);
@@ -147,27 +169,27 @@ static void project2d(float * velocX, float * velocY, float * p, float * div, co
 		for (int i = 1; i < N - 1; ++i)
 		{
 			div[IX_2D(i, j)] =
-				-0.5f *
+				-0.255f *
 					(
 						+ velocX[IX_2D(i+1, j  )]
 						- velocX[IX_2D(i-1, j  )]
 						+ velocY[IX_2D(i  , j+1)]
 						- velocY[IX_2D(i  , j-1)]
-				) / N;
-			p[IX_2D(i, j)] = 0;
+					);
 		}
 	}
 	
     set_bnd2d(0, div, N);
-    set_bnd2d(0, p, N);
+	
+    memset(p, 0, N * N * sizeof(float));
 	lin_solve2d(0, p, div, 1, 4, iter, N);
 	
 	for (int j = 1; j < N - 1; ++j)
 	{
 		for (int i = 1; i < N - 1; ++i)
 		{
-			velocX[IX_2D(i, j)] -= 0.5f * ( p[IX_2D(i+1, j)] - p[IX_2D(i-1, j)] ) * N;
-			velocY[IX_2D(i, j)] -= 0.5f * ( p[IX_2D(i, j+1)] - p[IX_2D(i, j-1)] ) * N;
+			velocX[IX_2D(i, j)] -= ( p[IX_2D(i+1, j)] - p[IX_2D(i-1, j)] );
+			velocY[IX_2D(i, j)] -= ( p[IX_2D(i, j+1)] - p[IX_2D(i, j-1)] );
 		}
 	}
 
@@ -212,13 +234,8 @@ static void project3d(float * velocX, float * velocY, float * velocZ, float * p,
 
 static void advect2d(int b, float * d, float * d0, float * velocX, float * velocY, float dt, int N)
 {
-    float i0, i1, j0, j1;
-	
-    float dtx = dt * (N - 2);
-    float dty = dt * (N - 2);
-	
-    float s0, s1, t0, t1;
-    float tmp1, tmp2, x, y;
+    const float dtx = dt * (N - 2);
+    const float dty = dt * (N - 2);
 	
     float Nfloat = N;
     float ifloat, jfloat;
@@ -228,30 +245,34 @@ static void advect2d(int b, float * d, float * d0, float * velocX, float * veloc
 	{
 		for (i = 1, ifloat = 1; i < N - 1; ++i, ++ifloat)
 		{
-			tmp1 = dtx * velocX[IX_2D(i, j)];
-			tmp2 = dty * velocY[IX_2D(i, j)];
+			const float tmp1 = dtx * velocX[IX_2D(i, j)];
+			const float tmp2 = dty * velocY[IX_2D(i, j)];
 			
-			x    = ifloat - tmp1;
-			y    = jfloat - tmp2;
+			float x = ifloat - tmp1;
+			float y = jfloat - tmp2;
 			
 			if(x < 0.5f) x = 0.5f;
 			if(x > Nfloat + 0.5f) x = Nfloat + 0.5f;
+			float i0, i1;
 			i0 = floorf(x);
 			i1 = i0 + 1.0f;
+			
 			if(y < 0.5f) y = 0.5f;
 			if(y > Nfloat + 0.5f) y = Nfloat + 0.5f;
+			float j0, j1;
 			j0 = floorf(y);
 			j1 = j0 + 1.0f;
 			
+			float s0, s1, t0, t1;
 			s1 = x - i0;
 			s0 = 1.0f - s1;
 			t1 = y - j0;
 			t0 = 1.0f - t1;
 			
-			int i0i = i0;
-			int i1i = i1;
-			int j0i = j0;
-			int j1i = j1;
+			const int i0i = i0;
+			const int i1i = i1;
+			const int j0i = j0;
+			const int j1i = j1;
 			
 			d[IX_2D(i, j)] =
 				s0 * (t0 * d0[IX_2D(i0i, j0i)] + t1 * d0[IX_2D(i0i, j1i)]) +
@@ -351,6 +372,12 @@ struct FluidCube
 
 	void addDensity2d(const int x, const int y, const float amount)
 	{
+		if (x < 0 || x >= size ||
+			y < 0 || y >= size)
+		{
+			return;
+		}
+		
 		const int N = size;
 		
 		density[IX_2D(x, y)] += amount;
@@ -358,6 +385,13 @@ struct FluidCube
 	
 	void addDensity3d(const int x, const int y, const int z, const float amount)
 	{
+		if (x < 0 || x >= size ||
+			y < 0 || y >= size ||
+			z < 0 || z >= size)
+		{
+			return;
+		}
+		
 		const int N = size;
 		
 		density[IX_3D(x, y, z)] += amount;
@@ -365,6 +399,12 @@ struct FluidCube
 
 	void addVelocity2d(const int x, const int y, const float amountX, const float amountY)
 	{
+		if (x < 0 || x >= size ||
+			y < 0 || y >= size)
+		{
+			return;
+		}
+		
 		const int N = size;
 		const int index = IX_2D(x, y);
 
@@ -374,6 +414,13 @@ struct FluidCube
 	
 	void addVelocity3d(const int x, const int y, const int z, const float amountX, const float amountY, const float amountZ)
 	{
+		if (x < 0 || x >= size ||
+			y < 0 || y >= size ||
+			z < 0 || z >= size)
+		{
+			return;
+		}
+		
 		const int N = size;
 		const int index = IX_3D(x, y, z);
 
@@ -385,35 +432,37 @@ struct FluidCube
 	void step()
 	{
 	    const int N = size;
+		
+	    const int iter = 4;
 	
 	#if THREE_DIMENSIONAL
-		diffuse3d(1, Vx0.data(), Vx.data(), visc, dt, 4, N);
-	    diffuse3d(2, Vy0.data(), Vy.data(), visc, dt, 4, N);
-	    diffuse3d(3, Vz0.data(), Vz.data(), visc, dt, 4, N);
+		diffuse3d(1, Vx0.data(), Vx.data(), visc, dt, iter, N);
+	    diffuse3d(2, Vy0.data(), Vy.data(), visc, dt, iter, N);
+	    diffuse3d(3, Vz0.data(), Vz.data(), visc, dt, iter, N);
 		
-		project3d(Vx0.data(), Vy0.data(), Vz0.data(), Vx.data(), Vy.data(), 4, N);
+		project3d(Vx0.data(), Vy0.data(), Vz0.data(), Vx.data(), Vy.data(), iter, N);
 		
 		advect3d(1, Vx.data(), Vx0.data(), Vx0.data(), Vy0.data(), Vz0.data(), dt, N);
 	    advect3d(2, Vy.data(), Vy0.data(), Vx0.data(), Vy0.data(), Vz0.data(), dt, N);
 	    advect3d(3, Vz.data(), Vz0.data(), Vx0.data(), Vy0.data(), Vz0.data(), dt, N);
 		
-		project3d(Vx.data(), Vy.data(), Vz.data(), Vx0.data(), Vy0.data(), 4, N);
+		project3d(Vx.data(), Vy.data(), Vz.data(), Vx0.data(), Vy0.data(), iter, N);
 		
-		diffuse3d(0, s.data(), density.data(), diff, dt, 4, N);
+		diffuse3d(0, s.data(), density.data(), diff, dt, iter, N);
 		
 	    advect3d(0, density.data(), s.data(), Vx.data(), Vy.data(), Vz.data(), dt, N);
 	#else
-		diffuse2d(1, Vx0.data(), Vx.data(), visc, dt, 4, N);
-	    diffuse2d(2, Vy0.data(), Vy.data(), visc, dt, 4, N);
+		diffuse2d(1, Vx0.data(), Vx.data(), visc, dt, iter, N);
+	    diffuse2d(2, Vy0.data(), Vy.data(), visc, dt, iter, N);
 		
-		project2d(Vx0.data(), Vy0.data(), Vx.data(), Vy.data(), 4, N);
+		project2d(Vx0.data(), Vy0.data(), Vx.data(), Vy.data(), iter, N);
 		
 		advect2d(1, Vx.data(), Vx0.data(), Vx0.data(), Vy0.data(), dt, N);
 	    advect2d(2, Vy.data(), Vy0.data(), Vx0.data(), Vy0.data(), dt, N);
 		
-		project2d(Vx.data(), Vy.data(), Vx0.data(), Vy0.data(), 4, N);
+		project2d(Vx.data(), Vy.data(), Vx0.data(), Vy0.data(), iter, N);
 		
-		diffuse2d(0, s.data(), density.data(), diff, dt, 4, N);
+		diffuse2d(0, s.data(), density.data(), diff, dt, iter, N);
 		
 		advect2d(0, density.data(), s.data(), Vx.data(), Vy.data(), dt, N);
 	#endif
@@ -454,10 +503,14 @@ void freeFluidCube(FluidCube *& cube)
 
 int main(int argc, const char * argv[])
 {
-	if (!framework.init(800, 600))
+	if (!framework.init(600, 600))
 		return -1;
 
-	FluidCube * cube = createFluidCube(256, 0.0f, 0.0f, 1.f / 60.f);
+	FluidCube * cube = createFluidCube(200, 0.01f, 0.001f, 1.f / 30.f);
+	
+	GxTexture texture;
+	texture.allocate(200, 200, GX_R32_FLOAT, false, true);
+	texture.setSwizzle(0, 0, 0, GX_SWIZZLE_ONE);
 	
 	for (;;)
 	{
@@ -473,8 +526,8 @@ int main(int argc, const char * argv[])
 		{
 			for (int y = -4; y <= +4; ++y)
 			{
-				cube->addDensity2d(mouse.x + x, mouse.y + y, 1.f);
-				cube->addVelocity2d(mouse.x, mouse.y, mouse.dx / 100.f, mouse.dy / 100.f);
+				cube->addDensity2d(mouse.x / SCALE + x, mouse.y / SCALE + y, 1.f);
+				cube->addVelocity2d(mouse.x / SCALE, mouse.y / SCALE, mouse.dx / 100.f, mouse.dy / 100.f);
 			}
 		}
 		
@@ -482,19 +535,19 @@ int main(int argc, const char * argv[])
 		
 		framework.beginDraw(255, 255, 255, 0);
 		{
-			auto texture = createTextureFromR32F(
-				keyboard.isDown(SDLK_v)
-				? cube->Vx.data()
-				: cube->density.data(), cube->size, cube->size, false, true);
-			gxSetTexture(texture);
-			drawRect(0, 0, cube->size, cube->size);
+			texture.upload(cube->density.data(), 4, 0);
+			gxSetTexture(texture.id);
+			drawRect(0, 0, cube->size * SCALE, cube->size * SCALE);
 			gxSetTexture(0);
-			freeTexture(texture);
 		}
 		framework.endDraw();
 	}
+	
+	texture.free();
 
 	freeFluidCube(cube);
 
 	framework.shutdown();
+	
+	return 0;
 }
