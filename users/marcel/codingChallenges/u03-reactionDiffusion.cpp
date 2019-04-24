87ca31e88c4ebed518838f793096476d00afcc94
Marcel Smit <marcel303@gmail.com>
2019-04-24 11:28:50 +0200

    Coding challenge #u03: - Added comments. - Added various constants for various reaction-diffusion effects. - Made initial seeding more clear.

--- users/marcel/codingChallenges/u03-reactionDiffusion.cpp
+++ users/marcel/codingChallenges/u03-reactionDiffusion.cpp
@@ -1,5 +1,17 @@
 #include "framework.h"
 
+const int GFX_SX = 400;
+const int GFX_SY = 400;
+
+/*
+Shader based Reaction-Diffusion algorithm.
+At its core it uses a shader which runs for all pixels of a surface.
+The surface itself is double-buffered, meaning it can be used in a ping-pong fashion
+to store both the previous frame (read-only) the the new frame (written to by the shader).
+
+http://karlsims.com/rd.html
+*/
+
 static const char * ps = R"SHADER(
 
 include engine/ShaderPS.txt
@@ -35,38 +47,76 @@ void main()
 	
 	vec2 laplacian = laplacian2D(texelCoord);
 
-	float A_1 = A + (0.9  * laplacian.x - A * B * B + 0.0545 * (1 - A));
-	float B_1 = B + (0.18 * laplacian.y + A * B * B - (0.062 + 0.0545) * B);
+// A' = A + (dA * laplacianA) - (A * B * B) + (feed * (1 - A))
+// B' = B + (dB * laplacianB) + (A * B * B) - ((k + feed) * B)
 
-	shader_fragColor = vec4(A_1, B_1, 1.0, 1.0);
+// where k is the kill rate
+
+#if 1
+	// interpolating feed and k values over the screen
+	float dA = 1.0;
+	float dB = 0.5;
+	float feed = mix(0.03, 0.06, v_texcoord0.y);
+	float k = mix(0.055, 0.07, v_texcoord0.x);
+#elif 0
+	// 'common values'
+	float dA = 1.0;
+	float dB = 0.5;
+	float feed = 0.055;
+	float k = 0.062;
+#elif 1
+	// 'mitosis'
+	float dA = 1.0;
+	float dB = 0.45;
+	float feed = 0.0367;
+	float k = 0.0649;
+#elif 0
+	// 'coral growth'
+	float dA = 1.0;
+	float dB = 0.45;
+	float feed = 0.0545;
+	float k = 0.062;
+#endif
+
+	float dT = 1.0;
+
+	float A_1 = A + ((dA * laplacian.x) - (A * B * B) + ((    feed) * (1.0 - A))) * dT;
+	float B_1 = B + ((dB * laplacian.y) + (A * B * B) - ((k + feed) * (      B))) * dT;
+
+	shader_fragColor = vec4(A_1, B_1, 0.0, 0.0);
 }
 
 )SHADER";
 
 int main(int argc, const char * argv[])
 {
-	if (!framework.init(800, 600))
+	if (!framework.init(GFX_SX, GFX_SY))
 		return -1;
 	
-	Surface surface(800, 600, false, true, SURFACE_RG16F);
+	Surface surface(GFX_SX, GFX_SY, false, true, SURFACE_RG16F);
 	surface.setSwizzle(0, 1, GX_SWIZZLE_ONE, GX_SWIZZLE_ONE);
 	
 	auto restart = [&]()
 	{
-		surface.clear(255, 0, 0);
-		surface.swapBuffers();
-		
 		pushSurface(&surface);
 		{
-			surface.clear();
+			// seed A with 1.0
+			
+			surface.clearf(1, 0, 0);
+			
+			// seed a small area A with 0.0 and B with 1.0
+			
+			setColorf(0, 1, 0);
 			
 			for (int i = 0; i < 4; ++i)
 			{
-				setColorf(0, 1, 0);
 				gxPushMatrix();
-				gxTranslatef(rand() % 800, rand() % 600, 0);
-				gxScalef(random<float>(0.f, 10.f), random<float>(0.f, 10.f), 1);
-				fillCircle(0, 0, 10, 3 + (rand() % 3));
+				{
+					gxTranslatef(rand() % GFX_SX, rand() % GFX_SY, 0);
+					gxScalef(random<float>(0.f, 10.f), random<float>(0.f, 10.f), 1);
+					
+					fillCircle(0, 0, 10, 3 + (rand() % 3));
+				}
 				gxPopMatrix();
 			}
 		}
