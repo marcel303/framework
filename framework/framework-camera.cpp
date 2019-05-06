aa144e68d67fc365413096d708944c2d70fee78b
Marcel Smit <marcel303@gmail.com>
2019-05-06 16:22:53 +0200

    framework-camera: Header include optimization, copyright message.

--- framework/framework-camera.cpp
+++ framework/framework-camera.cpp
@@ -1,5 +1,31 @@
-#pragma once
+/*
+	Copyright (C) 2019 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
 
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "framework.h"
 #include "framework-camera.h"
 
 void Camera::Orbit::tick(const float dt, bool & inputIsCaptured)
@@ -266,7 +292,7 @@ void Camera::calculateProjectionMatrix(const int viewportSx, const int viewportS
 	}
 }
 
-Mat4x4 Camera::calculateViewProjectionMatrix(const int viewportSx, const int viewportSy) const
+void Camera::calculateViewProjectionMatrix(const int viewportSx, const int viewportSy, Mat4x4 & out_matrix) const
 {
 	Mat4x4 projectionMatrix;
 	Mat4x4 worldMatrix;
@@ -287,7 +313,7 @@ Mat4x4 Camera::calculateViewProjectionMatrix(const int viewportSx, const int vie
 		break;
 	}
 	
-	return projectionMatrix * worldMatrix.CalcInv();
+	out_matrix = projectionMatrix * worldMatrix.CalcInv();
 }
 
 void Camera::pushProjectionMatrix()
--- framework/framework-camera.h
+++ framework/framework-camera.h
@@ -1,6 +1,34 @@
+/*
+	Copyright (C) 2019 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #pragma once
 
-#include "framework.h"
+#include "Mat4x4.h"
+#include "Vec3.h"
 
 class Camera
 {
@@ -55,12 +83,14 @@ public:
 		}
 	};
 	
+public:
 	Mode mode = kMode_Orbit;
 	
 	Orbit orbit;
 	Ortho ortho;
 	FirstPerson firstPerson;
 	
+public:
 	void tick(const float dt, bool & inputIsCaptured);
 	
 	void calculateWorldMatrix(Mat4x4 & out_matrix) const;
@@ -68,7 +98,7 @@ public:
 
 	void calculateProjectionMatrix(const int viewportSx, const int viewportSy, Mat4x4 & out_matrix) const;
 	
-	Mat4x4 calculateViewProjectionMatrix(const int viewportSx, const int viewportSy) const;
+	void calculateViewProjectionMatrix(const int viewportSx, const int viewportSy, Mat4x4 & out_matrix) const;
 
 	void pushProjectionMatrix();
 	void popProjectionMatrix();
