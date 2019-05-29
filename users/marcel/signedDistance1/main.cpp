79750abea32ec1ed681085595a02ef0cbba086ba
Marcel Smit <marcel303@gmail.com>
2019-05-29 19:11:39 +0200

    Signed distance user app: - Correct image with Metal backend. - Enable real-time editing.

--- users/marcel/signedDistance1/main.cpp
+++ users/marcel/signedDistance1/main.cpp
@@ -4,6 +4,8 @@ int main(int argc, char * argv[])
 {
 	changeDirectory(CHIBI_RESOURCE_PATH);
 	
+	framework.enableRealTimeEditing = true;
+	
 	if (!framework.init(800, 600))
 		return -1;
 	
@@ -23,12 +25,17 @@ int main(int argc, char * argv[])
 		
 		framework.beginDraw(0, 0, 0, 0);
 		{
+			Mat4x4 viewMatrix = camera.getViewMatrix();
+		#if ENABLE_OPENGL
+			viewMatrix = Mat4x4(true).Scale(1, -1, 1) * viewMatrix;
+		#endif
+		
 			Shader shader("sd");
 			setShader(shader);
 			shader.setImmediate("u_resolution", 800.f, 600.f, 0, 0);
-			shader.setImmediateMatrix4x4("u_invView", camera.getViewMatrix().CalcInv().m_v);
-			
+			shader.setImmediateMatrix4x4("u_invView", viewMatrix.CalcInv().m_v);
 			drawRect(0, 0, 800, 600);
+			clearShader();
 		}
 		framework.endDraw();
 	}
