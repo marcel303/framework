73cb42e23fdd278d3730553ec5d5d0ea549b5013
Marcel Smit <marcel303@gmail.com>
2019-07-11 09:31:57 +0200

    imgui-framework: Add specialized ImGui::Image function, which avoid having to cast texture ids.

--- 3rdparty/imgui-framework/imgui-framework.cpp
+++ 3rdparty/imgui-framework/imgui-framework.cpp
@@ -518,3 +518,16 @@ void FrameworkImGuiContext::render(const ImDrawData * draw_data)
 	
 	clearDrawRect();
 }
+
+namespace ImGui
+{
+	void Image(
+		GxTextureId user_texture_id,
+		const ImVec2& size,
+		const ImVec2& uv0, const ImVec2& uv1,
+		const ImVec4& tint_col,
+		const ImVec4& border_col)
+	{
+		ImGui::Image((ImTextureID)(uintptr_t)user_texture_id, size, uv0, uv1, tint_col, border_col);
+	}
+}
