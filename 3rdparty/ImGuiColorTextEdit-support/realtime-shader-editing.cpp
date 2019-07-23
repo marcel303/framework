2b046c556d6a54a7895f446a330fe5745f79f5f1
Marcel Smit <marcel303@gmail.com>
2019-07-23 19:11:18 +0200

    ImGuiColorTextEdit: Refactor real-time shader editor example. And fix a compile issue in one of the example shaders.

--- 3rdparty/ImGuiColorTextEdit-support/realtime-shader-editing.cpp
+++ 3rdparty/ImGuiColorTextEdit-support/realtime-shader-editing.cpp
@@ -44,6 +44,338 @@ enum Tab
 	kTab_FileBrowser
 };
 
+struct RealtimeShaderEditor
+{
+	FrameworkImGuiContext framework_context;
+	
+	TextEditor textEditor;
+	
+	std::string shaderPs;
+	
+	Shader shader;
+	
+	std::string shaderNamePs;
+	
+	Tab tab;
+	
+	std::vector<std::string> files;
+	
+	float idleTime = 0.f;
+	float alphaAnim = 0.f;
+	
+	std::vector<SavedUniform> savedUniforms;
+	
+	std::string path;
+	bool pathIsValid = false;
+
+	TextIO::LineEndings lineEndings = TextIO::kLineEndings_Unix;
+	
+	void init()
+	{
+		framework_context.init();
+
+		textEditor.SetLanguageDefinition(TextEditor::LanguageDefinition::CPlusPlus());
+	
+		shaderSource("demoShader.vs", s_shaderVs);
+		shaderSource("demoShader.ps", s_shaderPs);
+	
+		textEditor.SetText(s_shaderPs);
+	
+		SDL_StartTextInput();
+	
+		shaderPs = s_shaderPs;
+	
+		shader = Shader("demoShader");
+		shaderNamePs = "demoShader.ps";
+	
+		tab = kTab_Uniforms;
+	
+		files = listFiles("testShaders", false);
+		auto removed = std::remove_if(files.begin(), files.end(), [](const std::string & s) { return Path::GetExtension(s, true) != "ps"; });
+		files.erase(removed, files.end());
+		std::sort(files.begin(), files.end());
+	}
+	
+	void shut()
+	{
+		SDL_StopTextInput();
+	
+		framework_context.shut();
+	}
+	
+	void tick(const float dt, bool & inputIsCaptured, const bool showEditor)
+	{
+		Assert(pathIsValid == !path.empty());
+		
+		if (keyboard.isIdle() && mouse.isIdle())
+			idleTime += framework.timeStep;
+		else
+			idleTime = 0.f;
+		
+		if (idleTime < 4.f && showEditor)
+			alphaAnim -= framework.timeStep / .5f;
+		else
+			alphaAnim += framework.timeStep / (showEditor ? 2.f : .5f);
+		
+		alphaAnim = clamp(alphaAnim, 0.f, 1.f);
+		
+		framework_context.processBegin(framework.timeStep, GFX_SX, GFX_SY, inputIsCaptured);
+		{
+			const float alpha = lerp(.9f, .0f, alphaAnim);
+			ImGui::GetStyle().Alpha = alpha;
+			
+			ImGui::SetNextWindowPos(ImVec2(0, 0));
+			ImGui::SetNextWindowSize(ImVec2(GFX_SX, GFX_SY));
+			
+			ImGui::Begin("Text Editor Demo", nullptr,
+					ImGuiWindowFlags_NoTitleBar |
+					ImGuiWindowFlags_NoResize |
+					ImGuiWindowFlags_HorizontalScrollbar |
+					ImGuiWindowFlags_MenuBar);
+			{
+				if (ImGui::BeginMenuBar())
+				{
+					if (ImGui::BeginMenu("File"))
+					{
+						if (ImGui::MenuItem("Load.."))
+						{
+							nfdchar_t * filename = nullptr;
+							
+							if (NFD_OpenDialog(nullptr, nullptr, &filename) == NFD_OKAY)
+							{
+								if (loadIntoTextEditor(filename, lineEndings, textEditor))
+								{
+									path = filename;
+									pathIsValid = true;
+								}
+								else
+								{
+									path.clear();
+									pathIsValid = false;
+								}
+							}
+							
+							if (filename != nullptr)
+							{
+								free(filename);
+								filename = nullptr;
+							}
+						}
+						
+						if (ImGui::MenuItem("Save", nullptr, false, pathIsValid))
+						{
+							if (pathIsValid)
+							{
+								auto lines = textEditor.GetTextLines();
+								
+								if (TextIO::save(path.c_str(), lines, lineEndings) == false)
+								{
+									SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Save error", "Failed to save to file", nullptr);
+								}
+							}
+						}
+						
+						if (ImGui::MenuItem("Save as.."))
+						{
+							nfdchar_t * filename = nullptr;
+							
+							if (NFD_SaveDialog(nullptr, nullptr, &filename) == NFD_OKAY)
+							{
+								auto lines = textEditor.GetTextLines();
+								
+								if (TextIO::save(filename, lines, lineEndings) == false)
+								{
+									SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Save error", "Failed to save to file", nullptr);
+									
+									path.clear();
+									pathIsValid = false;
+								}
+								else
+								{
+									path = filename;
+									pathIsValid = true;
+								}
+							}
+							
+							if (filename != nullptr)
+							{
+								free(filename);
+								filename = nullptr;
+							}
+						}
+						
+						ImGui::EndMenu();
+					}
+					
+					if (ImGui::BeginMenu("Edit"))
+					{
+						if (ImGui::MenuItem("Undo"))
+							textEditor.Undo();
+						if (ImGui::MenuItem("Redo"))
+							textEditor.Redo();
+						
+						ImGui::Separator();
+						if (ImGui::MenuItem("Copy"))
+							textEditor.Copy();
+						if (ImGui::MenuItem("Cut"))
+							textEditor.Cut();
+						if (ImGui::MenuItem("Paste"))
+							textEditor.Paste();
+						
+						ImGui::Separator();
+						if (ImGui::MenuItem("Select all"))
+							textEditor.SelectAll();
+						
+						ImGui::EndMenu();
+					}
+					
+					ImGui::EndMenuBar();
+				}
+				
+				std::vector<std::string> errorMessages;
+				TextEditor::ErrorMarkers errorMarkers;
+				if (shader.getErrorMessages(errorMessages))
+				{
+					// make sure the buffers passed to sscanf are large enough to store the result
+					
+					size_t maxSize = 0;
+					
+					for (size_t i = 0; i < errorMessages.size(); ++i)
+						if (errorMessages[i].size() > maxSize)
+							maxSize = errorMessages[i].size();
+					
+					char * type = new char[maxSize + 1];
+					char * message = new char[maxSize + 1];
+					
+					for (size_t i = 0; i < errorMessages.size(); ++i)
+					{
+						int lineNumber;
+						int fileId;
+						
+						const int n = sscanf(errorMessages[i].c_str(), "%s %d:%d: %[^\n]", type, &fileId, &lineNumber, message);
+						
+						if (n == 4 && fileId == 0 && strstr(type, "ERROR") != nullptr)
+						{
+							errorMarkers[lineNumber + 1] = message;
+						}
+					}
+					
+					delete [] type;
+					delete [] message;
+				}
+				textEditor.SetErrorMarkers(errorMarkers);
+
+				textEditor.Render("TextEditor", ImVec2(0, GFX_SY - 170 - 30));
+				
+			#if 1
+				if (ImGui::RadioButton("File browser", tab == kTab_FileBrowser))
+					tab = kTab_FileBrowser;
+				ImGui::SameLine();
+				if (ImGui::RadioButton("Uniforms", tab == kTab_Uniforms))
+					tab = kTab_Uniforms;
+				ImGui::SameLine();
+				if (ImGui::RadioButton("Statistics", tab == kTab_Statistics))
+					tab = kTab_Statistics;
+				ImGui::SameLine();
+				if (ImGui::RadioButton("Errors", tab == kTab_Errors))
+					tab = kTab_Errors;
+			#endif
+				
+				ImGui::BeginChild("Tabs", ImVec2(0, 170));
+				{
+					if (tab == kTab_FileBrowser)
+					{
+						std::string filename;
+						
+						if (showFileBrowser(shaderNamePs, files, filename))
+						{
+							std::vector<std::string> lines;
+							TextIO::LineEndings lineEndings;
+							
+							if (TextIO::load(filename.c_str(), lines, lineEndings) == false)
+							{
+								path.clear();
+								pathIsValid = false;
+							}
+							else
+							{
+								textEditor.SetTextLines(lines);
+							
+								// we need to create a virtual copy of the shader from disk. otherwise,
+								// when the automatic shader reloading kicks in, it will reload the file
+								// from disk, instead of the cached shader source we give it here
+								// we just simply append '-copy' to the original file name to make it distinct
+								shaderNamePs = shaderNamePs + "-copy";
+								
+								// register the shader's source code with framework
+								const std::string & source = textEditor.GetText();
+								shaderSource(shaderNamePs.c_str(), source.c_str());
+								shaderPs = source;
+								
+								// and set the shader
+								shader = Shader(shaderNamePs.c_str(), "testShader.vs", shaderNamePs.c_str());
+								
+								path = filename;
+								pathIsValid = true;
+							}
+						}
+					}
+					else if (tab == kTab_Uniforms)
+						showUniforms(shader);
+					else if (tab == kTab_Statistics)
+						showStatistics(shader);
+					else if (tab == kTab_Errors)
+						showErrors(shader);
+					else
+						Assert(false);
+				}
+				ImGui::EndChild();
+			}
+			ImGui::End();
+		
+			// update mouse cursor
+		
+			framework_context.updateMouseCursor();
+		}
+		framework_context.processEnd();
+		
+		const std::string newShaderPs = textEditor.GetText();
+		
+		if (newShaderPs != shaderPs)
+		{
+			shaderPs = newShaderPs;
+			
+			if (shader.isValid())
+				saveUniforms(shader, savedUniforms);
+			
+			shaderSource(shaderNamePs.c_str(), shaderPs.c_str());
+			
+			loadUniforms(shader, savedUniforms, true);
+		}
+	}
+	
+	void draw()
+	{
+		auto setShaderConstants = [&](const int viewSx, const int viewSy)
+		{
+			shader.setImmediate("time", framework.time);
+			shader.setImmediate("mouse", mouse.x / float(viewSx), mouse.y / float(viewSy));
+			shader.setImmediate("mouse_down", mouse.isDown(BUTTON_LEFT));
+			shader.setTexture("s", 0, framework_context.font_texture.id);
+		};
+		
+		if (shader.isValid())
+		{
+			setShader(shader);
+			setShaderConstants(GFX_SX, GFX_SY);
+			drawRect(0, 0, GFX_SX, GFX_SY);
+			clearShader();
+		}
+	
+		framework_context.draw();
+	}
+};
+
 int main(int argc, char * argv[])
 {
 #if defined(CHIBI_RESOURCE_PATH)
@@ -52,47 +384,15 @@ int main(int argc, char * argv[])
 
 	if (framework.init(GFX_SX, GFX_SY))
 	{
-		FrameworkImGuiContext framework_context;
+		RealtimeShaderEditor shaderEditor;
 		
-		framework_context.init();
-		
-		TextEditor textEditor;
-
-		textEditor.SetLanguageDefinition(TextEditor::LanguageDefinition::CPlusPlus());
-		
-		shaderSource("demoShader.vs", s_shaderVs);
-		shaderSource("demoShader.ps", s_shaderPs);
-		
-		textEditor.SetText(s_shaderPs);
-		
-		SDL_StartTextInput();
-		
-		std::string shaderPs = s_shaderPs;
+		shaderEditor.init();
 		
 	#if ENABLE_PREVIEW_WINDOW
 		Window window("Preview", 600, 600, true);
 	#endif
 		
-		Shader shader("demoShader");
-		std::string shaderNamePs = "demoShader.ps";
-		
-		Tab tab = kTab_Uniforms;
-		
-		auto files = listFiles("testShaders", false);
-		auto removed = std::remove_if(files.begin(), files.end(), [](const std::string & s) { return Path::GetExtension(s, true) != "ps"; });
-		files.erase(removed, files.end());
-		std::sort(files.begin(), files.end());
-		
-		float idleTime = 0.f;
-		float alphaAnim = 0.f;
 		bool showEditor = true;
-		
-		std::vector<SavedUniform> savedUniforms;
-		
-		std::string path;
-		bool pathIsValid = false;
-		
-		TextIO::LineEndings lineEndings = TextIO::kLineEndings_Unix;
 		
 		while (!framework.quitRequested)
 		{
@@ -100,8 +400,6 @@ int main(int argc, char * argv[])
 			
 			if (keyboard.wentDown(SDLK_ESCAPE))
 				framework.quitRequested = true;
-			
-			Assert(pathIsValid == !path.empty());
 			
 			bool inputIsCaptured = false;
 			
@@ -114,271 +412,11 @@ int main(int argc, char * argv[])
 			if (showEditor == false)
 				inputIsCaptured = true;
 			
-			if (keyboard.isIdle() && mouse.isIdle())
-				idleTime += framework.timeStep;
-			else
-				idleTime = 0.f;
-			
-			if (idleTime < 4.f && showEditor)
-				alphaAnim -= framework.timeStep / .5f;
-			else
-				alphaAnim += framework.timeStep / (showEditor ? 2.f : .5f);
-			
-			alphaAnim = clamp(alphaAnim, 0.f, 1.f);
-			
-			framework_context.processBegin(framework.timeStep, GFX_SX, GFX_SY, inputIsCaptured);
-			{
-				const float alpha = lerp(.9f, .0f, alphaAnim);
-				ImGui::GetStyle().Alpha = alpha;
-				
-				ImGui::SetNextWindowPos(ImVec2(0, 0));
-				ImGui::SetNextWindowSize(ImVec2(GFX_SX, GFX_SY));
-				
-				ImGui::Begin("Text Editor Demo", nullptr,
-						ImGuiWindowFlags_NoTitleBar |
-						ImGuiWindowFlags_NoResize |
-						ImGuiWindowFlags_HorizontalScrollbar |
-						ImGuiWindowFlags_MenuBar);
-				{
-					if (ImGui::BeginMenuBar())
-					{
-						if (ImGui::BeginMenu("File"))
-						{
-							if (ImGui::MenuItem("Load.."))
-							{
-								nfdchar_t * filename = nullptr;
-								
-								if (NFD_OpenDialog(nullptr, nullptr, &filename) == NFD_OKAY)
-								{
-									if (loadIntoTextEditor(filename, lineEndings, textEditor))
-									{
-										path = filename;
-										pathIsValid = true;
-									}
-									else
-									{
-										path.clear();
-										pathIsValid = false;
-									}
-								}
-								
-								if (filename != nullptr)
-								{
-									free(filename);
-									filename = nullptr;
-								}
-							}
-							
-							if (ImGui::MenuItem("Save", nullptr, false, pathIsValid))
-							{
-								if (pathIsValid)
-								{
-									auto lines = textEditor.GetTextLines();
-									
-									if (TextIO::save(path.c_str(), lines, lineEndings) == false)
-									{
-										SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Save error", "Failed to save to file", nullptr);
-									}
-								}
-							}
-							
-							if (ImGui::MenuItem("Save as.."))
-							{
-								nfdchar_t * filename = nullptr;
-								
-								if (NFD_SaveDialog(nullptr, nullptr, &filename) == NFD_OKAY)
-								{
-									auto lines = textEditor.GetTextLines();
-									
-									if (TextIO::save(filename, lines, lineEndings) == false)
-									{
-										SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Save error", "Failed to save to file", nullptr);
-										
-										path.clear();
-										pathIsValid = false;
-									}
-									else
-									{
-										path = filename;
-										pathIsValid = true;
-									}
-								}
-								
-								if (filename != nullptr)
-								{
-									free(filename);
-									filename = nullptr;
-								}
-							}
-							
-							ImGui::EndMenu();
-						}
-						
-						if (ImGui::BeginMenu("Edit"))
-						{
-							if (ImGui::MenuItem("Undo"))
-								textEditor.Undo();
-							if (ImGui::MenuItem("Redo"))
-								textEditor.Redo();
-							
-							ImGui::Separator();
-							if (ImGui::MenuItem("Copy"))
-								textEditor.Copy();
-							if (ImGui::MenuItem("Cut"))
-								textEditor.Cut();
-							if (ImGui::MenuItem("Paste"))
-								textEditor.Paste();
-							
-							ImGui::Separator();
-							if (ImGui::MenuItem("Select all"))
-								textEditor.SelectAll();
-							
-							ImGui::EndMenu();
-						}
-						
-						ImGui::EndMenuBar();
-					}
-					
-					std::vector<std::string> errorMessages;
-					TextEditor::ErrorMarkers errorMarkers;
-					if (shader.getErrorMessages(errorMessages))
-					{
-						// make sure the buffers passed to sscanf are large enough to store the result
-						
-						size_t maxSize = 0;
-						
-						for (size_t i = 0; i < errorMessages.size(); ++i)
-							if (errorMessages[i].size() > maxSize)
-								maxSize = errorMessages[i].size();
-						
-						char * type = new char[maxSize + 1];
-						char * message = new char[maxSize + 1];
-						
-						for (size_t i = 0; i < errorMessages.size(); ++i)
-						{
-							int lineNumber;
-							int fileId;
-							
-							const int n = sscanf(errorMessages[i].c_str(), "%s %d:%d: %[^\n]", type, &fileId, &lineNumber, message);
-							
-							if (n == 4 && fileId == 0 && strstr(type, "ERROR") != nullptr)
-							{
-								errorMarkers[lineNumber + 1] = message;
-							}
-						}
-						
-						delete [] type;
-						delete [] message;
-					}
-					textEditor.SetErrorMarkers(errorMarkers);
-
-					textEditor.Render("TextEditor", ImVec2(0, GFX_SY - 170 - 30));
-					
-				#if 1
-					if (ImGui::RadioButton("File browser", tab == kTab_FileBrowser))
-						tab = kTab_FileBrowser;
-					ImGui::SameLine();
-					if (ImGui::RadioButton("Uniforms", tab == kTab_Uniforms))
-						tab = kTab_Uniforms;
-					ImGui::SameLine();
-					if (ImGui::RadioButton("Statistics", tab == kTab_Statistics))
-						tab = kTab_Statistics;
-					ImGui::SameLine();
-					if (ImGui::RadioButton("Errors", tab == kTab_Errors))
-						tab = kTab_Errors;
-				#endif
-					
-					ImGui::BeginChild("Tabs", ImVec2(0, 170));
-					{
-						if (tab == kTab_FileBrowser)
-						{
-							std::string filename;
-							
-							if (showFileBrowser(shaderNamePs, files, filename))
-							{
-								std::vector<std::string> lines;
-								TextIO::LineEndings lineEndings;
-								
-								if (TextIO::load(filename.c_str(), lines, lineEndings) == false)
-								{
-									path.clear();
-									pathIsValid = false;
-								}
-								else
-								{
-									textEditor.SetTextLines(lines);
-								
-									// we need to create a virtual copy of the shader from disk. otherwise,
-									// when the automatic shader reloading kicks in, it will reload the file
-									// from disk, instead of the cached shader source we give it here
-									// we just simply append '-copy' to the original file name to make it distinct
-									shaderNamePs = shaderNamePs + "-copy";
-									
-									// register the shader's source code with framework
-									const std::string & source = textEditor.GetText();
-									shaderSource(shaderNamePs.c_str(), source.c_str());
-									shaderPs = source;
-									
-									// and set the shader
-									shader = Shader(shaderNamePs.c_str(), "testShader.vs", shaderNamePs.c_str());
-									
-									path = filename;
-									pathIsValid = true;
-								}
-							}
-						}
-						else if (tab == kTab_Uniforms)
-							showUniforms(shader);
-						else if (tab == kTab_Statistics)
-							showStatistics(shader);
-						else if (tab == kTab_Errors)
-							showErrors(shader);
-						else
-							Assert(false);
-					}
-					ImGui::EndChild();
-				}
-				ImGui::End();
-			
-				// update mouse cursor
-			
-				framework_context.updateMouseCursor();
-			}
-			framework_context.processEnd();
-			
-			const std::string newShaderPs = textEditor.GetText();
-			
-			if (newShaderPs != shaderPs)
-			{
-				shaderPs = newShaderPs;
-				
-				if (shader.isValid())
-					saveUniforms(shader, savedUniforms);
-				
-				shaderSource(shaderNamePs.c_str(), shaderPs.c_str());
-				
-				loadUniforms(shader, savedUniforms, true);
-			}
-			
-			auto setShaderConstants = [&](const int viewSx, const int viewSy)
-			{
-				shader.setImmediate("time", framework.time);
-				shader.setImmediate("mouse", mouse.x / float(viewSx), mouse.y / float(viewSy));
-				shader.setImmediate("mouse_down", mouse.isDown(BUTTON_LEFT));
-				shader.setTexture("s", 0, framework_context.font_texture.id);
-			};
+			shaderEditor.tick(framework.timeStep, inputIsCaptured, showEditor);
 			
 			framework.beginDraw(0, 0, 0, 0);
 			{
-				if (shader.isValid())
-				{
-					setShader(shader);
-					setShaderConstants(GFX_SX, GFX_SY);
-					drawRect(0, 0, GFX_SX, GFX_SY);
-					clearShader();
-				}
-				
-				framework_context.draw();
+				shaderEditor.draw();
 			}
 			framework.endDraw();
 			
@@ -404,9 +442,7 @@ int main(int argc, char * argv[])
 		#endif
 		}
 		
-		SDL_StopTextInput();
-		
-		framework_context.shut();
+		shaderEditor.shut();
 
 		framework.shutdown();
 	}
