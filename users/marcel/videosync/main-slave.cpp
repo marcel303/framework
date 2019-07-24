e7ceb4d72458e65af5ac95c13707c530c7d4cf85
Marcel Smit <marcel303@gmail.com>
2019-07-24 22:53:23 +0200

    Adding videosync user app.

---
+++ users/marcel/videosync/main-slave.cpp
@@ -0,0 +1,248 @@
+#include "framework.h"
+#include "oscReceiver.h"
+#include "videoloop.h"
+#include "reflection.h"
+#include "reflection-bindtofile.h"
+
+#define DEBUG_MASTER_TIME 0
+
+struct Settings
+{
+	std::string videoFilename;
+	int oscReceivePort = 2000;
+	int windowWidth = 1920;
+	int windowHeight = 1080;
+	bool fullscreen = false;
+	
+	static void reflect(TypeDB & typeDB)
+	{
+		typeDB.addStructured<Settings>("Settings")
+			.add("videoFilename", &Settings::videoFilename)
+			.add("oscReceivePort", &Settings::oscReceivePort)
+			.add("windowWidth", &Settings::windowWidth)
+			.add("windowHeight", &Settings::windowHeight)
+			.add("fullscreen", &Settings::fullscreen);
+	}
+};
+
+struct MasterTimer : OscReceiveHandler
+{
+	bool isStarted = false;
+	
+	float time = 0.f;
+	float extrapolatedReceivedTime = 0.f;
+	float extrapolatedReceivedTime_exact = 0.f;
+	
+	void tick(const float dt)
+	{
+		if (isStarted)
+		{
+			extrapolatedReceivedTime += dt;
+			extrapolatedReceivedTime_exact += dt;
+		}
+		
+		const float delta1 = fabsf(extrapolatedReceivedTime - extrapolatedReceivedTime_exact);
+		extrapolatedReceivedTime = lerp<float>(extrapolatedReceivedTime, extrapolatedReceivedTime_exact, .2f);\
+		const float delta2 = fabsf(extrapolatedReceivedTime - extrapolatedReceivedTime_exact);
+		logDebug("extrapolated time diff: %dms -> %dms", int(delta1 * 1000.f), int(delta2 * 1000.f));
+	}
+	
+	virtual void handleOscMessage(const osc::ReceivedMessage & m, const IpEndpointName & remoteEndpoint) override
+	{
+		if (strcmp(m.AddressPattern(), "/start") == 0)
+		{
+			for (auto arg_itr = m.ArgumentsBegin(); arg_itr != m.ArgumentsEnd(); ++arg_itr)
+			{
+				auto & arg = *arg_itr;
+				
+				if (arg.IsBool())
+					isStarted = arg.AsBoolUnchecked();
+				if (arg.IsInt32())
+					isStarted = arg.AsInt32Unchecked();
+				if (arg.IsFloat())
+					isStarted = arg.AsFloatUnchecked();
+			}
+		}
+		else if (strcmp(m.AddressPattern(), "/stop") == 0)
+		{
+			for (auto arg_itr = m.ArgumentsBegin(); arg_itr != m.ArgumentsEnd(); ++arg_itr)
+			{
+				auto & arg = *arg_itr;
+				
+				if (arg.IsBool())
+					isStarted = 0 == arg.AsBoolUnchecked();
+				if (arg.IsInt32())
+					isStarted = 0 == arg.AsInt32Unchecked();
+				if (arg.IsFloat())
+					isStarted = 0 == arg.AsFloatUnchecked();
+			}
+		}
+		else if (strcmp(m.AddressPattern(), "/time") == 0)
+		{
+			for (auto arg_itr = m.ArgumentsBegin(); arg_itr != m.ArgumentsEnd(); ++arg_itr)
+			{
+				auto & arg = *arg_itr;
+				
+				if (arg.IsInt32())
+				{
+					time = arg.AsInt32Unchecked();
+				}
+				else if (arg.IsFloat())
+				{
+					time = arg.AsFloatUnchecked();
+				}
+				else if (arg.IsDouble())
+				{
+					time = arg.AsDoubleUnchecked();
+				}
+			}
+			
+			if (fabsf(extrapolatedReceivedTime - time) > 1.f / 20.f)
+				extrapolatedReceivedTime = time;
+			extrapolatedReceivedTime_exact = time;
+			
+			const float delta = fabsf(extrapolatedReceivedTime - extrapolatedReceivedTime_exact);
+			logDebug("extrapolated time diff with exact time: %dms", int(delta * 1000.f));
+		}
+	}
+};
+
+int main(int argc, char * argv[])
+{
+#if defined(CHIBI_RESOURCE_PATH)
+	changeDirectory(CHIBI_RESOURCE_PATH);
+#else
+	changeDirectory(SDL_GetBasePath());
+#endif
+	
+	TypeDB typeDB;
+	typeDB.addPlain<int>("int", kDataType_Int);
+	typeDB.addPlain<std::string>("string", kDataType_String);
+	Settings::reflect(typeDB);
+	
+	Settings settings;
+	bindObjectToFile(&typeDB, &settings, "settings.json");
+	
+	//
+	
+	framework.fullscreen = settings.fullscreen;
+	
+	if (settings.fullscreen == false)
+	{
+		settings.windowWidth /= 2;
+		settings.windowHeight /= 2;
+	}
+	
+	framework.windowIsResizable = true;
+	
+	if (!framework.init(settings.windowWidth, settings.windowHeight))
+		return -1;
+	
+	//
+	
+	OscReceiver oscReceiver;
+	if (!oscReceiver.init("255.255.255.255", settings.oscReceivePort))
+		logError("failed to initialzie OSC receiver");
+	
+	MasterTimer masterTimer;
+	
+#if DEBUG_MASTER_TIME
+	float time = 0.f;
+	float restartTimer = 0.f;
+#endif
+	
+	float targetTime = 0.f;
+	
+	VideoLoop videoLoop(settings.videoFilename.c_str());
+	
+	for (;;)
+	{
+		framework.process();
+
+		if (framework.quitRequested)
+			break;
+
+		const float dt = framework.timeStep;
+		
+	#if DEBUG_MASTER_TIME
+		time += dt;
+		
+		restartTimer += dt;
+		
+		if (restartTimer >= 3.f)
+		{
+			logDebug("faking seek to start");
+			restartTimer = 0.f;
+			time = 0.f;
+		}
+	#endif
+		
+		masterTimer.tick(dt);
+		
+		oscReceiver.flushMessages(&masterTimer);
+		
+	#if DEBUG_MASTER_TIME
+		if ((rand() % 10) == 0)
+		{
+			receivedTime = time;
+			
+			masterTimer.extrapolatedReceivedTime = receivedTime;
+		}
+	#endif
+		
+		if (targetTime - 1.f > masterTimer.extrapolatedReceivedTime)
+		{
+			targetTime = masterTimer.extrapolatedReceivedTime;
+			
+			logDebug("seek back detected. switching videos");
+			
+			videoLoop.switchVideos();
+			
+			if (targetTime > 1.f)
+			{
+				logDebug("seeking video");
+				videoLoop.seekTo(targetTime);
+			}
+		}
+		
+		targetTime = masterTimer.extrapolatedReceivedTime;
+		
+		videoLoop.tick(targetTime, framework.timeStep);
+		
+		framework.beginDraw(0, 0, 0, 0);
+		{
+			int videoSxPx;
+			int videoSyPx;
+			double duration;
+			double sampleAspectRatio;
+			if (videoLoop.mediaPlayer1->getVideoProperties(videoSxPx, videoSyPx, duration, sampleAspectRatio))
+			{
+				int sx, sy;
+				framework.getCurrentViewportSize(sx, sy);
+				
+				const float videoSx = videoSxPx;
+				const float videoSy = videoSyPx * sampleAspectRatio;
+				
+				const float scaleX = sx / videoSx;
+				const float scaleY = sy / videoSy;
+				
+				const float scale = fminf(scaleX, scaleY);
+				const float rectSx = videoSx * scale;
+				const float rectSy = videoSy * scale;
+				const float offsetX = (sx - rectSx) / 2.f;
+				const float offsetY = (sy - rectSy) / 2.f;
+				
+				pushBlend(BLEND_OPAQUE);
+				gxSetTexture(videoLoop.getTexture());
+				drawRect(offsetX, offsetY, offsetX + rectSx, offsetY + rectSy);
+				gxSetTexture(0);
+				popBlend();
+			}
+		}
+		framework.endDraw();
+	}
+
+	framework.shutdown();
+
+	return 0;
+}
---
+++ users/marcel/videosync/videoloop.h
@@ -0,0 +1,94 @@
+/*
+	Copyright (C) 2017 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "framework.h"
+#include "video.h"
+
+struct VideoLoop
+{
+	std::string filename;
+	MediaPlayer * mediaPlayer1;
+	MediaPlayer * mediaPlayer2;
+	
+	VideoLoop(const char * _filename)
+		: filename()
+		, mediaPlayer1(nullptr)
+		, mediaPlayer2(nullptr)
+	{
+		filename = _filename;
+		
+		mediaPlayer1 = new MediaPlayer();
+		mediaPlayer1->openAsync(filename.c_str(), MP::kOutputMode_RGBA);
+		
+		mediaPlayer2 = new MediaPlayer();
+		mediaPlayer2->openAsync(filename.c_str(), MP::kOutputMode_RGBA);
+	}
+	
+	~VideoLoop()
+	{
+		delete mediaPlayer1;
+		mediaPlayer1 = nullptr;
+		
+		delete mediaPlayer2;
+		mediaPlayer2 = nullptr;
+	}
+	
+	void tick(const float targetTime, const float dt)
+	{
+		if (mediaPlayer1->context->hasPresentedLastFrame)
+		{
+			//switchVideos();
+		}
+		
+		mediaPlayer1->presentTime = targetTime;
+		mediaPlayer1->tick(mediaPlayer1->context, true);
+		
+		mediaPlayer2->tick(mediaPlayer2->context, true);
+	}
+	
+	void seekTo(const float targetTime)
+	{
+		if (mediaPlayer1 != nullptr)
+			mediaPlayer1->seek(targetTime, true);
+	}
+	
+	void switchVideos()
+	{
+		std::swap(mediaPlayer1, mediaPlayer2);
+		
+		mediaPlayer2->close(false);
+		mediaPlayer2->presentTime = 0.0;
+		mediaPlayer2->openAsync(filename.c_str(), MP::kOutputMode_RGBA);
+	}
+	
+	GxTextureId getTexture() const
+	{
+		return mediaPlayer1->getTexture();
+	}
+};
