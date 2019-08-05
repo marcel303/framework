2684768c20b1f24484899d87bbb8a263363b8447
Marcel Smit <marcel303@gmail.com>
2019-08-05 20:11:54 +0200

    maxpatch: Ignore button elements for now when generating Max patches.

--- users/marcel/maxpatch1/maxPatchFromControlSurfaceDefinition.cpp
+++ users/marcel/maxpatch1/maxPatchFromControlSurfaceDefinition.cpp
@@ -198,6 +198,9 @@ bool maxPatchFromControlSurfaceDefinition(const ControlSurfaceDefinition::Surfac
 			}
 			else if (elem.type == ControlSurfaceDefinition::kElementType_Button)
 			{
+			#if 0
+				// todo : implement button output
+				// todo : add support for trigger type buttons and toggle buttons
 				std::string osc_id;
 				
 				if (elem.button.oscAddress.empty() == false)
@@ -230,6 +233,7 @@ bool maxPatchFromControlSurfaceDefinition(const ControlSurfaceDefinition::Surfac
 					connect(osc_id, 0, button_id, 0);
 					connect(button_id, 0, osc_id, 0);
 				}
+			#endif
 			}
 			else if (elem.type == ControlSurfaceDefinition::kElementType_Slider2)
 			{
