967e20d6cd2f7ae536a2dbabd8dadf9474640522
Marcel Smit <marcel303@gmail.com>
2019-09-03 16:16:09 +0200

    gpupath: Polish and small optimize.

--- users/marcel/gpupath1/main.cpp
+++ users/marcel/gpupath1/main.cpp
@@ -22,22 +22,25 @@ int main(int argc, char * argv[])
 			setShader(shader);
 			shader.setImmediate("color1", 0.1, 0.3, 0.9, 1.0);
 			shader.setImmediate("color2", 0.8, 1.0, 0.1, 1.0);
-			shader.setImmediate("numVertices", 1000);
 			shader.setImmediate("halfThickness", 5.f / 2.f);
-			shader.setImmediate("hardness", .5f);
+			shader.setImmediate("hardness", 1.f);
 			
 			const GxImmediateIndex position1_idx = shader.getImmediate("position1");
 			const GxImmediateIndex position2_idx = shader.getImmediate("position2");
+			const GxImmediateIndex numVertices_idx = shader.getImmediate("numVertices");
 			
 			for (int i = 0; i < 100; ++i)
 			{
 				const float x = mouse.x + cosf(framework.time * i / 100.f) * i * 2.f;
 				const float y = mouse.y + sinf(framework.time * i / 100.f) * i * 2.f;
 				
+				const int numVertices = 200;
+				
 				shader.setImmediate(position1_idx, 10, 10);
 				shader.setImmediate(position2_idx, x, y);
+				shader.setImmediate(numVertices_idx, numVertices);
 				{
-					gxEmitVertices(GX_TRIANGLE_STRIP, 1000);
+					gxEmitVertices(GX_TRIANGLE_STRIP, numVertices);
 				}
 			}
 			clearShader();
