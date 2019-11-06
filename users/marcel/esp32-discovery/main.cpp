bfe36c8c7153b52aa66d18d21eef3fc75a5a39bf
Marcel Smit <marcel303@gmail.com>
2019-11-06 17:34:38 +0100

    - Add support for receiving node version, capabilities and description. - Show node capabilities. - Add node version check. - Update node record when its contents have changed. - Added TCP to I2S streaming test. - Store node state inside a per-node struct.

--- users/marcel/esp32-discovery/main.cpp
+++ users/marcel/esp32-discovery/main.cpp
@@ -17,9 +17,27 @@ This is a sketch which lets the user select a Wifi access point and connect to i
 
 #define ARTNET_PORT 6454
 
+enum Capabilities
+{
+	kCapability_ArtnetToDmx       = 1 << 0,
+	kCapability_ArtnetToLedstrip  = 1 << 1,
+	kCapability_ArtnetToAnalogPin = 1 << 2,
+	kCapability_TcpToI2S          = 1 << 3
+};
+
+struct DiscoveryPacket
+{
+	uint64_t id;
+	char version[4];
+	uint32_t capabilities;
+	char description[32];
+};
+
 struct DiscoveryRecord
 {
 	uint64_t id;
+	uint32_t capabilities;
+	char description[32];
 	IpEndpointName endpointName;
 };
 
@@ -153,38 +171,249 @@ private:
 		
 		// decode the discovery message
 		
-		if (size != sizeof(uint64_t))
+		if (size < sizeof(DiscoveryPacket))
 		{
 			logWarning("received invalid discovery message");
 			return;
 		}
 		
-		const uint64_t id = *(uint64_t*)data;
+		DiscoveryPacket * discoveryPacket = (DiscoveryPacket*)data;
 		
-		bool exists = false;
+		if (memcmp(discoveryPacket->version, "v100", 4) != 0)
+		{
+			logWarning("received discovery message with unknown version string");
+			return;
+		}
+		
+		DiscoveryRecord * existingRecord = nullptr;
 		
 		for (auto & record : s_discoveryRecords)
 		{
-			if (record.id == id)
-				exists = true;
+			if (record.id == discoveryPacket->id)
+				existingRecord = &record;
 		}
 		
-		if (exists == false)
+		DiscoveryRecord record;
+		memset(&record, 0, sizeof(record));
+		record.id = discoveryPacket->id;
+		record.capabilities = discoveryPacket->capabilities;
+		strcpy_s(record.description, sizeof(record.description), discoveryPacket->description);
+		record.endpointName = remoteEndpoint;
+		
+		if (existingRecord == nullptr)
 		{
-			logDebug("found a new node! id=%llx", id);
+			logDebug("found a new node! id=%llx", discoveryPacket->id);
 			
 			lock();
 			{
-				DiscoveryRecord record;
-				record.id = id;
-				record.endpointName = remoteEndpoint;
-				
 				s_discoveryRecords.push_back(record);
 			}
 			unlock();
 		}
+		else
+		{
+			if (memcmp(existingRecord, &record, sizeof(record)) != 0)
+			{
+				logDebug("updating existing node! id=%llx", discoveryPacket->id);
+				
+				lock();
+				{
+					*existingRecord = record;
+				}
+				unlock();
+			}
+		}
 	}
 };
+
+//
+
+#include "audiostream/AudioIO.h"
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
+
+struct Test_TcpToI2S
+{
+	bool isActive = false;
+	
+	std::atomic<bool> wantsToStop;
+	
+	std::thread thread;
+	
+	Test_TcpToI2S()
+		: wantsToStop(false)
+	{
+	}
+	
+	bool init(const uint32_t ipAddress, const uint16_t tcpPort)
+	{
+		Assert(isActive == false);
+		
+		if (isActive)
+		{
+			shut();
+		}
+		
+		isActive = true;
+		
+		thread = std::thread([=]()
+		{
+			struct sockaddr_in addr;
+			int sock = 0;
+			int sock_value = 0;
+			SoundData * soundData = nullptr;
+			int samplePosition = 0;
+			
+			//
+			
+			sock = socket(AF_INET, SOCK_STREAM, 0);
+			
+			if (sock == -1)
+			{
+				logError("failed opening socket");
+				goto error;
+			}
+			
+			// disables nagle's algorithm and send out packets immediately on write
+			
+			sock_value = 1;
+			setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &sock_value, sizeof(sock_value));
+			
+			// tell the TCP stack to use a specific buffer size. usually the TCP stack is
+			// configured to use a rather large buffer size to increase bandwidth. we want
+			// to keep latency down however, so we reduce the buffer size here
+			
+			sock_value =
+				3   * /* triple buffered */
+				256 * /* frame count */
+				2   * /* stereo */
+				sizeof(int16_t) /* sample size */;
+ 			setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sock_value, sizeof(sock_value));
+			
+			// todo : TCP_NODELAY (osx). disables nagle's algorithm and sends out packets immediately on write
+			// todo : TCP_NOPUSH (osx)
+			// todo : TCP_CORK (linux). will manually batch messages and send them when the cork is removed
+
+		// todo : strp-laserapp : use writev or similar to send multiple packets to the same Artnet controller
+		
+			addr.sin_family = AF_INET;
+			addr.sin_addr.s_addr = htonl(ipAddress);
+			addr.sin_port = htons(tcpPort);
+
+			logDebug("connecting socket to remote endpoint");
+			
+			if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+			{
+				logError("failed to connect socket");
+				goto error;
+			}
+			
+			logError("connected to remote endpoint!");
+			
+			soundData = loadSound("loop01.ogg");
+			
+			while (wantsToStop.load() == false)
+			{
+				// todo : detect is disconnected, and attempt to reconnect
+				// todo : avoid high CPU on disconnect
+				// todo : perform disconnection test
+				
+				while (keyboard.isDown(SDLK_SPACE))
+				{
+					SDL_Delay(10);
+				}
+				
+				// todo : generate some audio data
+				
+				short data[256][2];
+				
+				// we're kind of strict with regard to the sound format we're going to allow .. to simplify the streaming a bit
+				if (soundData->sampleCount == 0 ||
+					soundData->channelCount != 2 ||
+					soundData->channelSize != 2)
+				{
+					memset(data, 0, sizeof(data));
+				}
+				else
+				{
+					const short * samples = (const short*)soundData->sampleData;
+					
+					const int volume = mouse.x * 256 / 800;
+					
+					for (int i = 0; i < 256; ++i)
+					{
+						data[i][0] = (samples[samplePosition * 2 + 0] * volume) >> 8;
+						data[i][1] = (samples[samplePosition * 2 + 1] * volume) >> 8;
+						
+						samplePosition++;
+						
+						if (samplePosition == soundData->sampleCount)
+							samplePosition = 0;
+					}
+				}
+				
+				send(sock, data, sizeof(data), 0);
+			}
+			
+		error:
+			delete soundData;
+			soundData = nullptr;
+			
+			if (sock != -1)
+			{
+				close(sock);
+				sock = -1;
+			}
+		});
+		
+		return true;
+	}
+	
+	void shut()
+	{
+		if (isActive)
+		{
+			wantsToStop = true;
+			
+			thread.join();
+			
+			wantsToStop = false;
+			
+			isActive = false;
+		}
+	}
+};
+
+//
+
+#include <list>
+
+struct NodeState
+{
+	uint64_t nodeId = -1;
+	
+	Test_TcpToI2S test_tcpToI2S;
+};
+
+static std::list<NodeState> s_nodeStates;
+
+static NodeState & findOrCreateNodeState(const uint64_t id)
+{
+	for (auto & nodeState : s_nodeStates)
+	{
+		if (nodeState.nodeId == id)
+			return nodeState;
+	}
+	
+	s_nodeStates.emplace_back();
+	auto & nodeState = s_nodeStates.back();
+	nodeState.nodeId = id;
+	return nodeState;
+}
 
 int main(int argc, char * argv[])
 {
@@ -200,7 +429,6 @@ int main(int argc, char * argv[])
 	discoveryProcess.init();
 	
 	UdpSocket artnetSocket;
-	//artnetSocket.
 	
 	for (;;)
 	{
@@ -212,22 +440,73 @@ int main(int argc, char * argv[])
 		if (framework.quitRequested)
 			break;
 
-		// send some artnet data to discovered nodes
+		const int numRecords = discoveryProcess.getRecordCount();
 		
+		for (int i = 0; i < numRecords; ++i)
 		{
-			ArtnetPacket packet;
+			auto record = discoveryProcess.getDiscoveryRecord(i);
 			
-			packet.makeDMX512(4);
+			auto & nodeState = findOrCreateNodeState(record.id);
 			
-			const int numRecords = discoveryProcess.getRecordCount();
-			
-			for (int i = 0; i < numRecords; ++i)
+			if (keyboard.wentDown(SDLK_s))
 			{
-				auto record = discoveryProcess.getDiscoveryRecord(i);
+				if (keyboard.isDown(SDLK_LSHIFT) || keyboard.isDown(SDLK_RSHIFT))
+				{
+					nodeState.test_tcpToI2S.shut();
+				}
+				else
+				{
+					const int tcpPort = 6458;
+					
+					nodeState.test_tcpToI2S.init(record.endpointName.address, tcpPort);
+				}
+			}
+		
+			// send some artnet data to discovered nodes
+			
+			if (record.capabilities & kCapability_ArtnetToDmx)
+			{
+				ArtnetPacket packet;
 				
-				IpEndpointName remoteEndpoint(record.endpointName.address, ARTNET_PORT);
+				auto * values = packet.makeDMX512(4);
+
+			#if 0
+				static float value = 0.f;
+				const uint16_t value16 = uint16_t(powf(value, 2.f) * (1 << 16));
+				values[0] = value16 >> 8;
+				values[1] = value16 & 0xff;
+				value = fmodf(value + 1.f / 1000.f, 1.f);
+				values[2] = mouse.x - mouse.y;
+				values[3] = mouse.x + mouse.y;
+			#else
+				values[0] = mouse.x;
+				values[1] = mouse.y;
+				values[2] = mouse.x - mouse.y;
+				values[3] = mouse.x + mouse.y;
+			#endif
+			
+				const IpEndpointName remoteEndpoint(record.endpointName.address, ARTNET_PORT);
 				
-				for (int i = 0; i < 2; ++i)
+				artnetSocket.SendTo(remoteEndpoint, (const char*)packet.data, packet.dataSize);
+			}
+			else if (record.capabilities & kCapability_ArtnetToLedstrip)
+			{
+				ArtnetPacket packet;
+			
+				auto packFloatToDmx16 = [](const float value, const float gamma, uint8_t & hi, uint8_t & lo)
+				{
+					const uint16_t value16 = uint16_t(powf(value, gamma) * (1 << 16));
+					hi = value16 >> 8;
+					lo = uint8_t(value16);
+				};
+			
+				auto * values = packet.makeDMX512(4);
+			
+				packFloatToDmx16(mouse.x / 800.f, 2.5f, values[0], values[1]);
+				packFloatToDmx16(mouse.y / 600.f, 2.5f, values[2], values[3]);
+			
+				const IpEndpointName remoteEndpoint(record.endpointName.address, ARTNET_PORT);
+				
 				artnetSocket.SendTo(remoteEndpoint, (const char*)packet.data, packet.dataSize);
 			}
 		}
@@ -241,10 +520,12 @@ int main(int argc, char * argv[])
 			{
 				auto record = discoveryProcess.getDiscoveryRecord(i);
 				
+				auto & nodeState = findOrCreateNodeState(record.id);
+				
 				char endpointName[IpEndpointName::ADDRESS_STRING_LENGTH];
 				record.endpointName.AddressAsString(endpointName);
 				
-				const int sy = 18;
+				const int sy = 22;
 				
 				const int x1 = 0;
 				const int y1 = (i + 0) * sy;
@@ -268,13 +549,62 @@ int main(int argc, char * argv[])
 					system(command);
 				}
 				
+				int x = x1 + 10;
+				
 				setColor(colorGreen);
-				drawText(x1 + 10, (y1 + y2) / 2.f, 14, +1, 0, "%s", endpointName);
+				drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "%s", endpointName);
+				x += 100;
+				
 				setColor(colorWhite);
-				drawText(x1 + 100, (y1 + y2) / 2.f, 14, +1, 0, "%llx", record.id);
+				drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "%llx", record.id);
+				x += 100;
+				
+				setColor(colorWhite);
+				drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "%s", record.description);
+				x += 140;
+				
+				if (record.capabilities & kCapability_ArtnetToDmx)
+				{
+					setColor(colorYellow);
+					drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "Art2DMX");
+					x += 100;
+				}
+				
+				if (record.capabilities & kCapability_ArtnetToLedstrip)
+				{
+					setColor(colorYellow);
+					drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "Art2LED");
+					x += 100;
+				}
+				
+				if (record.capabilities & kCapability_ArtnetToAnalogPin)
+				{
+					setColor(colorYellow);
+					drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "Art2Pin");
+					x += 100;
+				}
+				
+				if (record.capabilities & kCapability_TcpToI2S)
+				{
+					setColor(colorYellow);
+					drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "Tcp2I2S");
+					x += 100;
+				}
+				
+				if (nodeState.test_tcpToI2S.isActive)
+				{
+					setColor(colorGreen);
+					drawText(x, (y1 + y2) / 2.f, 14, +1, 0, "(Playing)");
+					x += 100;
+				}
 			}
 		}
 		framework.endDraw();
+	}
+	
+	for (auto & nodeState : s_nodeStates)
+	{
+		nodeState.test_tcpToI2S.shut();
 	}
 	
 	discoveryProcess.shut();
