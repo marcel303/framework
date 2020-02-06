1cb5fb46d8c635b4c46e560e691e98757795de58
Marcel Smit <marcel303@gmail.com>
2020-02-06 16:58:40 +0100

    sceneedit: - Update to use clipboard helpers for node copy/paste. - Update setTexture calls.

--- users/marcel/sceneedit1/scene_clipboardHelpers.cpp
+++ users/marcel/sceneedit1/scene_clipboardHelpers.cpp
@@ -33,83 +33,6 @@ bool node_to_clipboard_text(const SceneNode & node, std::string & text)
 	return true;
 }
 
-bool node_to_clipboard_text_recursive(const Scene & scene, const SceneNode & rootNode, std::string & text)
-{
-	// gather all nodes we will serialize later on
-	
-	std::vector<SceneNode> all_nodes;
-	SceneNode sceneRootNode;
-	sceneRootNode.parentId = -1;
-	sceneRootNode.id = rootNode.parentId;
-	sceneRootNode.childNodeIds.push_back(rootNode.id);
-	all_nodes.push_back(sceneRootNode);
-	
-	std::vector<const SceneNode*> node_stack;
-	node_stack.push_back(&sceneRootNode);
-	
-	while (node_stack.empty() == false)
-	{
-		auto * node = node_stack.back();
-		node_stack.pop_back();
-		
-		all_nodes.push_back(*node);
-		
-		for (auto childNodeId : node->childNodeIds)
-		{
-			auto & childNode = scene.getNode(childNodeId);
-			node_stack.push_back(&childNode);
-		}
-	}
-	
-	// allocate local node ids
-	
-	std::map<int, int> global_to_local_id;
-	
-	int nextLocalNodeId = 0;
-	for (auto & node : all_nodes)
-		global_to_local_id[node.id] = nextLocalNodeId++;
-	
-	// translate global node ids to local node ids
-	
-	auto global_to_local = [&](const int nodeId)
-	{
-		auto i = global_to_local_id.find(nodeId);
-		assert(i != global_to_local_id.end());
-		return i->second;
-	};
-	
-	for (auto & node : all_nodes)
-	{
-		node.id = global_to_local(node.id);
-		if (node.parentId != -1)
-			node.parentId = global_to_local(node.parentId);
-		for (auto & childNodeId : node.childNodeIds)
-			childNodeId = global_to_local(childNodeId);
-	}
-	
-	// create a temporary scene using the nodes we gathered
-	
-	Scene tempScene;
-	
-	for (auto & node : all_nodes)
-		tempScene.nodes[node.id] = &node;
-	
-	tempScene.rootNodeId = global_to_local(sceneRootNode.id);
-	
-	// save scene to lines
-	
-	LineWriter line_writer;
-	
-	line_writer.append(":node-hierarchy\n");
-	
-	tempScene.saveToLines(g_typeDB, line_writer, 0);
-	tempScene.nodes.clear();
-	
-	text = line_writer.to_string();
-	
-	return true;
-}
-
 bool node_from_clipboard_text(const char * text, SceneNode & node)
 {
 	std::vector<std::string> lines;
@@ -121,9 +44,8 @@ bool node_from_clipboard_text(const char * text, SceneNode & node)
 	LineReader line_reader(lines, 0, 0);
 	
 	const char * id = line_reader.get_next_line(true);
-
-	if (id == nullptr ||
-		strcmp(id, ":node") != 0)
+	
+	if (id == nullptr || (strcmp(id, ":node") != 0))
 	{
 		return false;
 	}
