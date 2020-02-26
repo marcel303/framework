5f2a77b299a7a3d868e513f375892494aa048b2b
Marcel Smit <marcel303@gmail.com>
2020-02-26 18:50:41 +0100

    framework: Add comments to a few enums.

--- framework/framework.h
+++ framework/framework.h
@@ -154,7 +154,7 @@ enum COLOR_MODE // setColorMode
 	COLOR_IGNORE
 };
 
-enum COLOR_POST
+enum COLOR_POST // setColorPost
 {
 	POST_NONE,
 	POST_RGB_MIX_ALPHA_TO_ZERO,
@@ -165,7 +165,7 @@ enum COLOR_POST
 	POST_RGB_TO_LUMI
 };
 
-enum DEPTH_TEST
+enum DEPTH_TEST // setDepthTest
 {
 	DEPTH_EQUAL,
 	DEPTH_LESS,
@@ -175,14 +175,14 @@ enum DEPTH_TEST
 	DEPTH_ALWAYS
 };
 
-enum CULL_MODE
+enum CULL_MODE // setCullMode
 {
 	CULL_NONE,
 	CULL_FRONT,
 	CULL_BACK
 };
 
-enum CULL_WINDING
+enum CULL_WINDING // setCullMode
 {
 	CULL_CCW,
 	CULL_CW
