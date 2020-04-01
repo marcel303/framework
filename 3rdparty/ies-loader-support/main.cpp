f5d4e058194a8acf5e03e591aee3fea8a3f754ba
Marcel Smit <marcel303@gmail.com>
2020-04-01 14:40:13 +0200

    ies-loader: Add a framework-based interactive IES preview application.

---
+++ 3rdparty/ies-loader-support/main.cpp
@@ -0,0 +1,95 @@
+#include "framework.h"
+#include "ies_loader.h"
+#include "Path.h"
+#include "TextIO.h"
+
+int main(int argc, char * argv[])
+{
+	setupPaths(CHIBI_RESOURCE_PATHS);
+	
+	if (!framework.init(400, 400))
+		return -1;
+
+	GxTextureId texture = 0;
+	
+	for (;;)
+	{
+		framework.waitForEvents = true;
+		
+		framework.process();
+
+		if (framework.quitRequested)
+			break;
+		
+		int viewSx;
+		int viewSy;
+		framework.getCurrentViewportSize(viewSx, viewSy);
+		
+		for (auto & file : framework.droppedFiles)
+		{
+			if (Path::GetExtension(file, true) == "ies")
+			{
+				char * text = nullptr;
+				size_t size;
+				if (TextIO::loadFileContents(file.c_str(), text, size))
+				{
+					IESLoadHelper helper;
+					IESFileInfo info;
+					
+					if (helper.load(text, size, info))
+					{
+					#if 1
+						uint8_t * data = new uint8_t[viewSx * viewSy];
+						
+						if (helper.saveAsPreview(info, data, viewSx, viewSy, 1))
+						{
+							freeTexture(texture);
+							
+							texture = createTextureFromR8(data, viewSx, viewSy, true, true);
+						}
+						
+						delete [] data;
+						data = nullptr;
+					#else
+						float * data = new float[viewSx * viewSy];
+						
+						if (helper.saveAs2D(info, data, viewSx, viewSy, 1))
+						{
+							freeTexture(texture);
+							
+							texture = createTextureFromR32F(data, viewSx, viewSy, true, true);
+						}
+						
+						delete [] data;
+						data = nullptr;
+					#endif
+					}
+					
+					delete [] text;
+					text = nullptr;
+				}
+			}
+		}
+
+		framework.beginDraw(0, 0, 0, 0);
+		{
+			if (texture != 0)
+			{
+				setColorClamp(false);
+				setLumif(mouse.x / 80.f);
+				setAlphaf(1.f);
+				setColor(colorWhite);
+				gxSetTexture(texture);
+				pushColorPost(POST_SET_RGB_TO_R);
+				drawRect(0, 0, viewSx, viewSy);
+				popColorPost();
+				gxSetTexture(0);
+			}
+		}
+		framework.endDraw();
+	}
+
+	framework.shutdown();
+
+	return 0;
+}
