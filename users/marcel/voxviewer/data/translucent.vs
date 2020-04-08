a85389a0586cc1e78b300337a41dfb43424c7c4f
Marcel Smit <marcel303@gmail.com>
2020-04-08 10:19:32 +0200

    voxviewer: - Add sky render. - Set directional light direction to sun direction. - Update light scatter origin using sun position. - Add shadow map drawing and lookup inside shader. - Enable screen space ambient occlusion. - Unify forward and deferred light drawing.

--- users/marcel/voxviewer/data/translucent.vs
+++ users/marcel/voxviewer/data/translucent.vs
@@ -1,15 +1,18 @@
 include engine/ShaderVS.txt
 
 shader_out vec3 v_position;
+shader_out vec3 v_normal;
 shader_out vec4 v_color;
 
 void main()
 {
 	vec4 position = unpackPosition();
+	vec3 normal = unpackNormal().xyz;
 	vec4 color = unpackColor();
 
 	gl_Position = objectToProjection(position);
 
 	v_position = objectToView(position).xyz;
+	v_normal = objectToView3(normal);
 	v_color = color;
 }
--- users/marcel/voxviewer/main.cpp
+++ users/marcel/voxviewer/main.cpp
@@ -12,13 +12,18 @@
 #include "lightDrawer.h"
 #include "renderer.h"
 #include "renderOptions.h"
+#include "shadowMapDrawer.h"
 
 #include "FileStream.h"
+
+#include "Quat.h"
 
 #include <stdint.h>
 #include <vector>
 
 using namespace rOne;
+
+static void drawSky(Vec3Arg sunPosition);
 
 int main(int argc, char * argv[])
 {
@@ -39,8 +44,8 @@ int main(int argc, char * argv[])
 	try
 	{
 		//FileStream stream("marcel-01.vox", OpenMode_Read);
-		FileStream stream("monu7.vox", OpenMode_Read);
-		//FileStream stream("room.vox", OpenMode_Read);
+		//FileStream stream("monu7.vox", OpenMode_Read);
+		FileStream stream("room.vox", OpenMode_Read);
 		StreamReader reader(&stream, false);
 		
 		readMagicaWorld(reader, world);
@@ -69,16 +74,23 @@ int main(int argc, char * argv[])
 	renderOptions.linearColorSpace = true;
 	renderOptions.backgroundColor.Set(1, .5f, .25f);
 	
-	renderOptions.fog.enabled = true;
-	renderOptions.fog.thickness = .01f;
+	renderOptions.screenSpaceAmbientOcclusion.enabled = true;
+	renderOptions.screenSpaceAmbientOcclusion.strength = 1.f;
 	
-	renderOptions.bloom.enabled = false;
+	renderOptions.fog.enabled = false;
+	renderOptions.fog.thickness = .001f;
+	
+	//renderOptions.debugRenderTargets = true;
+	
+	renderOptions.bloom.enabled = true;
 	renderOptions.bloom.blurSize = 20.f;
-	renderOptions.bloom.strength = .3f;
+	renderOptions.bloom.strength = .5f;
 	
 	renderOptions.lightScatter.enabled = true;
+	renderOptions.lightScatter.strength = 1.f;
+	renderOptions.lightScatter.numSamples = 40; // todo : bump to a higher number. fix light scatter getting more or less bright with # samples
 	
-	renderOptions.depthSilhouette.enabled = true;
+	renderOptions.depthSilhouette.enabled = false;
 	renderOptions.depthSilhouette.color.Set(0, 0, 0, .5f);
 	
 	renderOptions.fxaa.enabled = true;
@@ -87,6 +99,12 @@ int main(int argc, char * argv[])
 	
 	bool showSolids = false;
 	bool showTranslucents = false;
+	bool skyEnabled = false;
+	bool enableShadowMaps = true;
+	
+	ForwardLightingHelper helper;
+	ShadowMapDrawer shadowMapDrawer;
+	shadowMapDrawer.alloc(4, 1024);
 	
 	for (;;)
 	{
@@ -97,7 +115,7 @@ int main(int argc, char * argv[])
 
 		if (framework.quitRequested)
 			break;
-
+		
 		for (auto & file : framework.droppedFiles)
 		{
 			try
@@ -127,6 +145,12 @@ int main(int argc, char * argv[])
 		if (keyboard.wentDown(SDLK_t))
 			showTranslucents = !showTranslucents;
 		
+		if (keyboard.wentDown(SDLK_k))
+			skyEnabled = !skyEnabled;
+		
+		if (keyboard.wentDown(SDLK_m))
+			enableShadowMaps = !enableShadowMaps;
+		
 		camera.tick(framework.timeStep, true);
 		
 		framework.beginDraw(0, 0, 0, 0);
@@ -135,8 +159,115 @@ int main(int argc, char * argv[])
 			
 			camera.pushViewMatrix();
 			{
+				Quat sunRotation;
+				sunRotation.fromAngleAxis(sinf(framework.time/10.f) * M_PI/2.f * 1.1f, Vec3(1, 0.3f, -0.3f).CalcNormalized());
+				const Vec3 sunPosition = sunRotation.toMatrix().Mul3(Vec3(0, 40, 0));
+				//const Vec3 sunPosition(0, 20, 20);
+				
+			#if true
+				{
+					// update light scatter origin
+					
+					Mat4x4 projection;
+					Mat4x4 modelView;
+					gxGetMatrixf(GX_PROJECTION, projection.m_v);
+					gxGetMatrixf(GX_MODELVIEW, modelView.m_v);
+					
+					const Vec4 sunPosition_world(sunPosition[0], sunPosition[1], sunPosition[2], 1.f);
+					const Vec4 sunPosition_view = modelView * sunPosition_world;
+					const Vec4 sunPosition_projection = projection * sunPosition_view;
+					const Vec4 sunPosition_clip = sunPosition_projection / sunPosition_projection[3];
+					const Vec2 sunPosition_tex((sunPosition_clip[0] + 1.f) / 2.f, (-sunPosition_clip[1] + 1.f) / 2.f);
+					
+					renderOptions.lightScatter.origin.Set(sunPosition_tex[0], sunPosition_tex[1]);
+					
+					// update light scatter strength. fade out when not facing the sun
+					
+					const Vec3 lightDirection_view =
+						Vec3(
+							sunPosition_view[0],
+							sunPosition_view[1],
+							sunPosition_view[2]).CalcNormalized();
+					renderOptions.lightScatter.strengthMultiplier = fmaxf(0.f, lightDirection_view[2]);
+				}
+			#endif
+				
+				// -- make light list --
+				
+				std::vector<Light> lights;
+				
+				{
+					Light light;
+					light.type = kLightType_Directional;
+					light.position = sunPosition;
+					light.direction = -sunPosition.CalcNormalized();
+					light.attenuationBegin = 0.f;
+					light.attenuationEnd = 100.f;
+					light.color.Set(1, 1, 1);
+					light.intensity = .04f;
+					
+					lights.push_back(light);
+				}
+				
+				{
+					// camera following point light
+					
+					Light light;
+					light.type = kLightType_Point;
+					light.position = camera.position;
+					light.attenuationBegin = 0.f;
+					light.attenuationEnd = 4.f;
+					light.color.Set(1, 1, 1);
+					light.intensity = .01f;
+					
+					lights.push_back(light);
+					
+				// todo : add some kind of helper for building light lists
+					//helper.addPointLight(camera.position, 0.f, 4.f, Vec3(1, 1, 1), .01f);
+				}
+				
+				{
+					// rotating red spot light
+					
+					Light light;
+					light.type = kLightType_Spot;
+					light.position.Set(2, 0, -2);
+					//light.direction = Mat4x4(true).RotateY(framework.time * 4.f).GetAxis(2);
+					light.direction = Mat4x4(true).RotateY(framework.time / 2.f).GetAxis(2);
+					light.attenuationBegin = 0.01f;
+					light.attenuationEnd = 20.f;
+					light.color.Set(1, 0, 0);
+					light.spotAngle = float(M_PI/2.0);
+					light.intensity = 1.f;
+					
+					lights.push_back(light);
+				}
+				
+				if (false)
+				{
+					// pulsating red point light
+					
+					Light light;
+					light.type = kLightType_Point;
+					light.position.Set(0, 0, 0);
+					light.attenuationBegin = 0.f;
+					light.attenuationEnd = 2.f + sinf(framework.time * 2.34f) * 2.f;
+					light.color.Set(1, 0, 0);
+					light.intensity = .4f;
+					
+					lights.push_back(light);
+				}
+				
 				auto drawOpaque = [&]()
 				{
+					if (skyEnabled)
+					{
+						drawSky(sunPosition);
+					}
+					
+					//setColor(colorYellow);
+					//fillCube(sunPosition, Vec3(.4f, .4f, .4f));
+					
 					Shader shader("shader");
 					setShader(shader);
 					
@@ -177,11 +308,6 @@ int main(int argc, char * argv[])
 				
 				auto drawTranslucent = [&]()
 				{
-					ForwardLightingHelper helper;
-					helper.addPointLight(camera.position, 0.f, 4.f, Vec3(1, 1, 1), 1.f);
-					
-					helper.prepareShaderData(16, 16.f, true, camera.getViewMatrix());
-					
 					if (showTranslucents)
 					{
 						// draw some (semi-)transparent cubes
@@ -191,20 +317,42 @@ int main(int argc, char * argv[])
 						{
 							int nextTextureUnit = 0;
 							helper.setShaderData(shader, nextTextureUnit);
+							shadowMapDrawer.setShaderData(shader, nextTextureUnit, camera.getViewMatrix());
 							
-							pushCullMode(CULL_BACK, CULL_CCW);
+							const int kNumCubes = 1000;
+							Vec3 positions[kNumCubes];
+							
+							for (int i = 0; i < kNumCubes; ++i)
+							{
+								positions[i].Set(
+									cosf(i / 1.23f) * 6.f,
+									cosf(i / 2.34f) * 2.f,
+									cosf(i * 1.23f) * 6.f);
+							}
+							
+							Vec3 positions_view[kNumCubes];
+							int indices[kNumCubes];
+							Mat4x4 worldToView;
+							gxGetMatrixf(GX_MODELVIEW, worldToView.m_v);
+							for (int i = 0; i < kNumCubes; ++i)
+							{
+								positions_view[i] = worldToView.Mul4(positions[i]);
+								indices[i] = i;
+							}
+							
+							std::sort(indices, indices + kNumCubes, [&](int i1, int i2)
+								{
+									const float z1 = positions_view[i1][2];
+									const float z2 = positions_view[i2][2];
+									return z2 < z1;
+								});
+							
+							pushCullMode(CULL_BACK, CULL_CW);
 							beginCubeBatch();
 							{
-								for (int i = 0; i < 1*1000; ++i)
-								{
-									setColorf(1, 1, 1, .5f);
-									fillCube(
-										Vec3(
-											cosf(i / 1.23f) * 6.f,
-											cosf(i / 2.34f) * 2.f,
-											cosf(i * 1.23f) * 6.f),
-										Vec3(.2f, .2f, .2f));
-								}
+								setColorf(1, 1, 1, .5f);
+								for (int i = 0; i < kNumCubes; ++i)
+									fillCube(positions[indices[i]], Vec3(.2f, .2f, .2f));
 							}
 							endCubeBatch();
 							popCullMode();
@@ -215,10 +363,123 @@ int main(int argc, char * argv[])
 				
 				auto drawLights = [&]()
 				{
-					g_lightDrawer.drawDeferredAmbientLight(Vec3(.1f, .1f, .1f), 1.f);
+					//g_lightDrawer.drawDeferredAmbientLight(Vec3(1, 1, 1), .01f);
 					
-					g_lightDrawer.drawDeferredPointLight(camera.position, 0.f, 4.f, Vec3(1, 1, 1), 1.f);
+					for (auto & light : lights)
+					{
+						if (light.type == kLightType_Directional)
+						{
+							g_lightDrawer.drawDeferredDirectionalLight(
+								light.direction,
+								light.color,
+								light.color,
+								light.intensity);
+						}
+						else if (light.type == kLightType_Point)
+						{
+							g_lightDrawer.drawDeferredPointLight(
+								light.position,
+								light.attenuationBegin,
+								light.attenuationEnd,
+								light.color,
+								light.intensity);
+						}
+						else if (light.type == kLightType_Spot)
+						{
+							g_lightDrawer.drawDeferredSpotLight(
+								light.position,
+								light.direction,
+								light.spotAngle,
+								light.attenuationBegin,
+								light.attenuationEnd,
+								light.color,
+								light.intensity);
+						}
+					}
 				};
+				
+				// -- prepare lighting data --
+				
+				size_t lightId = 0;
+				
+				if (enableShadowMaps)
+				{
+					for (auto & light : lights)
+					{
+						if (light.type == kLightType_Directional)
+						{
+							Mat4x4 lightToWorld;
+							lightToWorld.MakeLookat(light.position, light.position + light.direction, Vec3(0, 1, 0));
+							lightToWorld = lightToWorld.CalcInv();
+						
+							shadowMapDrawer.addDirectionalLight(
+								lightId,
+								lightToWorld,
+								light.attenuationBegin,
+								light.attenuationEnd,
+								8.f);
+						}
+						else if (light.type == kLightType_Spot)
+						{
+							Mat4x4 lightToWorld;
+							lightToWorld.MakeLookat(light.position, light.position + light.direction, Vec3(0, 1, 0));
+							lightToWorld = lightToWorld.CalcInv();
+						
+							shadowMapDrawer.addSpotLight(
+								lightId,
+								lightToWorld,
+								light.spotAngle,
+								light.attenuationBegin,
+								light.attenuationEnd);
+						}
+						
+						lightId++;
+					}
+				}
+				
+				lightId = 0;
+				
+				for (auto & light : lights)
+				{
+					if (light.type == kLightType_Directional)
+					{
+						helper.addDirectionalLight(
+							light.direction,
+							light.color,
+							light.intensity,
+							shadowMapDrawer.getShadowMapId(lightId));
+					}
+					else if (light.type == kLightType_Point)
+					{
+						helper.addPointLight(
+							light.position,
+							light.attenuationBegin,
+							light.attenuationEnd,
+							light.color,
+							light.intensity,
+							shadowMapDrawer.getShadowMapId(lightId));
+					}
+					else if (light.type == kLightType_Spot)
+					{
+						helper.addSpotLight(
+							light.position,
+							light.direction,
+							light.spotAngle,
+							light.attenuationEnd,
+							light.color,
+							light.intensity,
+							shadowMapDrawer.getShadowMapId(lightId));
+					}
+				
+					lightId++;
+				}
+				
+				helper.prepareShaderData(16, 16.f, true, camera.getViewMatrix());
+				
+				shadowMapDrawer.drawOpaque = drawOpaque;
+				shadowMapDrawer.drawShadowMaps(camera.getViewMatrix());
+				
+				// -- render --
 				
 				RenderFunctions renderFunctions;
 				renderFunctions.drawOpaque = drawOpaque;
@@ -228,8 +489,15 @@ int main(int argc, char * argv[])
 				renderer.render(renderFunctions, renderOptions, framework.timeStep);
 			}
 			camera.popViewMatrix();
+			
+			projectScreen2d();
+			
+			//shadowMapDrawer.showRenderTargets();
 		}
 		framework.endDraw();
+		
+		helper.reset();
+		shadowMapDrawer.reset();
 	}
 	
 	// todo : shutdown renderer
@@ -237,4 +505,30 @@ int main(int argc, char * argv[])
 	framework.shutdown();
 
 	return 0;
+}
+
+static void drawSky(Vec3Arg sunPosition)
+{
+	pushBlend(BLEND_OPAQUE);
+	{
+		//const Vec3 lightDirection = Vec3(1, -.1f, 0).CalcNormalized();
+		const Vec3 lightDirection = -sunPosition.CalcNormalized();
+		
+		Mat4x4 viewMatrix;
+		gxGetMatrixf(GX_MODELVIEW, viewMatrix.m_v);
+		const Mat4x4 cameraMatrix = viewMatrix.CalcInv();
+		
+		Shader shader("rui-sky");
+		setShader(shader);
+		{
+			shader.setImmediate("ligthDirection",
+				lightDirection[0],
+				lightDirection[1],
+				lightDirection[2]);
+			
+			fillCube(cameraMatrix.GetTranslation(), Vec3(40, 40, 40));
+		}
+		clearShader();
+	}
+	popBlend();
 }
