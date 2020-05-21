1f7964605eb1e217caf7d37311920f9c91cc6b7c
Marcel Smit <marcel303@gmail.com>
2020-05-21 09:44:29 +0200

    framework: Include native app glue and define android_main. Call regular main from there.

---
+++ framework/framework-android-app.cpp
@@ -0,0 +1,60 @@
+/*
+	Copyright (C) 2020 Marcel Smit
+	marcel303@gmail.com
+	https://www.facebook.com/marcel.smit981
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#if defined(ANDROID)
+
+#include "framework-android-app.h"
+#include <android_native_app_glue.h>
+#include <android/native_activity.h>
+
+static android_app * s_app;
+
+android_app * get_android_app()
+{
+	return s_app;
+}
+
+//
+
+extern int main(int argc, char * argv[]);
+
+extern "C"
+{
+    void android_main(android_app * app)
+    {
+    	s_app = app;
+
+    	char * argv[] =
+    	{
+    		""
+    	};
+
+    	main(sizeof(argv) / sizeof(argv[0]), argv);
+    }
+}
+
+#endif
---
+++ framework/framework-android-app.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if defined(ANDROID)
+
+struct android_app;
+
+android_app * get_android_app();
+
+#endif
