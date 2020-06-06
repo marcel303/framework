fa2ef5d78d1108a4a6900beba5df625c21bca152
Marcel Smit <marcel303@gmail.com>
2020-06-06 13:04:28 +0200

    allegro-support: Make the allegro mutex recursive.

--- users/marcel/allegro2-support/allegro2-mutex.cpp
+++ users/marcel/allegro2-support/allegro2-mutex.cpp
@@ -3,26 +3,26 @@
 
 AllegroMutex::AllegroMutex()
 {
-	mutex = new std::mutex();
+	mutex = new std::recursive_mutex();
 }
 
 AllegroMutex::~AllegroMutex()
 {
-	auto * std_mutex = (std::mutex*)mutex;
+	auto * std_mutex = (std::recursive_mutex*)mutex;
 
 	delete std_mutex;
 }
 
 void AllegroMutex::lock()
 {
-	auto * std_mutex = (std::mutex*)mutex;
+	auto * std_mutex = (std::recursive_mutex*)mutex;
 
 	std_mutex->lock();	
 }
 
 void AllegroMutex::unlock()
 {
-	auto * std_mutex = (std::mutex*)mutex;
+	auto * std_mutex = (std::recursive_mutex*)mutex;
 
 	std_mutex->unlock();
 }
