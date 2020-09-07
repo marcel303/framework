0eedf13468b7a19ff7d86889a40deed18c43ecf5
Marcel Smit <marcel303@gmail.com>
2020-09-07 10:50:58 +0200

    libgg: - Add error checking parse functions. - Add double parsing functions.

--- libgg/Parse.cpp
+++ libgg/Parse.cpp
@@ -23,6 +23,53 @@ static bool strEqual(const char * a, const char * b)
 	return false;
 }
 
+// conversion with error checking
+
+bool Parse::Int32(const char * text, int32_t & result)
+{
+	char * endptr;
+
+	result = (int32_t)strtol(text, &endptr, 10);
+	
+	return endptr != text;
+}
+
+bool Parse::UInt32(const char * text, uint32_t & result)
+{
+	char * endptr;
+
+	result = (uint32_t)strtoul(text, &endptr, 10);
+	
+	return endptr != text;
+}
+
+bool Parse::Float(const char * text, float & result)
+{
+	char * endptr;
+
+	result = (float)strtod(text, &endptr);
+	
+	return endptr != text;
+}
+
+bool Parse::Double(const char * text, double & result)
+{
+	char * endptr;
+
+	result = strtod(text, &endptr);
+	
+	return endptr != text;
+}
+
+bool Parse::Bool(const char * text, bool & result)
+{
+	result = Bool(text);
+	
+	return true;
+}
+
+// conversion without error checking
+
 int Parse::Int32(const char * text)
 {
 	return atoi(text);
@@ -36,6 +83,11 @@ unsigned int Parse::UInt32(const char * text)
 float Parse::Float(const char * text)
 {
 	return (float)atof(text);
+}
+
+double Parse::Double(const char * text)
+{
+	return atof(text);
 }
 
 bool Parse::Bool(const char * text)
--- libgg/Parse.h
+++ libgg/Parse.h
@@ -1,13 +1,25 @@
 #pragma once
 
+#include <stdint.h>
 #include <string>
 
 class Parse
 {
 public:
-	static int Int32(const char * text);
-	static unsigned int UInt32(const char * text);
+	// conversion with error checking
+	
+	static bool Int32(const char * text, int32_t & result);
+	static bool UInt32(const char * text, uint32_t & result);
+	static bool Float(const char * text, float & result);
+	static bool Double(const char * text, double & result);
+	static bool Bool(const char * text, bool & result);
+	
+	// conversion without error checking
+	
+	static int32_t Int32(const char * text);
+	static uint32_t UInt32(const char * text);
 	static float Float(const char * text);
+	static double Double(const char * text);
 	static bool Bool(const char * text);
 	
 	// convenience functions for std::string
@@ -27,8 +39,40 @@ public:
 		return Float(text.c_str());
 	}
 	
+	static double Double(const std::string & text)
+	{
+		return Double(text.c_str());
+	}
+	
 	static bool Bool(const std::string & text)
 	{
 		return Bool(text.c_str());
+	}
+	
+	//
+	
+	static bool Int32(const std::string & text, int32_t & result)
+	{
+		return Int32(text.c_str(), result);
+	}
+	
+	static bool UInt32(const std::string & text, uint32_t & result)
+	{
+		return UInt32(text.c_str(), result);
+	}
+	
+	static bool Float(const std::string & text, float & result)
+	{
+		return Float(text.c_str(), result);
+	}
+	
+	static bool Double(const std::string & text, double & result)
+	{
+		return Double(text.c_str(), result);
+	}
+	
+	static bool Bool(const std::string & text, bool & result)
+	{
+		return Bool(text.c_str(), result);
 	}
 };
