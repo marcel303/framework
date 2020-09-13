137ab4d6da5ef92725e1c72eeb37f04f1dbc5f7e
Marcel Smit <marcel303@gmail.com>
2020-09-13 18:05:32 +0200

    audiograph: Remove a todo.

--- audioGraph/audioGraphMgr/audioGraphRealTimeConnection.cpp
+++ audioGraph/audioGraphMgr/audioGraphRealTimeConnection.cpp
@@ -128,10 +128,6 @@ void AudioRealTimeConnection::updateAudioValues()
 	if (audioGraph == nullptr)
 		return;
 
-// todo : make a copy of the history data when we're done here
-//        this to avoid data races, where the audio thread is calling this method and providing more data
-//        while at the same time we're drawing visualizers
-
 	AUDIO_SCOPE;
 	
 	if (audioGraph->currentTickTraversalId < 0)
