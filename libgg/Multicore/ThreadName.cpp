35fc7d229a381ab9f48a535bf674e5e7204a73d0
Marcel Smit <marcel303@gmail.com>
2020-10-19 22:44:38 +0200

    Windows compile fix.

---
+++ libgg/Multicore/ThreadName.cpp
@@ -0,0 +1,43 @@
+#include "ThreadName.h"
+
+#if defined(WINDOWS)
+	#include <Windows.h>
+	
+	static const DWORD MS_VC_EXCEPTION = 0x406D1388;
+
+	#pragma pack(push,8)
+	struct THREADNAME_INFO
+	{
+		DWORD dwType; // Must be 0x1000.
+		LPCSTR szName; // Pointer to name (in user addr space).
+		DWORD dwThreadID; // Thread ID (-1=caller thread).
+		DWORD dwFlags; // Reserved for future use, must be zero.
+	};
+	#pragma pack(pop)
+#else
+	#include <thread>
+#endif
+
+void SetCurrentThreadName(const char * name)
+{
+#if defined(MACOS)
+	pthread_setname_np(name);
+#elif defined(LINUX)
+	pthread_setname_np(pthread_self(), name);
+#elif defined(WINDOWS)
+	THREADNAME_INFO info;
+    info.dwType = 0x1000;
+    info.szName = name;
+    info.dwThreadID = (DWORD)-1;
+    info.dwFlags = 0;
+	#pragma warning(push)
+	#pragma warning(disable: 6320 6322)
+    __try {
+        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+    } __except (EXCEPTION_EXECUTE_HANDLER) {
+    }
+#pragma warning(pop)
+#else
+	#error
+#endif
+}
--- libgg/Multicore/ThreadName.h
+++ libgg/Multicore/ThreadName.h
@@ -1,16 +1,3 @@
 #pragma once
 
-#include <thread>
-
-inline void SetCurrentThreadName(const char * name)
-{
-#if defined(MACOS)
-	pthread_setname_np(name);
-#elif defined(LINUX)
-	pthread_setname_np(pthread_self(), name);
-#elif defined(WINDOWS)
-	SetThreadName(name);
-#else
-	#error
-#endif
-}
+void SetCurrentThreadName(const char * name);
