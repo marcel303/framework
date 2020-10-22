60eb69b7512dd02cb90709efbc795ae8de9cc6c8
Marcel Smit <marcel303@gmail.com>
2020-10-22 20:56:22 +0200

    framework: Separate loading wave headers from wave data.

--- framework/soundfile/SoundIO-wave.cpp
+++ framework/soundfile/SoundIO-wave.cpp
@@ -103,16 +103,8 @@ static bool readChunk(FileReader & r, Chunk & chunk, int32_t & size)
 	}
 }
 
-SoundData * loadSound_WAV(const char * filename)
+struct WaveHeadersReader
 {
-	FileReader r;
-	
-	if (!r.open(filename, false))
-	{
-		LOG_ERR("failed to open %s", filename);
-		return nullptr;
-	}
-	
 	bool hasFmt = false;
 	int32_t fmtLength;
 	int16_t fmtCompressionType; // format code is a better name. 1 = PCM/integer, 2 = ADPCM, 3 = float, 7 = u-law
@@ -127,229 +119,151 @@ SoundData * loadSound_WAV(const char * filename)
 	int32_t dataOffset = 0;
 	int32_t dataLength = 0;
 	
-	for (;;)
+	bool read(FileReader & r)
 	{
-		Chunk chunk;
-		int32_t byteCount;
-		
-		if (!readChunk(r, chunk, byteCount))
-			break;
-		
-		if (chunk == kChunk_RIFF || chunk == kChunk_WAVE)
+		for (;;)
 		{
-			// just process sub chunks
-		}
-		else if (chunk == kChunk_FMT)
-		{
-			bool ok = true;
+			Chunk chunk;
+			int32_t byteCount;
 			
-			ok &= r.read(fmtLength);
-			ok &= r.read(fmtCompressionType);
-			ok &= r.read(fmtChannelCount);
-			ok &= r.read(fmtSampleRate);
-			ok &= r.read(fmtByteRate);
-			ok &= r.read(fmtBlockAlign);
-			ok &= r.read(fmtBitDepth);
+			if (!readChunk(r, chunk, byteCount))
+				break;
 			
-			if (fmtCompressionType != 1)
-				ok &= r.read(fmtExtraLength);
-			else
-				fmtExtraLength = 0;
-			
-			if (fmtCompressionType == WAVE_FORMAT_EXTENSIBLE)
+			if (chunk == kChunk_RIFF || chunk == kChunk_WAVE)
 			{
-				// read WAVEFORMATEXTENSIBLE structure and change format accordingly
+				// just process sub chunks
+			}
+			else if (chunk == kChunk_FMT)
+			{
+				bool ok = true;
 				
-				int16_t numValidBits;
-				int32_t channelMask;
-				int32_t guidFormatTag;
-				int8_t guidRemainder[12];
+				ok &= r.read(fmtLength);
+				ok &= r.read(fmtCompressionType);
+				ok &= r.read(fmtChannelCount);
+				ok &= r.read(fmtSampleRate);
+				ok &= r.read(fmtByteRate);
+				ok &= r.read(fmtBlockAlign);
+				ok &= r.read(fmtBitDepth);
 				
-				ok &= r.read(numValidBits);
-				ok &= r.read(channelMask);
-				ok &= r.read(guidFormatTag);
-				ok &= r.read(guidRemainder, 12);
+				if (fmtCompressionType != 1)
+					ok &= r.read(fmtExtraLength);
+				else
+					fmtExtraLength = 0;
 				
-				if (guidFormatTag == WAVE_FORMAT_PCM)
+				if (fmtCompressionType == WAVE_FORMAT_EXTENSIBLE)
 				{
-					fmtCompressionType = WAVE_FORMAT_PCM;
-				}
-				else if (guidFormatTag == WAVE_FORMAT_IEEE_FLOAT)
-				{
-					fmtCompressionType = WAVE_FORMAT_IEEE_FLOAT;
+					// read WAVEFORMATEXTENSIBLE structure and change format accordingly
+					
+					int16_t numValidBits;
+					int32_t channelMask;
+					int32_t guidFormatTag;
+					int8_t guidRemainder[12];
+					
+					ok &= r.read(numValidBits);
+					ok &= r.read(channelMask);
+					ok &= r.read(guidFormatTag);
+					ok &= r.read(guidRemainder, 12);
+					
+					if (guidFormatTag == WAVE_FORMAT_PCM)
+					{
+						fmtCompressionType = WAVE_FORMAT_PCM;
+					}
+					else if (guidFormatTag == WAVE_FORMAT_IEEE_FLOAT)
+					{
+						fmtCompressionType = WAVE_FORMAT_IEEE_FLOAT;
+					}
+					else
+					{
+						LOG_ERR("unknown format found in WAVEFORMATEXTENSIBLE");
+						ok = false;
+					}
 				}
 				else
 				{
-					LOG_ERR("unknown format found in WAVEFORMATEXTENSIBLE");
+					ok &= r.skip(fmtExtraLength);
+				}
+				
+				if (!ok)
+				{
+					LOG_ERR("failed to read FMT chunk");
+					return false;
+				}
+				
+				if (fmtCompressionType != WAVE_FORMAT_PCM &&
+					fmtCompressionType != WAVE_FORMAT_IEEE_FLOAT)
+				{
+					LOG_ERR("only PCM and IEEE float are supported. type: %d", fmtCompressionType);
 					ok = false;
 				}
-			}
-			else
-			{
-				ok &= r.skip(fmtExtraLength);
-			}
-			
-			if (!ok)
-			{
-				LOG_ERR("failed to read FMT chunk");
-				return nullptr;
-			}
-			
-			if (fmtCompressionType != WAVE_FORMAT_PCM &&
-				fmtCompressionType != WAVE_FORMAT_IEEE_FLOAT)
-			{
-				LOG_ERR("only PCM and IEEE float are supported. type: %d", fmtCompressionType);
-				ok = false;
-			}
-			if (fmtChannelCount <= 0)
-			{
-				LOG_ERR("invalid channel count: %d", fmtChannelCount);
-				ok = false;
-			}
-			if (fmtBitDepth != 8 && fmtBitDepth != 16 && fmtBitDepth != 24 && fmtBitDepth != 32)
-			{
-				LOG_ERR("bit depth not supported: %d", fmtBitDepth);
-				ok = false;
-			}
-			
-			if (!ok)
-				return nullptr;
-			
-			hasFmt = true;
-		}
-		else if (chunk == kChunk_DATA)
-		{
-		#if 1
-			dataOffset = r.position();
-			dataLength = byteCount;
-			
-			hasData = true;
-			
-			if (!r.skip(byteCount))
-			{
-				LOG_ERR("failed to read DATA chunk");
-				return nullptr;
-			}
-		#else
-			if (hasFmt == false)
-			{
-				LOG_ERR("DATA chunk precedes FMT chunk. cannot load WAVE data when we don't know the format yet");
+				if (fmtChannelCount <= 0)
+				{
+					LOG_ERR("invalid channel count: %d", fmtChannelCount);
+					ok = false;
+				}
+				if (fmtBitDepth != 8 && fmtBitDepth != 16 && fmtBitDepth != 24 && fmtBitDepth != 32)
+				{
+					LOG_ERR("bit depth not supported: %d", fmtBitDepth);
+					ok = false;
+				}
 				
-				return nullptr;
+				if (!ok)
+					return false;
+			
+				hasFmt = true;
 			}
-			
-		// todo : remember offset to data. keep parsing chunks. load data after having parsed all the chunks. this will make it possible to separate parsing the headers/chunks from the data, and create either a sound loader, or an audio stream implementation, using the same RIFF/WAVE header parsing code
-			bytes = new uint8_t[byteCount];
-			
-			if (!r.read(bytes, byteCount))
+			else if (chunk == kChunk_DATA)
 			{
-				LOG_ERR("failed to load WAVE data");
-				delete [] bytes;
-				return nullptr;
+				dataOffset = r.position();
+				dataLength = byteCount;
+				
+				hasData = true;
+				
+				if (!r.skip(byteCount))
+				{
+					LOG_ERR("failed to seek until end of chunk");
+					return false;
+				}
 			}
-			
-			// convert data if necessary
-			
-			if (fmtCompressionType == WAVE_FORMAT_PCM)
+			else if (chunk == kChunk_OTHER)
 			{
-				if (fmtBitDepth == 8)
+				//LOG_DBG("wave loader: skipping %d bytes of unknown chunk", byteCount);
+				
+				if (!r.skip(byteCount))
 				{
-					// for 8 bit data the integers are unsigned. convert them to signed here
-					
-					const uint8_t * srcValues = bytes;
-					int8_t * dstValues = (int8_t*)bytes;
-					const int numValues = byteCount;
-					
-					for (int i = 0; i < numValues; ++i)
-					{
-						const int value = int(srcValues[i]) - 128;
-						
-						dstValues[i] = value;
-					}
-				}
-				else if (fmtBitDepth == 16)
-				{
-					// 16 bit data is already signed. no conversion needed
-				}
-				else if (fmtBitDepth == 24)
-				{
-					const int sampleCount = byteCount / 3;
-					float * samplesData = new float[sampleCount];
-					
-					for (int i = 0; i < sampleCount; ++i)
-					{
-						int32_t value =
-							(bytes[i * 3 + 0] << 8) |
-							(bytes[i * 3 + 1] << 16) |
-							(bytes[i * 3 + 2] << 24);
-						
-						// perform a shift right to sign-extend the 24 bit number (which we packed into the top-most 24 bits of a 32 bits number)
-						value >>= 8;
-						
-						samplesData[i] = value / float(1 << 23);
-					}
-					
-					delete [] bytes;
-					bytes = nullptr;
-					
-					bytes = (uint8_t*)samplesData;
-					
-					fmtBitDepth = 32;
-					byteCount = byteCount * 4 / 3;
-				}
-				else if (fmtBitDepth == 32)
-				{
-					const int32_t * srcValues = (int32_t*)bytes;
-					float * dstValues = (float*)bytes;
-					const int numValues = byteCount / 4;
-					
-					for (int i = 0; i < numValues; ++i)
-					{
-						dstValues[i] = float(srcValues[i] / double(1 << 31));
-					}
+					LOG_ERR("failed to seek until end of chunk");
+					return false;
 				}
 			}
-			else if (fmtCompressionType == WAVE_FORMAT_IEEE_FLOAT)
-			{
-				if (fmtBitDepth == 32)
-				{
-					// no conversion is needed
-				}
-				else
-				{
-					LOG_ERR("only 32 bit IEEE float is supported");
-					delete [] bytes;
-					return nullptr;
-				}
-			}
-			else
-			{
-				LOG_ERR("unknown WAVE data format");
-				delete [] bytes;
-				return nullptr;
-			}
-			
-			numBytes = byteCount;
-			
-			done = true;
-		#endif
 		}
-		else if (chunk == kChunk_OTHER)
-		{
-			//LOG_DBG("wave loader: skipping %d bytes of unknown chunk", byteCount);
-			
-			r.skip(byteCount);
-		}
+		
+		return true;
+	}
+};
+
+SoundData * loadSound_WAV(const char * filename)
+{
+	FileReader r;
+	
+	if (!r.open(filename, false))
+	{
+		LOG_ERR("failed to open %s", filename);
+		return nullptr;
 	}
 	
-#if 1
-	if (hasFmt == false)
+	WaveHeadersReader headers;
+	
+	if (!headers.read(r))
+	{
+		return nullptr;
+	}
+	
+	if (headers.hasFmt == false)
 	{
 		LOG_ERR("missing FMT chunk. cannot load WAVE data when we don't know the format yet");
 		return nullptr;
 	}
 
-	if (hasData == false)
+	if (headers.hasData == false)
 	{
 		LOG_ERR("missing DATA chunk. cannot load WAVE data when we don't know the format yet");
 		return nullptr;
@@ -357,14 +271,14 @@ SoundData * loadSound_WAV(const char * filename)
 	
 // todo : remember offset to data. keep parsing chunks. load data after having parsed all the chunks. this will make it possible to separate parsing the headers/chunks from the data, and create either a sound loader, or an audio stream implementation, using the same RIFF/WAVE header parsing code
 
-	if (!r.seek(dataOffset))
+	if (!r.seek(headers.dataOffset))
 	{
 		LOG_ERR("failed to seek to WAVE data");
 		return nullptr;
 	}
 	
-	uint8_t * bytes = new uint8_t[dataLength];
-	int numBytes = dataLength;
+	uint8_t * bytes = new uint8_t[headers.dataLength];
+	int numBytes = headers.dataLength;
 
 	if (!r.read(bytes, numBytes))
 	{
@@ -375,9 +289,9 @@ SoundData * loadSound_WAV(const char * filename)
 
 	// convert data if necessary
 
-	if (fmtCompressionType == WAVE_FORMAT_PCM)
+	if (headers.fmtCompressionType == WAVE_FORMAT_PCM)
 	{
-		if (fmtBitDepth == 8)
+		if (headers.fmtBitDepth == 8)
 		{
 			// for 8 bit data the integers are unsigned. convert them to signed here
 			
@@ -392,11 +306,11 @@ SoundData * loadSound_WAV(const char * filename)
 				dstValues[i] = value;
 			}
 		}
-		else if (fmtBitDepth == 16)
+		else if (headers.fmtBitDepth == 16)
 		{
 			// 16 bit data is already signed. no conversion needed
 		}
-		else if (fmtBitDepth == 24)
+		else if (headers.fmtBitDepth == 24)
 		{
 			const int sampleCount = numBytes / 3;
 			float * samplesData = new float[sampleCount];
@@ -419,10 +333,10 @@ SoundData * loadSound_WAV(const char * filename)
 			
 			bytes = (uint8_t*)samplesData;
 			
-			fmtBitDepth = 32;
+			headers.fmtBitDepth = 32;
 			numBytes = numBytes * 4 / 3;
 		}
-		else if (fmtBitDepth == 32)
+		else if (headers.fmtBitDepth == 32)
 		{
 			const int32_t * srcValues = (int32_t*)bytes;
 			float * dstValues = (float*)bytes;
@@ -434,9 +348,9 @@ SoundData * loadSound_WAV(const char * filename)
 			}
 		}
 	}
-	else if (fmtCompressionType == WAVE_FORMAT_IEEE_FLOAT)
+	else if (headers.fmtCompressionType == WAVE_FORMAT_IEEE_FLOAT)
 	{
-		if (fmtBitDepth == 32)
+		if (headers.fmtBitDepth == 32)
 		{
 			// no conversion is needed
 		}
@@ -453,21 +367,12 @@ SoundData * loadSound_WAV(const char * filename)
 		delete [] bytes;
 		return nullptr;
 	}
-#endif
-
-	{
-		// suppress unused variables warnings
-		(void)fmtLength;
-		(void)fmtByteRate;
-		(void)fmtBlockAlign;
-		(void)fmtExtraLength;
-	}
 	
 	SoundData * soundData = new SoundData();
-	soundData->channelSize = fmtBitDepth / 8;
-	soundData->channelCount = fmtChannelCount;
-	soundData->sampleCount = numBytes / (fmtBitDepth / 8 * fmtChannelCount);
-	soundData->sampleRate = fmtSampleRate;
+	soundData->channelSize = headers.fmtBitDepth / 8;
+	soundData->channelCount = headers.fmtChannelCount;
+	soundData->sampleCount = numBytes / (headers.fmtBitDepth / 8 * headers.fmtChannelCount);
+	soundData->sampleRate = headers.fmtSampleRate;
 	soundData->sampleData = bytes;
 	
 	return soundData;
