30844d237566b9ed785f40f4dc69395e9750cf64
Marcel Smit <marcel303@gmail.com>
2022-02-08 22:07:15 +0100

    libgg: Use a more robust hash combine function.

--- libgg/Hash.h
+++ libgg/Hash.h
@@ -6,7 +6,12 @@ typedef uint32_t Hash;
 
 inline Hash Hash_Combine(Hash hash1, Hash hash2)
 {
-	return hash1 ^ hash2;
+	// note : XOR may be dangerous when combing the same hash twice - it essentially cancels out both hashes
+	//return hash1 ^ hash2;
+	
+	// boost's hash combine..
+	hash1 ^= hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2);
+	return hash1;
 }
 
 namespace HashFunc
